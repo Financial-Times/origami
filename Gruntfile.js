a8da2314565adc57933dd307e596bf99911aec16
Rhys Evans <rhys.evans@ft.com>
2013-12-02 14:55:14 +0000

    Included grunt-origami-demoer module

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+/*
+ * grunt-contrib-uglify
+ * http://gruntjs.com/
+ *
+ * Copyright (c) 2013 "Cowboy" Ben Alman, contributors
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    'origami-demo': {
+      options: {
+        modernizr: false,
+        viewModel: {
+          footer: require('./footer.json')
+        }
+      }
+    },
+    watch: {
+      'origami-demo': {
+          files: ['*.scss', 'footer.mustache', 'footer.json', 'bower-components/**/*'],
+          tasks: ['origami-demo']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-origami-demoer');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['origami-demo']);
+
+};
