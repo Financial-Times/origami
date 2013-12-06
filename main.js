1fec355a2563f3ca6ab5cc4889ebee732a2bd590
Rhys Evans <rhys.evans@ft.com>
2013-12-06 12:51:25 +0000

    Added multiple overlay examples with triggers

--- main.js
+++ main.js
@@ -2,50 +2,57 @@ function getSpacing(el, side) {
   return parseInt(el.css('padding-' + side), 10) + parseInt(el.css('margin-' + side), 10);
 }
 
-$(".dialog").each(function () {
-  var dialogWrapper = $(this),
-    dialogEl = dialogWrapper.children(".dialog__content"),
-    dialogOrigWidth = dialogEl.outerWidth() + getSpacing(dialogWrapper, 'left') + getSpacing(dialogWrapper, 'right'),
-    dialogOrigHeight = dialogEl.outerHeight() + getSpacing(dialogWrapper, 'top') + getSpacing(dialogWrapper, 'bottom');
 
 
 
-  function resizeDialog() {
-    var win = $(window),
-        vw = win.width(),
-        vh = win.height();
-    
-    if (vw <= dialogOrigWidth) {
-      dialogWrapper.addClass("dialog--full-width");
-    } else {
-       dialogWrapper.removeClass("dialog--full-width");
+$.fn.resizify = function () {
+  $(window).off("resize.resizify");
+  
+  return this.each(function () {
+    var dialogWrapper = $(this),
+      dialogEl = dialogWrapper.children(".dialog__content"),
+      dialogOrigWidth = dialogEl.outerWidth() + getSpacing(dialogWrapper, 'left') + getSpacing(dialogWrapper, 'right'),
+      dialogOrigHeight = dialogEl.outerHeight() + getSpacing(dialogWrapper, 'top') + getSpacing(dialogWrapper, 'bottom');
+
+
+
+    function resizeDialog() {
+      var win = $(window),
+          vw = win.width(),
+          vh = win.height();
+      
+      if (vw <= dialogOrigWidth) {
+        dialogWrapper.addClass("dialog--full-width");
+      } else {
+         dialogWrapper.removeClass("dialog--full-width");
+      }
+      
+      
+      if (vh <= dialogOrigHeight) {
+        dialogWrapper.addClass("dialog--full-height");
+      } else {
+        dialogWrapper.removeClass("dialog--full-height");
+      }
+      
     }
-    
-    
-    if (vh <= dialogOrigHeight) {
-      dialogWrapper.addClass("dialog--full-height");
-    } else {
-      dialogWrapper.removeClass("dialog--full-height");
-    }
-    
-  }
 
-  resizeDialog();
+    resizeDialog();
 
-  $(window).on("resize", function() {
-      resizeDialog();
+    $(window).on("resize.resizify", function() {
+        resizeDialog();
+    });
+
+
+
   });
-
-
-
-});
+};
 
 /* document.getElementById('username').focus(); */
 
-$('.login-trigger').click(function () {
-    $('.login-form').toggleClass('is-open');
+$('.dialog-trigger').click(function () {
+    $('.' + this.dataset.target).toggleClass('is-open').resizify();
 });
 
 $('.dialog--overlay').click(function () {
-    $(this).css('display', 'none');
+    $(this).removeClass('is-open');
 });
