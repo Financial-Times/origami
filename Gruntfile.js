8940cbfda7e31731d685b791f4d31139507ff1f7
Dan Searle <dan.searle@ft.com>
2013-12-12 15:15:59 +0000

    Draft o-ft-fonts Origami module for styling forms and buttons.

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+"use strict";
+
+module.exports = function(grunt) {
+
+   // Project configuration.
+  grunt.initConfig({
+    'origami-demo': {
+      options: {
+        modernizr: false,
+        main: ['main.scss', 'example.mustache'],
+        viewModel: {}// a javascript object of example content to be consumed by your module's template
+      }
+    },
+    'watch': {
+      'origami-demo': {
+          files: ['example.mustache', 'main.scss', 'src/**/*'], //edit as necessary
+          tasks: ['origami-demo']
+      }
+    }
+  });
+
+   grunt.loadNpmTasks('grunt-origami-demoer');
+   grunt.loadNpmTasks('grunt-contrib-watch');
+
+   grunt.registerTask('default', ['origami-demo']);
+
+ };
