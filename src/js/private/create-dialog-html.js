272b882c1078c40f5bfa7f9b2c37158984bc2a6e
Rhys Evans <rhys.evans@ft.com>
2014-02-07 13:46:59 +0000

    fixed typo

--- src/js/private/create-dialog-html.js
+++ src/js/private/create-dialog-html.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var $ = require('jquery'),
-    closeDialog = require('../public/close-dialog')
+    closeDialog = require('../public/close-dialog'),
     
     globalListenersApplied = false,
 
