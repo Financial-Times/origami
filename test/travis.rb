707227fc16e8084952147a4b2832155594014820
Dan Searle <dan.searle@ft.com>
2014-02-11 14:37:57 +0000

    Added alternate style of Heading Medium for old IEs. Hover classes now correctly use o-hoverable, as per Origami spec. Added browser features to origami.json Created new functions and mixing for wrapping styles in the correct selectors depending silent mode (or not). Added missing Heading XLarge link style. Organised styles better. Improved wording of Travis tests.

--- test/travis.rb
+++ test/travis.rb
@@ -1,17 +1,17 @@
 #!/usr/bin/env ruby
 
-Dir.mkdir("build");
-
-# Compile o-ft-typography normally
-result = `sass --load-path ./bower_components/ --scss main.scss build/main.css`
+# Compile o-ft-typography normally (silent)
+result = `sass --style compressed --load-path ./bower_components/ --scss main.scss test/main.css`
 
 # Check the exit code was 0 (i.e. no errors), raise an exception with the output from sass if not
 raise result unless $?.to_i == 0
-raise "When compiled the module should output some CSS" unless File.exists?('build/main.css');
-puts "Regular compile worked successfully"
+raise "By default the module should not output any CSS" unless File.size('test/main.css') == 0
+puts "Regular compile was correctly silent."
+File.delete('test/main.css')
 
-# Compile o-ft-typography with silent classes
-result = `sass --load-path ./bower_components/ --scss test/silent.scss build/silent.css --style compressed`
+# Compile o-ft-typography with silent mode off
+result = `sass --load-path ./bower_components/ --scss test/not-silent.scss test/not-silent.css --style compressed`
 raise result unless $?.to_i == 0
-raise "When $o-ft-typography-is-silent is set to true the module should not output any CSS" unless File.size('build/silent.css') == 0
-puts "Silent compile worked successfully"
+raise "When $o-ft-typography-is-silent: false the module should output some CSS." unless File.exists?('test/not-silent.css');
+puts "Non-silent compile was successful."
+File.delete('test/not-silent.css')
