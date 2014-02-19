9a6266e71f4ce4c9226e406cfec1a1714a3881ad
Dan Searle <dan.searle@ft.com>
2014-02-19 10:21:07 +0000

    Proposed new approach for defining colour use cases. Makes use cases CSS-property-agnostic (except when in non-silent mode) and allows for a more limited set of 'primary' use case, which can optionally have variants.

--- test/travis.rb
+++ test/travis.rb
@@ -12,26 +12,27 @@ end
 # Prepare build test folder
 FileUtils.mkdir_p "test/output"
 
-# Compile o-colors normally
-stdout, stderr, status = Open3.capture3 "sass --scss main.scss test/output/main.css"
+
+# Compile o-colors normally (silent)
+stdout, stderr, status = Open3.capture3 "sass --style compressed --scss main.scss test/output/main.css"
 raise stderr unless status.success?
 raise stderr if stderr.length > 0
-raise "When compiled the module should not output any CSS" unless File.exists? 'test/output/main.css'
-puts "Default compile was correctly silent."
+raise "When $o-colors-is-silent is set to true the module should not output any CSS" unless File.size('test/output/main.css') == 0
+puts "Normal compile was correctly silent."
 File.delete('test/output/main.css')
 
-# Compile o-colors with silent mode turned off
-stdout, stderr, status = Open3.capture3 "sass --scss test/not-silent.scss test/output/not-silent.css --style compressed"
+# Compile o-colors with silent mode off
+stdout, stderr, status = Open3.capture3 "sass --style compressed --scss test/not-silent.scss test/output/not-silent.css"
 raise stderr unless status.success?
 raise stderr if stderr.length > 0
-raise "When $o-colors-is-silent is set to true the module should not output any CSS" unless File.size('test/output/not-silent.css') == 0
-puts "Compile with silent mode off correctly produced some CSS."
+raise "When compiled with silent mode off, the module should output some CSS" unless File.size('test/output/not-silent.css') > 0
+puts "Non-silent compile correctly produced output"
 File.delete('test/output/not-silent.css')
 
 # Attempting to use an undefined color generates a warning
 stdout, stderr, status = Open3.capture3 "sass --scss test/undefined-colors-warn.scss test/output/undefined-colors-warn.css --style compressed"
 raise "Using an undefined color shouldn't fail build" unless status.success?
-raise "Using an undefined color should throw a warning" unless stderr.squish.match /^WARNING\: Undefined use-case 'undefined-use-case-name' used on line [0-9]+ of [0-9A-Za-z_\/\\\.]+$/
+raise "Using an undefined color should throw a warning" unless stderr.squish.match /^WARNING\: Undefined use-case: name 'undefined-use-case-name', variant 'default' used on line [0-9]+ of [0-9A-Za-z_\/\\\.]+$/
 raise "Using an undefined color should not affect output" unless File.open("test/output/undefined-colors-warn.css").read.squish == ".test{color:#000}"
 puts "Checked non-fatal warnings thrown when undefined color used"
 File.delete('test/output/undefined-colors-warn.css')
