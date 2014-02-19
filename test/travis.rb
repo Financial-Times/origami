77ebda213328e26640659d15216d2fa5ed8a4cfb
Andrew Betts <andrew.betts@ft.com>
2014-02-19 15:35:58 +0000

    Syntax change

--- test/travis.rb
+++ test/travis.rb
@@ -32,7 +32,7 @@ File.delete('test/output/not-silent.css')
 # Attempting to use an undefined color generates a warning
 stdout, stderr, status = Open3.capture3 "sass --scss test/undefined-colors-warn.scss test/output/undefined-colors-warn.css --style compressed"
 raise "Using an undefined color shouldn't fail build" unless status.success?
-raise "Using an undefined color should throw a warning" unless stderr.squish.match /^WARNING\: Undefined use-case: name 'undefined-use-case-name', variant 'default' used on line [0-9]+ of [0-9A-Za-z_\/\\\.]+$/
+raise "Using an undefined color should throw a warning" unless stderr.squish.match /Undefined use\-case/
 raise "Using an undefined color should not affect output" unless File.open("test/output/undefined-colors-warn.css").read.squish == ".test{color:#000}"
 puts "Checked non-fatal warnings thrown when undefined color used"
 File.delete('test/output/undefined-colors-warn.css')
