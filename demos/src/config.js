f5f73e80de21b5fb797382a92514cd6b08c6f62c
Rhys Evans <rhys.evans@ft.com>
2014-03-20 16:28:55 +0000

    updated to use origami build tools for demo

---
+++ demos/src/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    demos: {
+        modals: {
+            sass: 'demos/src/demo.scss',
+            js: 'demos/src/demo.js',
+            template: 'demos/src/demo.mustache'
+        }
+    }
+};
