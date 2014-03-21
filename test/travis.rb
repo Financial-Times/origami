9acf76ec6b46a59b94a3b725d6015c94ee981780
Dan Searle <dan.searle@ft.com>
2014-03-21 09:39:55 +0000

    Removed CSS classes for palette colours when silent mode is turned off. Fixes #35. Replaced Ruby versions of standard tests with origami-build-tools test (kept Ruby test for deprecation warning).

--- test/travis.rb
+++ test/travis.rb
@@ -12,23 +12,6 @@ end
 # Prepare build test folder
 FileUtils.mkdir_p "test/output"
 
-
-# Compile o-colors normally (silent)
-stdout, stderr, status = Open3.capture3 "sass --style compressed --scss main.scss test/output/main.css"
-raise stderr unless status.success?
-raise stderr if stderr.length > 0
-raise "When $o-colors-is-silent is set to true the module should not output any CSS" unless File.size('test/output/main.css') == 0
-puts "Normal compile was correctly silent."
-File.delete('test/output/main.css')
-
-# Compile o-colors with silent mode off
-stdout, stderr, status = Open3.capture3 "sass --style compressed --scss test/not-silent.scss test/output/not-silent.css"
-raise stderr unless status.success?
-raise stderr if stderr.length > 0
-raise "When compiled with silent mode off, the module should output some CSS" unless File.size('test/output/not-silent.css') > 0
-puts "Non-silent compile correctly produced output"
-File.delete('test/output/not-silent.css')
-
 # Attempting to use an undefined color generates a warning
 stdout, stderr, status = Open3.capture3 "sass --scss test/undefined-colors-warn.scss test/output/undefined-colors-warn.css --style compressed"
 raise "Using an undefined color shouldn't fail build" unless status.success?
