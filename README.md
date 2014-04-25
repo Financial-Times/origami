cc0a09496f1c4d18bf090d60abd403d46bb3ba60
Rhys Evans <rhys.evans@ft.com>
2014-04-25 13:03:07 +0100

    initial implementation

--- README.md
+++ README.md
@@ -2,3 +2,42 @@ o-viewport
 ==========
 
 Utility for moderating listeners for browser events on window
+
+## Methods
+
+### `o-viewport#debug()`
+Tuns on debug mode (logging event details to the console). *Product use only*
+
+### `o-viewport#resize()`
+Attaches a debounced listener to the window resize event, which broadcasts a namespaced custom event.
+
+### `o-viewport#orientation()`
+Attaches a debounced listener to the window orientationchange event, which broadcasts a namespaced custom event.
+
+### `o-viewport#scroll()`
+Attaches a throttled listener to the window scroll event, which broadcasts a namespaced custom event.
+
+### `o-viewport#setInterval(event, interval)`
+Sets the debounce/throttle interval for a given event [`resize`, `orientation` or `scroll`]. *Product use only*
+
+## Events
+Each of these custom events are fired on `document.body` and `event.detail.originalEvent` contains a reference to the original browser event. Additiional properties in `event.detail` are detailed below:
+
+### `oViewport.resize`
+    clientHeight: body.clientHeight
+    clientWidth: body.clientWidth
+
+### `oViewport.orientation`
+
+    clientHeight: body.clientHeight
+    clientWidth: body.clientWidth
+    orientation: 'portrait' or 'landscape'
+
+### `oViewport.scroll`
+
+    clientHeight: body.clientHeight
+    clientWidth: body.clientWidth
+    scrollHeight: body.scrollHeight
+    scrollLeft: body.scrollLeft
+    scrollTop: body.scrollTop
+    scrollWidth: body.scrollWidth
---
+++ main.js
@@ -0,0 +1,110 @@
+var Delegate = require('ftdomdelegate');
+var _ = require('lodash');
+var prefixer = require('o-useragent').prefixer;
+var body;
+var debug;
+var delegate;
+var initFlags = {};
+var intervals = {
+    resize: 100,
+    orientation: 100,
+    scroll: 100
+};
+
+function broadcast (name, data) {
+    if (debug) {
+        console.log('o-viewport', name, data);
+    }
+    body.dispatchEvent(new CustomEvent('oViewport.' + eventType, {
+        detail: data,
+        bubbles: true
+    }));
+}
+
+function getOrientationListener () {
+    
+    // not using window.orientation because of http://www.matthewgifford.com/blog/2011/12/22/a-misconception-about-window-orientation/
+
+    var orientationPropName = prefixer.dom(screen, 'orientation');
+
+    if (orientationPropName) {
+        return function (ev) {
+            broadcast('orientation', {
+                clientHeight: body.clientHeight,
+                clientWidth: body.clientWidth,
+                orientation: screen[orientationPropName].split('-')[0],
+                originalEvent: ev
+            });
+        };
+    } else {
+        return function (ev) {
+            broadcast('orientation', {
+                clientHeight: body.clientHeight,
+                clientWidth: body.clientWidth,
+                orientation: body.clientHeight >= body.clientWidth ? 'portrait' : 'landscape',
+                originalEvent: ev
+            });
+        };
+    }
+}
+
+function setInterval (event, interval) {
+    intervals[event] = interval;
+}
+
+function init(event) {
+    if (initFlags[event]) return true;
+
+    initFlags[event] = true;
+
+    delegate = delegate || new Delegate(window);
+    
+    body = body || document.body;
+}
+
+function resize () {
+
+    if (init('resize')) return;
+  
+    delegate.on('resize', _.debounce(intervals.resize, function (ev) {
+        broadcast('resize', {
+            clientHeight: body.clientHeight,
+            clientWidth: body.clientWidth,
+            originalEvent: ev
+        });
+    }));
+}
+
+function orientation () {
+
+    if (init('orientation')) return;
+
+    delegate.on('orientationchange', _.debounce(intervals.orientation, getOrientationListener()));
+}
+
+function scroll () {
+
+    if (init('scroll')) return;
+
+    delegate.on('scroll', _.throttle(intervals.scroll, function (ev) {
+        broadcast('scroll', {
+            clientHeight: body.clientHeight,
+            clientWidth: body.clientWidth,
+            scrollHeight: body.scrollHeight,
+            scrollLeft: body.scrollLeft,
+            scrollTop: body.scrollTop,
+            scrollWidth: body.scrollWidth,
+            originalEvent: ev
+        });
+    }));
+}
+
+module.exports = {
+    debug: function () {
+        debug = true;
+    },
+    resize: resize,
+    orientation: orientation,
+    scroll: scroll,
+    setInterval: setInterval
+};
