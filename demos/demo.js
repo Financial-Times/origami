6066cb3d1d306ae817a2c5d0c08f2c4970349b16
Dan Searle <dan.searle@ft.com>
2014-04-25 10:29:03 +0100

    Added transparent and inherit to palette. Updated demo to exclude transparent and inherit. Updated table use cases. Various minor jshint fixes to demo.js

--- demos/demo.js
+++ demos/demo.js
@@ -1,10 +1,16 @@
-function loadDemo(showtype) {
-  var palette = {};
-  var usecases = {}, roles = [];
-  var el = document.querySelector('.'+showtype+'-results');
-  getdata('palette');
+/*global OrigamiRegistry*/
 
-  function getdata(type) {
+function loadDemo(showtype) {
+  "use strict";
+  var palette = {},
+      useCases = {},
+      roles = [],
+      el = document.querySelector('.'+showtype+'-results'),
+      paletteExclusions = ['transparent', 'inherit'];
+
+  getData('palette');
+
+  function getData(type) {
     var oReq = new XMLHttpRequest();
     oReq.open("GET", "../src/scss/_"+type+".scss", true);
     oReq.onload = function() {
@@ -15,49 +21,55 @@ function loadDemo(showtype) {
         rule = rule.replace(/^\s+/, '').replace(/\s+$/, '');
         var m = rule.split(/\s+/);
 
-        if (m && m[3] != 'DEPRECATED') {
-          if (type=='palette') {
+        if (m && m[3] !== 'DEPRECATED') {
+          if (type === 'palette') {
             palette[m[0]] = m[1];
-            if (showtype == 'palette') {
+            if (showtype === 'palette' && paletteExclusions.indexOf(m[1]) === -1) {
               el.innerHTML += '<div data-o-grid-colspan="4 XL3 L3 M6 S12" class="sample"><div class="swatch background" style="background-color:'+m[1]+'"></div><div class="name">'+m[0]+'</div><div class="descrip">'+m[1]+'</div></div>';
             }
           } else {
-            if (roles.indexOf(m[2]) === -1 && m[2] !== 'all') roles.push(m[2]);
-            usecases[m[0]] = usecases[m[0]] || {};
-            usecases[m[0]][m[2]] = m[1];
+            if (roles.indexOf(m[2]) === -1 && m[2] !== 'all') {
+                roles.push(m[2]);
+            }
+            useCases[m[0]] = useCases[m[0]] || {};
+            useCases[m[0]][m[2]] = m[1];
           }
         }
       });
-      if (type == 'palette' && showtype == 'use-cases') {
-        return getdata(showtype);
+      if (type === 'palette' && showtype === 'use-cases') {
+        return getData(showtype);
       }
-      if (showtype == 'use-cases') {
+      if (showtype === 'use-cases') {
         var op = '';
-        op = '<table><thead><tr><th>Use case</th>'
+        op = '<table><thead><tr><th>Use case</th>';
         roles.forEach(function(role) {
           op += '<th>'+role+' colour</th>';
         });
         op += '</tr></thead><tbody>';
-        for (var i in usecases) {
-          op += '<tr><td>'+i+'</td>';
-          roles.forEach(function(role) {
-            var uc = usecases[i][role] || usecases[i]['all'];
-            if (uc) {
-              op += '<td><div class="swatch o-colors-'+i+'-'+role+' o-colors-'+i+'-all '+role+'"></div><div class="descrip">'+uc+'<br/>'+palette[uc]+'</div></td>';
-            } else {
-              op += '<td></td>';
-            }
-          });
-          op += '</tr>';
+
+        for (var i in useCases) {
+          if (useCases.hasOwnProperty(i)) {
+              op += '<tr><td>'+i+'</td>';
+              roles.forEach(function(role) {
+                var uc = useCases[i][role] || useCases[i].all,
+                    descrip = (uc !== palette[uc]) ? uc + '<br/>' + palette[uc] : uc;
+                if (uc) {
+                  op += '<td><div class="swatch o-colors-'+i+'-'+role+' o-colors-'+i+'-all '+role+'"></div><div class="descrip">' + descrip + '</div></td>';
+                } else {
+                  op += '<td></td>';
+                }
+              });
+              op += '</tr>';
+          }
         }
         op += '</table>';
         el.innerHTML = op;
       }
-      if (typeof OrigamiRegistry != 'undefined') {
+      if (typeof OrigamiRegistry !== 'undefined') {
         window.addEventListener('resize', OrigamiRegistry.resize, false);
         OrigamiRegistry.resize();
       }
     };
     oReq.send();
-  };
+  }
 }
