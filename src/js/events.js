bd0f38b968cc59458845986e5ac632d323cd71fa
Rhys Evans <rhys.evans@ft.com>
2014-05-06 12:05:34 +0100

    commenting events implementation

--- src/js/events.js
+++ src/js/events.js
@@ -1,60 +1,65 @@
-// 'use strict';
+'use strict';
 
-// var Delegate = require('dom-delegate');
-// var defaults = require('lodash-node/modern/objects/defaults');
-// var ddOff = Delegate.prototype.off;
-// var ddOn = Delegate.prototype.on;
+var Delegate = require('dom-delegate');
+var defaults = require('lodash-node/modern/objects/defaults');
+var ddOff = Delegate.prototype.off;
+var ddOn = Delegate.prototype.on;
 
-// var Events = function(root, andBody) {
-//     if (andBody) {
-//         Delegate.apply(this, root);
-//         this.bodyDelegate = new Delegate(document.body);
-//     } else {
-//         return new Delegate(root);
-//     }
-// };
+var Events = function(root, andBody) {
+    // if developer specifies they need a body listener as well as the owned dom run ftdomdelegate's constructor 
+    // on this (to get ftdd's setup, but using the prototype below) and also add a subdelegate attached to body
+    if (andBody) {
+        Delegate.call(this, root);
+        this.bodyDelegate = new Delegate(document.body);
+    } else {
+        // otherwise just return an ftdomdelegate instance
+        return new Delegate(root);
+    }
+};
 
-// Events.protoype = defaults({
-//     on: function(event, selector, handler) {
-//         if (selector === document.body) {
-//             this.onBody(event, handler);
-//         } else {
-//             ddOn.apply(this, arguments);
-//         }
-//     },
 
-//     off: function(event, selector, handler) {
-//         if (event === true) {
-//             ddOff.apply(this);
-//             this.offBody();
-//         } else if (selector === document.body) {
-//             this.offBody(event, handler);
-//         } else {
-//             ddOff.apply(this, arguments);
-//         }
-//     },
+// override & extend ftdd prototype with the following
+Events.protoype = defaults({
 
-//     onBody: function(event, handler) {
-//         this.bodyDelegate.on(event, handler);
-//     },
+    on: function(event, selector, handler) {
+        // same API as ftdd but if selector is body then use the delegate that's attached to body
+        if (selector === document.body) {
+            this.bodyDelegate.on(event, handler);
+        } else {
+            ddOn.apply(this, arguments);
+        }
+    },
 
-//     offBody: function(event, handler) {
-//         this.bodyDelegate.off(event, handler);
-//     },
+    off: function(event, selector, handler) {
+        // same API as ftdd but 
+        // ... if passed true turn all events on body and owned DOm off
+        if (event === true) {
+            ddOff.apply(this);
+            this.bodyDelegate.off();
+        // ... if passed body turn only events on body off
+        } else if (selector === document.body) {
+            this.bodyDelegate.off(event, handler);
+        // ... otherwise just turn events on the owned DOM off
+        } else {
+            ddOff.apply(this, arguments);
+        }
+    },
 
-//     disable: function() {
-//         this._rootElement = this.rootElement;
-//         this.rootElement = null;
-//         this.bodyDelegate.root();
-//     },
+    disable: function() {
+        this._rootElement = this.rootElement;
+        this.rootElement = null;
+        this.bodyDelegate.root();
+    },
 
-//     enable: function() {
-//         if (!this._rootElement) return;
-//         this.rootElement = this._rootElement;
-//         this._rootElement = null;
-//         this.bodyDelegate.root(document.body);
-//     }
+    enable: function() {
+        if (!this._rootElement) {
+            return;
+        }
+        this.rootElement = this._rootElement;
+        this._rootElement = null;
+        this.bodyDelegate.root(document.body);
+    }
 
-// }, Delegate.prototype);
+}, Delegate.prototype);
 
-// module.exports = Events;
+module.exports = Events;
