5e90adf16fd2977933300441d72d9abdd3ca2950
Rhys Evans <rhys.evans@ft.com>
2014-05-12 10:40:26 +0100

    midway through adding auto updating of 'time ago' dates

--- main.js
+++ main.js
@@ -2,10 +2,13 @@
 
 var months = 'January,February,March,April,May,June,July,August,September,October,November,December'.split(',');
 var days = 'Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday'.split(',');
+var timer;
+var updaters = [];
+
 
 var formats = {
     long: 'MMMM d, yyyy h:mm a',
-    short: 'd/M/y'
+    short: 'd/M/yy'
 };
 
 /*
@@ -19,7 +22,7 @@ var formatReplacementsMap = {
         return months[this.getMonth()].substr(0,3);
     },
     MM: function () {
-        return pad(this.getMonth() + 1, 2);
+        return pad2(this.getMonth() + 1, 2);
     },
     M: function () {
         return this.getMonth() + 1;
@@ -36,40 +39,131 @@ var formatReplacementsMap = {
     },
     d: Date.prototype.getDate,
     dd: function () {
-        return pad(this.getDate() + 1, 2);
+        return pad2(this.getDate() + 1, 2);
     },
     m: Date.prototype.getMinutes,
     mm: function () {
-        return pad(this.getMinutes(), 2);
+        return pad2(this.getMinutes(), 2);
     },
     h: function () {
         return (this.getHours() % 12) - 1;
     },
     hh: function () {
-        return pad((this.getHours() % 12) - 1, 2);
+        return pad2((this.getHours() % 12) - 1, 2);
     },
     a: function () {
         return this.getHours() >= 12 ? 'pm' : 'am';
     }
 };
 
-function pad(number, chars) {
-    number = ''+number;
-    while(number.length < chars) {
-        number = '0' + number;
-    }
-    return number;
+function pad2 (number) {
+    return ('0' + number).slice(-2);
+}
+
+function compileTemplate (tpl) {
+
+}
+
+function toDate (date) {
+    return date instanceof Date ? date : new Date(date);
 }
 
 function format (date, tpl) {
     tpl = formats[tpl] || tpl;
-    date = date instanceof Date ? date : new Date(date);
+    date = toDate(date);
     return tpl.replace(/[a-z]+/ig, function (match) {
         var replacer = formatReplacementsMap[match];
         return replacer ? replacer.apply(date) : match;
     });
 }
 
+function timeAgo (date, fallback) {
+    date = toDate(date);
+    var interval = (new Date()) - date();
+    if (interval < 30 * 1000) {
+        // just now
+    } else if (interval < 90 * 1000) {
+        // minute
+    } else if (interval < 55 * 60 * 1000) {
+        // minutes
+    } else {
+        return fallback ? format(date, fallback) : '';
+    }
+}
+
+
+function watch (container, date) {
+    container = container || document.body;
+    if (container.classList.contains('o-date-updater')) {
+        date = date || container.getAttribute('datetime') || container.dataset.date;
+        createUpdater(container, date);
+    } else {
+        Array.prototype.forEach.apply(container.querySelectorAll('.o-date-updater'), function (el) {
+            createUpdater(el, el.getAttribute('datetime') || el.dataset.date);
+        });
+    }
+}
+
+function pause (container, destroy) {
+    // container = container || document.body;
+    // if (container.classList.contains('o-date-updater')) {
+    //     date = date || container.getAttribute('datetime') || container.dataset.date;
+    //     createUpdater(container, date);
+    // } else {
+    //     Array.prototype.forEach.apply(container.querySelectorAll('.o-date-updater'), function (el) {
+    //         createUpdater(el, el.getAttribute('datetime') || el.dataset.date);
+    //     });
+    // }
+}
+
+function unwatch (container, destroy) {
+    // container = container || document.body;
+    // if (container.classList.contains('o-date-updater')) {
+    //     date = date || container.getAttribute('datetime') || container.dataset.date;
+    //     createUpdater(container, date);
+    // } else {
+    //     Array.prototype.forEach.apply(container.querySelectorAll('.o-date-updater'), function (el) {
+    //         createUpdater(el, el.getAttribute('datetime') || el.dataset.date);
+    //     });
+    // }
+}
+
+function refresh () {
+    updaters.forEach(function (u) {
+        u.update();
+    });
+    return setTimeout(refresh, 60000);
+}
+
+function createUpdater (el, date) {
+    timer = timer || refresh();
+    updaters.push(new Updater(el, date));
+}
+
+function Updater (el, date) {
+    this.date = toDate(date);
+    this.el = el;
+    this.init();
+}
+
+Updater.prototype = {
+    init: function () {
+        this.fallback = this.el.dataset.oDateUpdateFallback || 'long';
+    },
+    update: function () {
+        if (!this.paused) {
+            this.printer.innerHTML = this.timeAgo();
+        }
+    },
+    timeAgo: function () {
+        return timeAgo(this.date, this.fallback);
+    }
+};
+
 module.exports = {
-    format: format
+    format: format,
+    watch: watch,
+    unwatch: unwatch,
+    pause: pause,
+    timeAgo: timeAgo
 };
