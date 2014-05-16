05f444158f42051d519f2c2f2c59190ac084e59e
Rhys Evans <rhys.evans@ft.com>
2014-05-16 15:37:53 +0100

    Added CI and demo

---
+++ demos/src/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    options: {
+        js: 'demos/src/demo.js'
+    },
+    demos: {
+        'dates': {
+            template: 'demos/src/dates.mustache'
+        }
+    }
+};
---
+++ demos/src/demo.js
@@ -0,0 +1,13 @@
+var times = document.querySelectorAll('time.o-date');
+
+var now = new Date();
+var today  = new Date();
+today.setHours(now.getHours() - 6);
+var lastMonth = new Date();
+lastMonth.setMonth(now.getMonth() - 6);
+
+times[0].setAttribute('datetime', today.toISOString());
+times[1].setAttribute('datetime', lastMonth.toISOString());
+
+
+require('../../main.js').init();
