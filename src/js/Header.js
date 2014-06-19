5ee42049c7ef028b6b502b99470594cbed1626aa
Alberto Elias <me@alberto-elias.com>
2014-06-19 17:03:36 +0100

    Added nav themes

--- src/js/Header.js
+++ src/js/Header.js
@@ -1,33 +1,33 @@
 /*global require,module*/
 
 var DomDelegate = require('dom-delegate'),
-    ResponsiveNav = require('o-hierarchical-nav');
+    oHierarchicalNav = require('o-hierarchical-nav');
 
 function Header(rootEl) {
     "use strict";
 
     var bodyDelegate,
-        responsiveNavEls = [
-            rootEl.querySelector('.o-hierarchical-nav__primary-theme'),
-            rootEl.querySelector('.o-hierarchical-nav__secondary-theme'),
-            rootEl.querySelector('.o-hierarchical-nav__tools-theme')
+        hierarchicalNavEls = [
+            rootEl.querySelector('.o-ft-header__nav--primary-theme'),
+            rootEl.querySelector('.o-ft-header__nav--secondary-theme'),
+            rootEl.querySelector('.o-ft-header__nav--tools-theme')
         ].filter(function(el) {
             return el && el.nodeType === 1;
         }),
-        responsiveNavs = [];
+        hierarchicalNavs = [];
 
     function init() {
         bodyDelegate = new DomDelegate(document.body);
-        responsiveNavs = responsiveNavEls.map(function(el) {
-            return new ResponsiveNav(el);
+        hierarchicalNavs = hierarchicalNavEls.map(function(el) {
+            return new oHierarchicalNav(el);
         });
     }
 
     function destroy() {
         bodyDelegate.destroy();
-        for (var c = 0, l = responsiveNavs.length; c < l; c++) {
-            if (responsiveNavs[c]) {
-                responsiveNavs[c].destroy();
+        for (var c = 0, l = hierarchicalNavs.length; c < l; c++) {
+            if (hierarchicalNavs[c]) {
+                hierarchicalNavs[c].destroy();
             }
         }
     }
