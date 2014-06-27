0369c62492fe4b463c605cb3ec61ec8f560b7a56
Dan Searle <dan.searle@ft.com>
2014-06-27 14:09:21 +0100

    Update dependencies. Use o-hoverable. scss-lint fixes. Update demo. Update origami.json

--- demos/src/config.js
+++ demos/src/config.js
@@ -5,9 +5,10 @@ module.exports = {
           'o-ft-footer': JSON.parse(require('fs').readFileSync(process.cwd() + '/footer.json', {encoding: 'utf8'}))
         }
     },
-    demos: {
-        'ft-footer': {
+    demos: [
+        {
+            name: 'ft-footer',
             template: 'main.mustache'
         }
-    }
+    ]
 };
