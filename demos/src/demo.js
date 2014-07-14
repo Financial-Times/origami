e6f34d857cb09355cd430d4d37e697254330ca01
Alberto Elias <me@alberto-elias.com>
2014-07-14 16:17:37 +0100

    Fixed megadropdown demo on mobile, hid abbr logo text, more menu with featured

--- demos/src/demo.js
+++ demos/src/demo.js
@@ -3,6 +3,6 @@ require('../../main.js');
 
 document.addEventListener("DOMContentLoaded", function() {
     "use strict";
-    console.log(CustomEvent);
+
     document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
 });
