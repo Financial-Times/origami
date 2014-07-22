9e86269e9e482246630fdd68d62a3514940eb51a
Alberto Elias <me@alberto-elias.com>
2014-07-22 16:49:55 +0100

    Added globals

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*global require, module*/
 'use strict';
 
 var months = '["' + 'January,February,March,April,May,June,July,August,September,October,November,December'.split(',').join('","') + '"]';
