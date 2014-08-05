6df7b23b8cf6372e8caca240943c2f8c6333819b
Alberto Elias <me@alberto-elias.com>
2014-08-05 13:11:27 +0100

    Fixed ifs

--- main.js
+++ main.js
@@ -73,12 +73,12 @@ function autoUpdate () {
 function ftTime(el) {
     var date = el.getAttribute('datetime');
     // Keep date as null if datetime hasn't been defined
-    date = date ? toDate(date) : date;
+    if (date) date = toDate(date);
     var printer = el.querySelector('.o-date__printer') || el;
     // If date hasn't been defined and printer isn't empty, keep the content of the printer
     if (date || (!date && printer.innerHTML.length === 0)) {
         // If date hasn't been defined, and the printer is empty, we'll set the default to be the current text
-        date = date ? date : new Date();
+        if (!date) date = new Date();
         var interval = Math.round(((new Date()) - date) / 1000);
         printer.innerHTML = interval < (365 * 60 * 60 * 24) ? timeAgo(date, interval) : format(date, 'date');
     }
