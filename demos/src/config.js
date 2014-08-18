5b7b23151d80eed82a351305e6fe221c84924648
Andrew Betts <andrew.betts@ft.com>
2014-08-18 15:57:44 +0100

    Completed footer implementation

--- demos/src/config.js
+++ demos/src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     options: {
-        sass: 'demos/src/demo.scss',
+        sass: 'main.scss',
         data: {
           'o-ft-footer': JSON.parse(require('fs').readFileSync(process.cwd() + '/footer.json', {encoding: 'utf8'}))
         }
