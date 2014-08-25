132a43e618df9cca17bdc67252face805496c3fd
Andrew Betts <andrew.betts@ft.com>
2014-08-25 19:53:01 +0100

    Fix demo load path for registry

--- demos/src/demo.js
+++ demos/src/demo.js
@@ -12,7 +12,7 @@ function loadDemo(showtype) {
 
   function getData(type) {
     var oReq = new XMLHttpRequest();
-    oReq.open("GET", "../../src/scss/_"+type+".scss", true);
+    oReq.open("GET", ((location.pathname.indexOf('/local') !== -1) ? "../" : "") + "../src/scss/_"+type+".scss", true);
     oReq.onload = function() {
       var src = this.responseText;
       var m = src.match(/\$[\w\-]+\:\s*\n([^;]+);/);
