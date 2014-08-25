6bea5f9dd36644cffed6459b0e0bf1f78feaca3b
Andrew Betts <andrew.betts@ft.com>
2014-08-25 12:49:11 +0100

    Improve demos

--- demos/src/demo.js
+++ demos/src/demo.js
@@ -5,14 +5,14 @@ function loadDemo(showtype) {
   var palette = {},
       useCases = {},
       roles = [],
-      el = document.querySelector('.'+showtype+'-results'),
+      el = document.getElementById('results'),
       paletteExclusions = ['transparent', 'inherit'];
 
   getData('palette');
 
   function getData(type) {
     var oReq = new XMLHttpRequest();
-    oReq.open("GET", "../src/scss/_"+type+".scss", true);
+    oReq.open("GET", "../../src/scss/_"+type+".scss", true);
     oReq.onload = function() {
       var src = this.responseText;
       var m = src.match(/\$[\w\-]+\:\s*\n([^;]+);/);
@@ -24,7 +24,7 @@ function loadDemo(showtype) {
           if (type=='palette') {
             palette[m[0]] = m[1];
             if (showtype === 'palette' && paletteExclusions.indexOf(m[1]) === -1) {
-              el.innerHTML += '<div data-o-grid-colspan="4 XL3 L3 M6 S12" class="sample"><div class="swatch background" style="background-color:'+m[1]+'"></div><div class="name">'+m[0]+'</div><div class="descrip">'+m[1]+'</div></div>';
+              el.innerHTML += '<div data-o-grid-colspan="4 XL3 L3 M6 S12" class="sample"><div class="swatch o-colors-palette-'+m[0]+' background"></div><span class="name">'+m[0]+'</span><span class="descrip">'+m[1]+'</span></div>';
             }
           } else {
             if (roles.indexOf(m[2]) === -1 && m[2] !== 'all') {
@@ -40,29 +40,21 @@ function loadDemo(showtype) {
       }
       if (showtype === 'use-cases') {
         var op = '';
-        op = '<table><thead><tr><th>Use case</th>';
-        roles.forEach(function(role) {
-          op += '<th>'+role+' colour</th>';
-        });
-        op += '</tr></thead><tbody>';
-
         for (var i in useCases) {
           if (useCases.hasOwnProperty(i)) {
-              op += '<tr><td>'+i+'</td>';
-              roles.forEach(function(role) {
-                var uc = useCases[i][role] || useCases[i].all,
-                    descrip = (uc !== palette[uc]) ? uc + '<br/>' + palette[uc] : uc;
-                if (uc) {
-                  op += '<td><div class="swatch o-colors-'+i+'-'+role+' o-colors-'+i+'-all '+role+'"></div><div class="descrip">' + descrip + '</div></td>';
-                } else {
-                  op += '<td></td>';
-                }
-              });
-              op += '</tr>';
+            op += '<div data-o-grid-colspan="4 XL3 L4 M6 S12" class="sample"><div class="swatch ';
+            var tips = [];
+            roles.forEach(function(role) {
+              var uc = useCases[i][role] || useCases[i].all;
+              if (uc) {
+                op += 'o-colors-'+i+'-'+role+' o-colors-'+i+'-all '+role+' ';
+                tips.push(role+': '+uc);
+              }
+            });
+            op += '" title="'+tips.join(', ')+'"></div><span class="name">'+i+'</span></div>';
           }
         }
-        op += '</table>';
-        el.innerHTML = op;
+        el.innerHTML += op;
       }
       if (typeof OrigamiRegistry !== 'undefined') {
         window.addEventListener('resize', OrigamiRegistry.resize, false);
@@ -72,3 +64,7 @@ function loadDemo(showtype) {
     oReq.send();
   }
 }
+
+window.addEventListener('DOMContentLoaded', function() {
+  loadDemo(document.getElementById('results').getAttribute('data-demo'));
+}, false);
