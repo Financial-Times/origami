655024d47e7bff439d428e6a4ffe7f40ca6b424c
Kaelig <kaelig@deloumeau.fr>
2014-11-19 18:11:17 +0000

    Fixes to pass linting

--- demos/src/config.js
+++ demos/src/config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    options: {
-        sass: 'main.scss',
-        data: {
-          'o-ft-footer': JSON.parse(require('fs').readFileSync(process.cwd() + '/footer.json', {encoding: 'utf8'}))
-        }
-    },
-    demos: [
-        {
-            name: 'ft-footer',
-            template: 'main.mustache'
-        }
-    ]
+	options: {
+		sass: 'main.scss',
+		data: {
+			'o-ft-footer': JSON.parse(require('fs').readFileSync(process.cwd() + '/footer.json', {encoding: 'utf8'}))
+		}
+	},
+	demos: [
+		{
+			name: 'ft-footer',
+			template: 'main.mustache'
+		}
+	]
 };
