662e2db36605a198c11386a4b504ae312a95991f
Kaelig <kaelig@deloumeau.fr>
2014-11-19 18:29:36 +0000

    Fixes to pass linting

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 /*global require,module*/
 'use strict';
 
-var oHeader = require('./src/js/Header'),
-    constructAll = function() {
-        oHeader.init();
-        document.removeEventListener('o.DOMContentLoaded', constructAll);
-    };
+var oHeader = require('./src/js/Header');
+var	constructAll = function() {
+	oHeader.init();
+	document.removeEventListener('o.DOMContentLoaded', constructAll);
+};
 
 document.addEventListener('o.DOMContentLoaded', constructAll);
 
