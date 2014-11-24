f9fa77b62ae756bd3401f3b4fed9ae772e7e4b99
Paul I Wilson <paul.i.wilson@ft.com>
2014-11-24 17:57:37 +0000

    fixing linting errors

--- demos/src/demo.js
+++ demos/src/demo.js
@@ -1,31 +1,30 @@
 /*global require*/
 
 (function() {
-    "use strict";
+	"use strict";
 
-    require('./../../main').wrap();
+	require('./../../main').wrap();
 
-    var tableEls,
-        checkboxEls;
+	var tableEls,
+		checkboxEls;
 
-    function checkboxChangeHandler(ev) {
-        var cssClass = ev.target.getAttribute('data-switch-class'),
-            c,
-            l;
-        for (c = 0, l = tableEls.length; c < l; c++) {
-            if (ev.target.checked) {
-                tableEls[c].classList.add(cssClass);
-            } else {
-                tableEls[c].classList.remove(cssClass);
-            }
-        }
-    }
+	function checkboxChangeHandler(ev) {
+		var cssClass = ev.target.getAttribute('data-switch-class'),
+			c,
+			l;
+		for (c = 0, l = tableEls.length; c < l; c++) {
+			if (ev.target.checked) {
+				tableEls[c].classList.add(cssClass);
+			} else {
+				tableEls[c].classList.remove(cssClass);
+			}
+		}
+	}
 
-    tableEls = document.querySelectorAll('.o-table');
-    checkboxEls = document.querySelectorAll('input[type=checkbox]');
+	tableEls = document.querySelectorAll('.o-table');
+	checkboxEls = document.querySelectorAll('input[type=checkbox]');
 
-    for (var c = 0, l = checkboxEls.length; c < l; c++) {
-        checkboxEls[c].addEventListener('change', checkboxChangeHandler, false);
-    }
-
+	for (var c = 0, l = checkboxEls.length; c < l; c++) {
+		checkboxEls[c].addEventListener('change', checkboxChangeHandler, false);
+	}
 })();
