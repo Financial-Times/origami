f94b37847845159e1f3825b173fefaec2a312506
Kaelig <kaelig@deloumeau.fr>
2014-11-28 12:17:23 +0000

    Spacing tweaks in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,19 +7,19 @@ module.exports = function(grunt) {
 	var path = require('path');
 
 	require('load-grunt-config')(grunt, {
-			configPath: path.join(process.cwd(), 'grunt-config'),
-			config: {
-					pkg: grunt.file.readJSON('package.json'),
-					bwr: grunt.file.readJSON('bower.json'),
-					o: grunt.file.readJSON('origami.json')
-			},
-			loadGruntTasks: {
-				pattern: ['grunt-*', '!grunt-template-jasmine-istanbul']
-			}
+		configPath: path.join(process.cwd(), 'grunt-config'),
+		config: {
+			pkg: grunt.file.readJSON('package.json'),
+			bwr: grunt.file.readJSON('bower.json'),
+			o: grunt.file.readJSON('origami.json')
+		},
+		loadGruntTasks: {
+			pattern: ['grunt-*', '!grunt-template-jasmine-istanbul']
+		}
 	});
 
 	grunt.loadTasks('grunt-tasks');
- 
+
 	// By default, lint and run all tests.
 	grunt.registerTask('default', ['test']);
 
