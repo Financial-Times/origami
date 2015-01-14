3b89c6f79199da5a2723a77a406a86556797c6f0
Alberto Elias <me@alberto-elias.com>
2015-01-14 13:31:26 +0000

    Some fixes @kaelig suggested

--- src/js/overlay.js
+++ src/js/overlay.js
@@ -228,30 +228,6 @@ Overlay.prototype = {
 		this.broadcast('ready');
 	},
 
-	realign: function(dimension, size) {
-		var edge = dimension === 'width' ? 'left' : 'top';
-
-		if (size <= this[dimension]) {
-			if (dimension === 'height') {
-				// Set the exact height that the content of the overlay will have which is the total
-				// height of the overlay minus the heading if there is one. If height = 100%, the
-				// heading is part of that 100%, so some content is truncated.
-				this.content.style.height = this.wrapper.clientHeight - this.wrapper.querySelector('header').offsetHeight + 'px';
-			}
-			this.wrapper.classList.add('o-overlay--full-' + dimension);
-			this.wrapper.style['margin' + utils.capitalise(edge)] = 0;
-		} else {
-			if (dimension === 'height') {
-				// Remove the property and let the overlay extend to its content
-				this.content.style.height = null;
-			}
-			this.wrapper.classList.remove('o-overlay--full-' + dimension);
-			if (!this.opts.arrow) {
-				this.wrapper.style['margin' + utils.capitalise(edge)] = -(this.wrapper['offset' + utils.capitalise(dimension)]/2) + 'px';
-			}
-		}
-	},
-
 	close: function() {
 		this.delegates.doc.off();
 		this.delegates.wrap.off();
@@ -309,6 +285,30 @@ Overlay.prototype = {
 			// Don't bubble above the overlay's layer context otherwise we risk triggering a listener on a parent context
 			bubbles: namespace !== 'oLayers' ? true : false
 		}));
+	},
+
+	realign: function(dimension, size) {
+		var edge = dimension === 'width' ? 'left' : 'top';
+
+		if (size <= this[dimension]) {
+			this.wrapper.classList.add('o-overlay--full-' + dimension);
+			this.wrapper.style['margin' + utils.capitalise(edge)] = 0;
+			if (dimension === 'height') {
+				// Set the exact height that the content of the overlay will have which is the total
+				// height of the overlay minus the heading if there is one. If height = 100%, the
+				// heading is part of that 100%, so some content is truncated.
+				this.content.style.height = this.wrapper.clientHeight - this.wrapper.querySelector('header').offsetHeight + 'px';
+			}
+		} else {
+			if (dimension === 'height') {
+				// Remove the property and let the overlay extend to its content
+				this.content.style.height = null;
+			}
+			this.wrapper.classList.remove('o-overlay--full-' + dimension);
+			if (!this.opts.arrow) {
+				this.wrapper.style['margin' + utils.capitalise(edge)] = -(this.wrapper['offset' + utils.capitalise(dimension)]/2) + 'px';
+			}
+		}
 	},
 
 	respondToWindow: function(size) {
