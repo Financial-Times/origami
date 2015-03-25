2c652fc67898cb8f908105e391611ec6a007b4df
Alberto Elias <me@alberto-elias.com>
2015-03-25 15:46:42 +0000

    Passes verify

--- test/browser/tests/overlayWithArrowTest.js
+++ test/browser/tests/overlayWithArrowTest.js
@@ -30,25 +30,21 @@ module.exports = {
 		var button_top;
 		var button_bottom;
 		var arrow_top;
-		var arrow_bottom;
-		var overlay_top;
 
 		browser
 			.isVisible(".o-overlay__arrow-top", function(result){
-				this.assert.equal(result.value,true,"Overlay arrow appeared")
+				this.assert.equal(result.value,true,"Overlay arrow appeared");
 			})
 			.getLocation(".o-overlay-trigger", function(location){
 				button_top = location.value.y;
 				browser.getElementSize(".o-overlay-trigger", function(size){
 					button_bottom = button_top + size.value.height;
-					console.log("button_bottom: " + button_bottom);
-				})
+				});
 			})
 			.getLocation(".o-overlay__arrow-top", function(location){
 				arrow_top = location.value.y;
-				console.log("arrow_top: " + arrow_top);
 				browser.assert.ok(arrow_top >= button_bottom, "Overlay with arrow appears below the trigger button");
-			})
+			});
 	},
 
 	// Module should render in the middle of the page.  Since we know this browser is set
@@ -68,8 +64,8 @@ module.exports = {
 			.getElementSize(".o-overlay--compact", function(size){
 				overlay_right = overlay_left + size.value.width;
 				browser.assert.ok(overlay_left>window_first_third, "Left side of overlay appears centered on the page");
-				browser.assert.ok(overlay_right<window_second_third, "Right side of overlay appears centered on the page")
-			})
+				browser.assert.ok(overlay_right<window_second_third, "Right side of overlay appears centered on the page");
+			});
 	},
 
 	'Step 5: Close the overlay': function(browser){
