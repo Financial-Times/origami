903ab80ab16d7a5e6ccda6a1f8ba09584d95be17
Kaelig <kaelig@deloumeau.fr>
2015-03-31 11:19:09 +0100

    Make tests pass linting

--- test/ShareLinks.test.js
+++ test/ShareLinks.test.js
@@ -4,61 +4,61 @@
 require('./helpers/polyfill');
 
 var fixtures = require('./helpers/fixtures'),
-    triggerEvent = require('./helpers/triggerEvent'),
-    ShareLinks = require('./../main'),
-    testShareLinks,
-    shareLinksEl;
+	triggerEvent = require('./helpers/triggerEvent'),
+	ShareLinks = require('./../main'),
+	testShareLinks,
+	shareLinksEl;
 
 describe("general behaviour", function() {
 
-    beforeEach(function(){
-        fixtures.insertShareLinks();
-        shareLinksEl = document.querySelector('[data-o-component=o-share]');
-        testShareLinks = new ShareLinks(shareLinksEl);
-    });
+	beforeEach(function(){
+		fixtures.insertShareLinks();
+		shareLinksEl = document.querySelector('[data-o-component=o-share]');
+		testShareLinks = new ShareLinks(shareLinksEl);
+	});
 
-    afterEach(function() {
-        testShareLinks.destroy();
-        fixtures.reset();
-    });
+	afterEach(function() {
+		testShareLinks.destroy();
+		fixtures.reset();
+	});
 
-    it("is defined", function() {
-        expect(testShareLinks).toBeDefined();
-    });
+	it("is defined", function() {
+		expect(testShareLinks).toBeDefined();
+	});
 
-    it("initialisation", function() {
-        expect(shareLinksEl.hasAttribute('data-o-share--js')).toBe(true);
-    });
+	it("initialisation", function() {
+		expect(shareLinksEl.hasAttribute('data-o-share--js')).toBe(true);
+	});
 
 });
 
 describe("general behaviour", function() {
 
-    var twitterLinkEl;
+	var twitterLinkEl;
 
-    beforeEach(function(){
-        fixtures.insertShareLinks();
-        shareLinksEl = document.querySelector('[data-o-component=o-share]');
-        testShareLinks = new ShareLinks(shareLinksEl);
-        spyOn(window, 'open').andCallThrough();
-        twitterLinkEl = document.querySelector('.o-share__action--twitter a');
-        triggerEvent(twitterLinkEl, 'click');
-    });
+	beforeEach(function(){
+		fixtures.insertShareLinks();
+		shareLinksEl = document.querySelector('[data-o-component=o-share]');
+		testShareLinks = new ShareLinks(shareLinksEl);
+		spyOn(window, 'open').andCallThrough();
+		twitterLinkEl = document.querySelector('.o-share__action--twitter a');
+		triggerEvent(twitterLinkEl, 'click');
+	});
 
-    afterEach(function() {
-        testShareLinks.destroy();
-        fixtures.reset();
-    });
+	afterEach(function() {
+		testShareLinks.destroy();
+		fixtures.reset();
+	});
 
-    it("clicking link opens new window", function() {
-        expect(window.open).toHaveBeenCalledWith(twitterLinkEl.getAttribute('href'), twitterLinkEl.getAttribute('href'), 'width=646,height=436');
-    });
+	it("clicking link opens new window", function() {
+		expect(window.open).toHaveBeenCalledWith(twitterLinkEl.getAttribute('href'), twitterLinkEl.getAttribute('href'), 'width=646,height=436');
+	});
 
-    it("clicking link opens new window only once", function() {
-        expect(window.open.calls.length).toEqual(1);
-        expect(window.open).toHaveBeenCalledWith(twitterLinkEl.getAttribute('href'), twitterLinkEl.getAttribute('href'), 'width=646,height=436');
-        triggerEvent(twitterLinkEl, 'click');
-        expect(window.open.calls.length).toEqual(1);
-    });
+	it("clicking link opens new window only once", function() {
+		expect(window.open.calls.length).toEqual(1);
+		expect(window.open).toHaveBeenCalledWith(twitterLinkEl.getAttribute('href'), twitterLinkEl.getAttribute('href'), 'width=646,height=436');
+		triggerEvent(twitterLinkEl, 'click');
+		expect(window.open.calls.length).toEqual(1);
+	});
 
 });
