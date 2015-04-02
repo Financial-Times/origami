7dbf766f542a27c47405bffae01ca4859ff01314
SamGiles <samuel.giles@ft.com>
2015-04-02 17:14:19 +0100

    Add auto initialisation method - if it fails, do nothing - deferring to the imperative init method

--- main.js
+++ main.js
@@ -8,6 +8,23 @@
 var Errors = require('./src/js/oErrors');
 var errors = new Errors();
 
+
+function initialise() {
+	if (!errors.initialised) {
+		try {
+			errors.init();
+		} catch(e) {
+			// Do nothing
+		}
+	}
+
+	document.removeEventListener('o.DOMContentLoaded', initialise);
+}
+
+// Try and initialise on o.DOMContentLoaded. If it fails, defer to the
+// consumer of the library.
+document.addEventListener('o.DOMContentLoaded', initialise);
+
 /**
  * A constructed object, this module is a Singleton due to the architecture of
  * Raven JS. See {@link Errors} for the publicly available interface.
