e53c703c8b0415d5b5b19f7d8826e563c3dc9efb
Alberto Elias <me@alberto-elias.com>
2015-04-24 13:07:49 +0200

    Merges master and fixes linting

--- test/browser/tests/modalTest.js
+++ test/browser/tests/modalTest.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 module.exports = {
 
@@ -54,31 +54,29 @@ module.exports = {
 			});
 	},
 
-	'Step 5: Verify scrollbars appear when page is shortened' : function (browser) {
+	'Step 5: Verify scrollbars appear when page is shortened' : function(browser) {
 		var scrollHeight;
 		var clientHeight;
-        if(!(browser.capabilities.browserName == "internet explorer" && browser.capabilities.version == 8)) {
-            browser
-                .windowSize('current', 800, 400, function () {
-                    browser.waitForElementVisible("button.o-overlay-trigger", 5000);
-                    browser.execute(function () {
-                        return document.getElementsByClassName("o-overlay__content")[0].scrollHeight;
-                    }, [], function (result) {
-                        scrollHeight = result.value;
-                    });
-                    browser.execute(function () {
-                        return document.getElementsByClassName("o-overlay__content")[0].clientHeight;
-                    }, [], function (result) {
-                        clientHeight = result.value;
-                        //console.log(JSON.stringify(scrollHeight));
-                        //console.log(JSON.stringify(clientHeight));
-                        browser.assert.ok(scrollHeight > clientHeight, "When the windows became smaller than the overlay content, the overlay did not appear scrollable");
-                    });
-                }
-            );
-        }else{
-            console.log("Running IE8...skipping this test");
-        }
+		if (!(browser.capabilities.browserName === "internet explorer" && browser.capabilities.version === 8)) {
+			browser
+				.windowSize('current', 800, 400, function () {
+					browser.waitForElementVisible("button.o-overlay-trigger", 5000);
+					browser.execute(function () {
+						return document.getElementsByClassName("o-overlay__content")[0].scrollHeight;
+					}, [], function (result) {
+						scrollHeight = result.value;
+					});
+					browser.execute(function () {
+						return document.getElementsByClassName("o-overlay__content")[0].clientHeight;
+					}, [], function (result) {
+						clientHeight = result.value;
+						//console.log(JSON.stringify(scrollHeight));
+						//console.log(JSON.stringify(clientHeight));
+						browser.assert.ok(scrollHeight > clientHeight, "When the windows became smaller than the overlay content, the overlay did not appear scrollable");
+					});
+				}
+			);
+		}
 	},
 
 	'Step 6: Close the overlay': function(browser){
