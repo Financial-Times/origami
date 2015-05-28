6406b0136053e4ec8c11b729662db6aa931a3437
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-05-28 17:09:05 +0100

    more tests

---
+++ tests/models/video.spec.js
@@ -0,0 +1,32 @@
+/* global describe, it, beforeEach, afterEach */
+'use strict';
+
+var Video = require('../../src/models/video');
+
+describe('Video', function () {
+
+	var videoEl;
+
+	beforeEach(function () {
+		videoEl = document.createElement('div');
+		videoEl.setAttribute('data-next-video-id', '1234567890');
+		videoEl.setAttribute('data-trackable', 'video');
+		document.body.appendChild(videoEl);
+	});
+
+	afterEach(function () {
+		document.body.removeChild(videoEl);
+	});
+
+	it('should exist', function () {
+		Video.should.exist;
+	});
+
+	it('should be able to instantiate', function () {
+		var video = new Video(videoEl);
+		video.should.exist;
+		video.domPathTokens.should.eql(['video']);
+		video.domPath.should.equal('video');
+	});
+
+});
