dcaf8237cdb1d1abd0687aa6f853639cecbd74fc
Arjun Gadhia <arjungadhia@gmail.com>
2015-06-05 11:52:48 +0100

    Fix tests

--- src/js/template.js
+++ src/js/template.js
@@ -6,7 +6,7 @@ module.exports = function(options){
 	return `
 	<section class="n-notification n-notification--js n-notification--${options.type}" data-trackable="${options.trackable}">
 		<div class="n-notification__content-wrapper">` +
-			options.title ? `<h3 class="n-notification__title">${options.title}</h3>` : `` +
+			(options.title ? `<h3 class="n-notification__title">${options.title}</h3>` : ``) +
 			`<div class="n-notification__content">${options.content}</div>
 		</div>
 		<button class="n-notification__close n-notification__close-js" data-trackable="close">Close</button>
