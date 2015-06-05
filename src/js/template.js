708abba85cf7a2992ff7b35a86a7da5229bbe806
Arjun Gadhia <arjungadhia@gmail.com>
2015-06-05 11:15:47 +0100

    Remove title html if no title passed, and fix padding for small screens

--- src/js/template.js
+++ src/js/template.js
@@ -5,16 +5,11 @@
 module.exports = function(options){
 	return `
 	<section class="n-notification n-notification--js n-notification--${options.type}" data-trackable="${options.trackable}">
-		<div class="n-notification__content-wrapper">
-			<h3 class="n-notification__title">
-				${options.title}
-			</h3>
-			<div class="n-notification__content">
-				${options.content}
-			</div>
+		<div class="n-notification__content-wrapper">` +
+			options.title ? `<h3 class="n-notification__title">${options.title}</h3>` : `` +
+			`<div class="n-notification__content">${options.content}</div>
 		</div>
 		<button class="n-notification__close n-notification__close-js" data-trackable="close">Close</button>
-
 	</section>
 `;
 };
