9ffe423c42b6e8a971126f5f490392c9e852b279
Keith Rogers <git@keithrogers.eu>
2015-06-11 13:42:36 +0100

    Bit more defensive demos

--- demos/src/main.js
+++ demos/src/main.js
@@ -1,35 +1,48 @@
 'use strict';
 
 var nNotification = require('../../main');
+var demoEvent = document.querySelector('.demo-notification--event');
+var demoMethod = document.querySelector('.demo-notification--method');
+var demoAlwaysOpen = document.querySelector('.demo-notification--always-open');
+var demoCustom = document.querySelector('.demo-notification--custom');
+
 nNotification.init();
 
-document.querySelector('.demo-notification--event').addEventListener('click', function() {
-	var event = new CustomEvent('nNotification.show', {detail:{
-			content:'Notification generated via event',
-			trackable:'event-notification'
-		}
+if (demoEvent) {
+	demoEvent.addEventListener('click', function() {
+		var event = new CustomEvent('nNotification.show', {detail:{
+				content:'Notification generated via event',
+				trackable:'event-notification'
+			}
+		});
+		document.dispatchEvent(event);
 	});
-	document.dispatchEvent(event);
-});
+}
 
-document.querySelector('.demo-notification--method').addEventListener('click', function() {
-	nNotification.show({
-		title: 'Title',
-		content: 'Notification generated via nNotification.show method'
+if (demoMethod) {
+	demoMethod.addEventListener('click', function() {
+		nNotification.show({
+			title: 'Title',
+			content: 'Notification generated via nNotification.show method'
+		});
 	});
-});
+}
 
-document.querySelector('.demo-notification--always-open').addEventListener('click', function() {
-	nNotification.show({
-		content: '<abbr title="myFT" class="myft-ui__icon"></abbr> This Notification will not close. Read and manage your alerts.',
-		duration: 0
+if (demoAlwaysOpen) {
+	demoAlwaysOpen.addEventListener('click', function() {
+		nNotification.show({
+			content: '<abbr title="myFT" class="myft-ui__icon"></abbr> This Notification will not close. Read and manage your alerts.',
+			duration: 0
+		});
 	});
-});
+}
 
-document.querySelector('.demo-notification--custom').addEventListener('click', function() {
-	nNotification.show({
-		title: 'Custom',
-		content: 'This Notification has custom styles',
-		type: 'custom'
+if (demoCustom) {
+	demoCustom.addEventListener('click', function() {
+		nNotification.show({
+			title: 'Custom',
+			content: 'This Notification has custom styles',
+			type: 'custom'
+		});
 	});
-});
+}
