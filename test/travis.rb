d495d3102763979109df075e9d006a253b2d2998
Kaelig <kaelig@deloumeau.fr>
2015-06-12 12:35:07 +0100

    Add tests for overriding use cases and switch to node-sass

--- test/travis.rb
+++ test/travis.rb
@@ -14,15 +14,18 @@ end
 FileUtils.mkdir_p "test/output"
 
 # Attempting to use an undefined color generates a warning
-stdout, stderr, status = Open3.capture3 "sass --scss test/undefined-colors-warn.scss test/output/undefined-colors-warn.css --style compressed --sourcemap=none"
+stdout, stderr, status = Open3.capture3 "node-sass test/test.scss test/output/test.css --output-style compressed"
 puts "Test: using undefined colors don't throw fatal errors…"
 raise "Using an undefined color shouldn't fail build" unless status.success?
 raise "Using an undefined color should throw warnings" unless (stderr.squish.include? "Undefined use-case" and stderr.squish.include? "Color name 'null' is not defined in the palette")
 puts "\e[32mPassed\e[0m"
 puts "Test: custom colors and use cases are registered correctly…"
-raise "Using an undefined color should not affect output" if File.open("test/output/undefined-colors-warn.css").read.squish.include? "test-undefined-use-case-name"
-raise "Using a custom use case compiles correctly" unless File.open("test/output/undefined-colors-warn.css").read.squish.include? "test-custom-use-case{color:#ccc"
+raise "Using an undefined color should not affect output" if File.open("test/output/test.css").read.squish.include? "test-undefined-use-case-name"
+raise "Using a custom use case compiles correctly" unless File.open("test/output/test.css").read.squish.include? "test-custom-use-case{color:#ccc"
 puts "\e[32mPassed\e[0m"
-# File.delete('test/output/undefined-colors-warn.css')
+puts "Test: overriding existing use cases…"
+raise "Overriding an existing use case compiles correctly" unless File.open("test/output/test.css").read.squish.include? ".test-override-existing-use-case{color:#27757b"
+puts "\e[32mPassed\e[0m"
+File.delete('test/output/test.css')
 
 FileUtils.rmdir "test/output"
