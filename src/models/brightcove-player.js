253239263c78d343fb005747dfc907c697f7cac8
Matthew Andrews <matt@mattandre.ws>
2015-06-17 16:17:09 +0100

    Use video js brightcove integration :) cc @commuterjoy

--- src/models/brightcove-player.js
+++ src/models/brightcove-player.js
@@ -7,43 +7,11 @@ function BrightcovePlayer(el, opts) {
 	this.el = el;
 }
 
-function ensureBrightcoveLibraryLoaded() {
-	if (brightcoveLibraryLoadPromise) {
-		return brightcoveLibraryLoadPromise;
-	}
-	var script = document.createElement('script');
-	script.setAttribute('type', 'text/javascript');
-	script.setAttribute('src', 'https://sadmin.brightcove.com/js/BrightcoveExperiences.js');
-	script.setAttribute('async', true);
-	script.setAttribute('defer', true);
-	document.getElementsByTagName("head")[0].appendChild(script);
-	brightcoveLibraryLoadPromise = new Promise(function(resolve, reject) {
-		script.addEventListener('load', function() {
-			resolve();
-		});
-	});
-	return brightcoveLibraryLoadPromise;
-}
-
 BrightcovePlayer.prototype.init = function() {
 	var videoId = this.el.getAttribute('data-n-video-id');
-	var width = this.el.getAttribute('data-width') || 600;
-	var height = this.el.getAttribute('data-height') || 338;
-
-	var output = `<object class="BrightcoveExperience">
-		<param name="bgcolor" value="#FFFFFF" />
-		<param name="width" value="${width}" />
-		<param name="height" value="${height}" />
-		<param name="playerID" value="1820439054001" />
-		<param name="playerKey" value="AQ~~,AAAACxbljZk~,eD0zYozylZ2-dDEd_-zFMJSX7rv31rz5" />
-		<param name="@videoPlayer" value="${videoId}" />
-		<param name="isVid" value="true" />
-		<param name="isUI" value="true" />
-		<param name="dynamicStreaming" value="true" />
-		<param name="htmlFallback" value="true" />
-		<param name="secureConnections" value="true" />
-		<param name="secureHTMLConnections" value="true" />
-	</object>`;
+	var output = `
+		<iframe class="n-video__brightcove" src='//players.brightcove.net/47628783001/fb14d319-ef92-49ac-92f7-3b565a32cdd7_default/index.html?videoId=${videoId}' allowfullscreen webkitallowfullscreen mozallowfullscreen></iframe>
+	`;
 	this.el.innerHTML = output;
 	return ensureBrightcoveLibraryLoaded()
 		.then(function() {
