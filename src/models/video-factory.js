e08850e077bbf3fe4b8086fff53083e2cbb2cf98
Matthew Andrews <matt@mattandre.ws>
2015-06-17 16:37:31 +0100

    Fix JS errors

--- src/models/video-factory.js
+++ src/models/video-factory.js
@@ -7,9 +7,10 @@ var YouTube = require('./you-tube');
 
 module.exports = function (el, opts) {
 	var source = el.getAttribute('data-n-video-source').toLowerCase();
+	var player = (el.getAttribute('data-n-video-player') || 'html5').toLowerCase();
 
 	if (source === 'brightcove') {
-		if (el.getAttribute('data-n-video-player').toLowerCase() === 'brightcove') {
+		if (player === 'brightcove') {
 			return new BrightcovePlayer(el, opts);
 		}
 		return new Brightcove(el, opts);
