daaa605fe752ea5775421ccb9de4e8e6062a5931
Mark Stephens <Mark.Stephens@ft.com>
2015-08-14 13:54:11 +0100

    1.0.3

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var settings = require('./src/javascript/core/settings');
  * The version of the tracking module.
  * @type {string}
  */
-var version = '1.0.2';
+var version = '1.0.3';
 /**
  * The source of this event.
  * @type {string}
