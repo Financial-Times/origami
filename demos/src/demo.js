0f98ed5333a90d26c53a711e9d285d85281c17e3
Kaelig <kaelig@deloumeau.fr>
2015-08-26 17:58:22 +0100

    Linting fixes

--- demos/src/demo.js
+++ demos/src/demo.js
@@ -1,11 +1,13 @@
+'use strict';
+
 var switchState = function(element, state) {
 	element.setAttribute(state, (!element.getAttribute(state) || element.getAttribute(state) === 'false' ? true : false));
 };
 
 document.addEventListener('click', function(e) {
 	if (e.target.className.includes('o-button')) {
-		if (!e.target.getAttribute('aria-pressed')
-			&& e.target.getAttribute('aria-disabled') !== 'true') {
+		if (!e.target.getAttribute('aria-pressed') &&
+			e.target.getAttribute('aria-disabled') !== 'true') {
 			switchState(e.target, 'aria-selected');
 		}
 		if (e.target.getAttribute('aria-pressed')) {
