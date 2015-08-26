eb258020094b7cbf6c2be70b737e032cfc367759
Kaelig <kaelig@deloumeau.fr>
2015-08-26 17:55:28 +0100

    Make states switchable in demos

---
+++ demos/src/demo.js
@@ -0,0 +1,16 @@
+var switchState = function(element, state) {
+	element.setAttribute(state, (!element.getAttribute(state) || element.getAttribute(state) === 'false' ? true : false));
+};
+
+document.addEventListener('click', function(e) {
+	if (e.target.className.includes('o-button')) {
+		if (!e.target.getAttribute('aria-pressed')
+			&& e.target.getAttribute('aria-disabled') !== 'true') {
+			switchState(e.target, 'aria-selected');
+		}
+		if (e.target.getAttribute('aria-pressed')) {
+			switchState(e.target, 'aria-pressed');
+		}
+	}
+	e.stopPropagation();
+}, false);
