0a60554c9fa12539367455fd143ddb259d5408c2
Roland Vachter <roland.vachter@iquestgroup.com>
2015-09-18 10:13:56 +0300

    Cover IEs as well, firing DOMContentLoaded as soon as possible.

--- main.js
+++ main.js
@@ -12,7 +12,42 @@ window.addEventListener('load', trigger.bind(null, 'load'));
 window.addEventListener('load', trigger.bind(null, 'DOMContentLoaded'));
 document.addEventListener('DOMContentLoaded', trigger.bind(null, 'DOMContentLoaded'));
 
+document.onreadystatechange = function () {
+	if (document.readyState === 'complete') {
+		trigger('DOMContentLoaded');
+		trigger('load');
+	}
+};
+
 if (document.readyState === 'complete') {
-	trigger('load');
 	trigger('DOMContentLoaded');
+	trigger('load');
+}
+
+
+if (document.attachEvent) {
+	// If IE and not a frame
+	// continually check to see if the document is ready
+	var top = false;
+
+	try {
+		top = window.frameElement == null && document.documentElement;
+	} catch(e) {}
+
+	if (top && top.doScroll) {
+		(function doScrollCheck() {
+			if (!('DOMContentLoaded' in hasFired)) {
+
+				try {
+					// Use the trick by Diego Perini
+					// http://javascript.nwbox.com/IEContentLoaded/
+					top.doScroll("left");
+				} catch(e) {
+					return setTimeout( doScrollCheck, 50 );
+				}
+
+				trigger('DOMContentLoaded');
+			}
+		})();
+	}
 }
