513bcbff457ad527aca331baac08bda132cac45e
Keith Rogers <git@keithrogers.eu>
2015-09-30 09:21:50 +0100

    No more vars/strict

--- tests/messaging.spec.js
+++ tests/messaging.spec.js
@@ -1,38 +1,36 @@
-/* jshint browser:true */
 /* global describe:false, it:false, require:false, beforeEach */
-"use strict";
 
-var expect = require('chai').expect;
-var nNotification = require('../main.js');
-var helpers = require('./phantomjs-helpers.js');
+const expect = require('chai').expect;
+const nNotification = require('../main.js');
+const helpers = require('./phantomjs-helpers.js');
 nNotification.init();
 
 describe("nNotification", function(){
 
 	beforeEach(function(done) {
-		var notification = document.querySelector('.n-notification');
+		const notification = document.querySelector('.n-notification');
 		if(notification) {
 			nNotification.hide();
 		}
 		setTimeout(done, 501);
 	});
 
-	var message = "<p>This is a notification</p>";
+	const message = "<p>This is a notification</p>";
 
 	it("Should be able to show a message with the given content", function(){
 
 		nNotification.show({content:message,type:'message'});
 		expect(document.querySelector('.n-notification')).not.to.be.null;
-		var content = document.querySelector('.n-notification__content').innerHTML;
+		const content = document.querySelector('.n-notification__content').innerHTML;
 		expect(content).to.contain(message);
 	});
 
 	it('Should show a message when an FT.Notification event is fired', function(done){
-		var event = new CustomEvent('nNotification.show', {detail:{content:message}});
+		const event = new CustomEvent('nNotification.show', {detail:{content:message}});
 		document.dispatchEvent(event);
 		setTimeout(function(){
 			expect(document.querySelector('.n-notification')).not.to.be.null;
-			var content = document.querySelector('.n-notification__content').innerHTML;
+			const content = document.querySelector('.n-notification__content').innerHTML;
 			expect(content).to.contain(message);
 			done();
 		}, 0);
@@ -52,7 +50,7 @@ describe("nNotification", function(){
 	it('Will hide message after 5 seconds if no other interaction', function(done){
 		this.timeout(8000);
 		nNotification.show({content:message,type:'message'});
-		var clickEvent = new Event('click');
+		const clickEvent = new Event('click');
 		document.querySelector('.n-notification__close').dispatchEvent(clickEvent);
 		setTimeout(function(){
 			expect(document.querySelector('.n-notification')).to.be.null;
