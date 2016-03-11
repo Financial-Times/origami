d91e318887e409aa744b87007d3e727398cb1935
j-mes <james.loveridge@ft.com>
2016-03-11 15:45:25 +0000

    Change from Travis to Circle, modify origami.json

--- main.js
+++ main.js
@@ -225,7 +225,7 @@ const init = function(el, opts) {
 		el = document.querySelector(el);
 	}
 	if (/\bo-expander\b/.test(el.getAttribute('data-o-component'))) {
-		return construct(el, opts)
+		return construct(el, opts);
 	}
 	return [].map.call(el.querySelectorAll('[data-o-component~="o-expander"]'), el => construct(el, opts));
 };
