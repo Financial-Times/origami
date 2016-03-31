355e746a9a1ceed8f2d8e5b961366067880a5da9
Adam Onishi <onishiweb@users.noreply.github.com>
2016-03-31 15:55:27 +0100

    Revert "More sensible 'days ago' terminology"

--- main.js
+++ main.js
@@ -34,15 +34,6 @@ const formatReplacementsMap = {
 	a: '(date.getHours() >= 12 ? "pm" : "am")' // pm
 };
 
-const inSeconds = {
-	minute: 60,
-	hour: 60 * 60,
-	day: 24 * 60 * 60,
-	week: 7 * 24 * 60 * 60,
-	month: (365 * 24 * 60 * 60) / 12,
-	year: 365 * 24 * 60 * 60
-};
-
 let interval;
 
 function ODate(rootEl) {
@@ -167,28 +158,28 @@ ODate.timeAgo = function(date, interval) {
 	if (!date) return;
 
 	interval = interval || Math.round(((new Date()) - date) / 1000);
-	if (interval < inSeconds.minute) {
+	if (interval < 45) {
 		return interval + ' seconds ago';
-	} else if (interval < (2 * inSeconds.minute)) {
+	} else if (interval < 90) {
 		return 'a minute ago';
-	} else if (interval < inSeconds.hour) {
-		return Math.floor(interval / 60) + ' minutes ago';
-	} else if (interval < (2 * inSeconds.hour)) {
+	} else if (interval < 45 * 60) {
+		return Math.round(interval / 60) + ' minutes ago';
+	} else if (interval < 90 * 60) {
 		return 'an hour ago';
-	} else if (interval < inSeconds.day) {
-		return Math.floor(interval / inSeconds.hour) + ' hours ago';
-	} else if (interval < (2 * inSeconds.day)) {
+	} else if (interval < 22 * 60 * 60) {
+		return Math.round(interval / (60 * 60)) + ' hours ago';
+	} else if (interval < 36 * 60 * 60) {
 		return 'a day ago';
-	} else if (interval < inSeconds.month) {
-		return Math.floor(interval / inSeconds.day) + ' days ago';
-	} else if (interval < (2 * inSeconds.month)) {
+	} else if (interval < 25 * 60 * 60 * 24) {
+		return Math.round(interval / (60 * 60 * 24)) + ' days ago';
+	} else if (interval < 45 * 60 * 60 * 24) {
 		return 'a month ago';
-	} else if (interval < inSeconds.year) {
-		return Math.floor(interval / inSeconds.month) + ' months ago';
-	} else if (interval < (2 * inSeconds.year)) {
+	} else if (interval < 345 * 60 * 60 * 24) {
+		return Math.round(interval / (60 * 60 * 24 * 30)) + ' months ago';
+	} else if (interval < 547 * 60 * 60 * 24) {
 		return 'a year ago';
 	} else {
-		return Math.max(2, Math.floor(interval / inSeconds.year)) + ' years ago';
+		return Math.max(2, Math.round(interval / (60 * 60 * 24 * 365))) + ' years ago';
 	}
 };
 
