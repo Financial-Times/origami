8ffadb009b3a8ed1d620a4408834e016018533e3
Roland Vachter <roland.vachter@iquestgroup.com>
2016-05-05 09:54:57 +0300

    Default config transformed into js file.

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	loginUrl: "https://accounts.ft.com/login",
+	livefyre: {
+		network: "ft.fyre.co",
+		domain: "ft.auth.fyre.co",
+		resourceDomainBase: "//cdn.livefyre.com/"
+	},
+	resourceUrls: {
+		livefyreJs: "Livefyre.js"
+	},
+	emailNotifications: true
+};
