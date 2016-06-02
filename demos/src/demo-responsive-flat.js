ee8efee762b6d40a58a2720f59660b40a229a730
j-mes <james.loveridge@ft.com>
2016-06-02 17:11:26 +0100

    Add demo of responsive-flat template/js

---
+++ demos/src/demo-responsive-flat.js
@@ -0,0 +1,10 @@
+const oTableEl = document.querySelector('[data-o-table-responsive="flat"]');
+const heads = oTableEl.querySelectorAll('thead tr th');
+const rows = oTableEl.getElementsByTagName('tr');
+
+for (const row of rows) {
+  const data = Array.from(row.getElementsByTagName('td'));
+	data.forEach((td, dataIndex) => {
+		td.parentNode.insertBefore(heads[dataIndex].cloneNode(true), td);
+	});
+}
