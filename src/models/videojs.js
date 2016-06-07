f7b6d1427901b9efbf378875b1bac9cf6ee07e04
Vlad Dubrovskis <vlad@vovka.co.uk>
2016-06-07 17:45:29 +0100

    unde change of formatting so that the changes are clear in the pul request

--- src/models/videojs.js
+++ src/models/videojs.js
@@ -12,259 +12,256 @@ let videoElementIdOrder = 0;
 let advertising;
 
 const pauseOtherVideos = (video) => {
-  requestedVideo = video;
-  if (currentlyPlayingVideo && currentlyPlayingVideo !== requestedVideo) {
-    currentlyPlayingVideo.pause();
-  }
+	requestedVideo = video;
+	if(currentlyPlayingVideo && currentlyPlayingVideo !== requestedVideo){
+		currentlyPlayingVideo.pause();
+	}
 
-  currentlyPlayingVideo = video;
+	currentlyPlayingVideo = video;
 };
 
 const ensureVideoJsLibraryLoaded = () => {
-  if (videoJsPromise) {
-    return videoJsPromise;
-  }
+	if (videoJsPromise) {
+		return videoJsPromise;
+	}
 
-  const videojsScript = document.createElement('script');
-  videojsScript.setAttribute('type', 'text/javascript');
-  videojsScript.setAttribute('src', `//vjs.zencdn.net/5.9.2/video.min.js`);
-  videojsScript.setAttribute('async', true);
-  videojsScript.setAttribute('defer', true);
-  document.getElementsByTagName("head")[0].appendChild(videojsScript);
+	const videojsScript = document.createElement('script');
+	videojsScript.setAttribute('type', 'text/javascript');
+	videojsScript.setAttribute('src', `//vjs.zencdn.net/5.9.2/video.min.js`);
+	videojsScript.setAttribute('async', true);
+	videojsScript.setAttribute('defer', true);
+	document.getElementsByTagName("head")[0].appendChild(videojsScript);
 
-  const videojsStyles = document.createElement('link')
-  videojsStyles.setAttribute('rel', 'stylesheet')
-  videojsStyles.setAttribute('type', 'text/css')
-  videojsStyles.setAttribute('href', '//vjs.zencdn.net/5.9.2/video-js.min.css')
-  document.getElementsByTagName('head')[0].appendChild(videojsStyles);
+	const videojsStyles = document.createElement('link')
+	videojsStyles.setAttribute('rel', 'stylesheet')
+	videojsStyles.setAttribute('type', 'text/css')
+	videojsStyles.setAttribute('href', '//vjs.zencdn.net/5.9.2/video-js.min.css')
+	document.getElementsByTagName('head')[0].appendChild(videojsStyles);
 
-  videoJsPromise = new Promise(resolve => {
-    videojsScript.addEventListener('load', () => {
-      resolve();
-    });
-  });
+	videoJsPromise = new Promise(resolve => {
+		videojsScript.addEventListener('load', () => {
+				resolve();
+		});
+	});
 
-  return videoJsPromise;
+	return videoJsPromise;
 }
 
 const ensureVideoJsPluginsAreLoaded = () => {
-  if (videoJsPluginsPromise) {
-    return videoJsPluginsPromise;
-  }
+	if(videoJsPluginsPromise) {
+		return videoJsPluginsPromise;
+	}
 
-  if (advertising) {
-    const googleSdkScript = document.createElement('script');
-    googleSdkScript.setAttribute('type', 'text/javascript');
-    googleSdkScript.setAttribute('src', `//imasdk.googleapis.com/js/sdkloader/ima3.js`);
-    googleSdkScript.setAttribute('async', true);
-    googleSdkScript.setAttribute('defer', true);
-    document.getElementsByTagName("head")[0].appendChild(googleSdkScript);
+	if(advertising) {
+		const googleSdkScript = document.createElement('script');
+		googleSdkScript.setAttribute('type', 'text/javascript');
+		googleSdkScript.setAttribute('src', `//imasdk.googleapis.com/js/sdkloader/ima3.js`);
+		googleSdkScript.setAttribute('async', true);
+		googleSdkScript.setAttribute('defer', true);
+		document.getElementsByTagName("head")[0].appendChild(googleSdkScript);
 
-    let googleSdkScriptPromise = new Promise(sdkLoaded => {
-      googleSdkScript.addEventListener('load', () => {
-        sdkLoaded();
-      });
-    });
+		let googleSdkScriptPromise = new Promise(sdkLoaded => {
+			googleSdkScript.addEventListener('load', () => {
+				sdkLoaded();
+			});
+		});
 
-    const videoJsAdPluginsScript = document.createElement('script');
-    videoJsAdPluginsScript.setAttribute('type', 'text/javascript');
-    videoJsAdPluginsScript.setAttribute('src', `https://next-geebee.ft.com/assets/videojs/videojs-plugins.min.js`);
-    videoJsAdPluginsScript.setAttribute('async', true);
-    videoJsAdPluginsScript.setAttribute('defer', true);
-    document.getElementsByTagName("head")[0].appendChild(videoJsAdPluginsScript);
+		const videoJsAdPluginsScript = document.createElement('script');
+		videoJsAdPluginsScript.setAttribute('type', 'text/javascript');
+		videoJsAdPluginsScript.setAttribute('src', `https://next-geebee.ft.com/assets/videojs/videojs-plugins.min.js`);
+		videoJsAdPluginsScript.setAttribute('async', true);
+		videoJsAdPluginsScript.setAttribute('defer', true);
+		document.getElementsByTagName("head")[0].appendChild(videoJsAdPluginsScript);
 
-    let videoJsAdPluginsScriptPromise = new Promise(pluginsLoaded => {
-      videoJsAdPluginsScript.addEventListener('load', () => {
-        pluginsLoaded();
-      });
-    });
+		let videoJsAdPluginsScriptPromise = new Promise(pluginsLoaded => {
+			videoJsAdPluginsScript.addEventListener('load', () => {
+				pluginsLoaded();
+			});
+		});
 
-    const videoJsAdPluginStyles = document.createElement('link')
-    videoJsAdPluginStyles.setAttribute('rel', 'stylesheet')
-    videoJsAdPluginStyles.setAttribute('type', 'text/css')
-    videoJsAdPluginStyles.setAttribute('href', 'https://next-geebee.ft.com/assets/videojs/videojs-plugins.min.css')
-    document.getElementsByTagName('head')[0].appendChild(videoJsAdPluginStyles);
-    videoJsPluginsPromise = Promise.all([googleSdkScriptPromise, videoJsAdPluginsScriptPromise]);
-    return videoJsPluginsPromise;
-  } else {
-    return Promise.resolve();
-  }
+		const videoJsAdPluginStyles = document.createElement('link')
+		videoJsAdPluginStyles.setAttribute('rel', 'stylesheet')
+		videoJsAdPluginStyles.setAttribute('type', 'text/css')
+		videoJsAdPluginStyles.setAttribute('href', 'https://next-geebee.ft.com/assets/videojs/videojs-plugins.min.css')
+		document.getElementsByTagName('head')[0].appendChild(videoJsAdPluginStyles);
+		videoJsPluginsPromise = Promise.all([googleSdkScriptPromise, videoJsAdPluginsScriptPromise]);
+		return videoJsPluginsPromise;
+	} else {
+		return Promise.resolve();
+	}
 }
 
 const ensureAllScriptsAreLoaded = () => {
-  return ensureVideoJsLibraryLoaded().then(() => {
-    return ensureVideoJsPluginsAreLoaded();
-  });
+	return ensureVideoJsLibraryLoaded().then(() => {
+		return ensureVideoJsPluginsAreLoaded();
+	});
 };
 
 
 // use the image resizing service, if width supplied
 const updatePosterUrl = (posterImage, width) => {
-  let url = `https://image.webservices.ft.com/v1/images/raw/${encodeURIComponent(posterImage)}?source=o-video`;
-  if (width) {
-    url += `&fit=scale-down&width=${width}`;
-  }
-  return url;
+	let url = `https://image.webservices.ft.com/v1/images/raw/${encodeURIComponent(posterImage)}?source=o-video`;
+	if (width) {
+		url += `&fit=scale-down&width=${width}`;
+	}
+	return url;
 };
 
 class VideoJsPlayer extends Video {
-  constructor(el, opts) {
-    advertising = opts && opts['advertising'] ? true : false;
-    ensureAllScriptsAreLoaded();
-    super(el, opts);
-    this.targeting = {
-      site: '/5887/ft.com',
-      position: 'video',
-      sizes: '592x333|400x225',
-      videoId: this.id
-    };
-  }
+	constructor(el, opts) {
+		advertising = opts && opts['advertising'] ? true : false;
+		ensureAllScriptsAreLoaded();
+		super(el, opts);
+		this.targeting = {
+			site: '/5887/ft.com',
+			position: 'video',
+			sizes: '592x333|400x225',
+			videoId: this.id
+		};
+	}
 
-  getData() {
-    const dataPromise = this.opts.data ? Promise.resolve(this.opts.data) : crossDomainFetch(`//next-video.ft.com/api/${this.id}`)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw Error('Brightcove responded with a ' + response.status + ' (' + response.statusText + ') for id ' + this.id);
-        }
-      });
+	getData() {
+		const dataPromise = this.opts.data ? Promise.resolve(this.opts.data) : crossDomainFetch(`//next-video.ft.com/api/${this.id}`)
+			.then(response => {
+				if (response.ok) {
+					return response.json();
+				} else {
+					throw Error('Brightcove responded with a ' + response.status + ' (' + response.statusText + ') for id ' + this.id);
+				}
+			});
 
-    return dataPromise.then(data => {
-      this.brightcoveData = data;
-      this.posterImage = updatePosterUrl(data.videoStillURL, this.opts.optimumWidth);
-      this.rendition = getAppropriateRendition(data.renditions);
-      this.targeting.brand = this.getVideoBrand();
-    });
-  }
+		return dataPromise.then(data => {
+			this.brightcoveData = data;
+			this.posterImage = updatePosterUrl(data.videoStillURL, this.opts.optimumWidth);
+			this.rendition = getAppropriateRendition(data.renditions);
+			this.targeting.brand = this.getVideoBrand();
+		});
+	}
 
-  renderVideo() {
-    if (this.rendition) {
-      if (this.opts.placeholder) {
-        this.addPlaceholder();
-      } else {
-        this.addVideo();
-      }
-    }
-    return this;
-  }
+	renderVideo() {
+		if (this.rendition) {
+			if (this.opts.placeholder) {
+				this.addPlaceholder();
+			} else {
+				this.addVideo();
+			}
+		}
+		return this;
+	}
 
-  init() {
-    const initPromise = this.getData().then(() => this.renderVideo());
-    return Promise.all([initPromise, videoJsPromise]);
-  }
+	init() {
+		const initPromise = this.getData().then(() => this.renderVideo());
+		return Promise.all([initPromise, videoJsPromise]);
+	}
 
-  info() {
-    const date = new Date(+this.brightcoveData.publishedDate);
-    return {
-      posterImage: this.posterImage,
-      id: this.brightcoveData.id,
-      length: this.brightcoveData.length,
-      longDescription: this.brightcoveData.longDescription,
-      name: this.brightcoveData.name,
-      publishedDate: date.toISOString(),
-      publishedDateReadable: date.toUTCString(),
-      shortDescription: this.brightcoveData.shortDescription,
-      tags: this.brightcoveData.tags,
-    };
-  }
+	info() {
+		const date = new Date(+this.brightcoveData.publishedDate);
+		return {
+			posterImage: this.posterImage,
+			id: this.brightcoveData.id,
+			length: this.brightcoveData.length,
+			longDescription: this.brightcoveData.longDescription,
+			name: this.brightcoveData.name,
+			publishedDate: date.toISOString(),
+			publishedDateReadable: date.toUTCString(),
+			shortDescription: this.brightcoveData.shortDescription,
+			tags: this.brightcoveData.tags,
+		};
+	}
 
 
-  addVideo() {
-    let videoIdProperty = 'test-video-' + videoElementIdOrder++;
-    this.el = document.createElement('video');
-    this.el.setAttribute('poster', this.posterImage);
-    this.el.setAttribute('src', this.rendition.url);
-    this.el.setAttribute('id', videoIdProperty);
-    this.el.className = Array.isArray(this.classes) ? this.classes.join(' ') : this.classes;
-    this.el.classList.add('o-video--videojs');
-    this.containerEl.appendChild(this.el);
-    this.el.addEventListener('playing', () => pauseOtherVideos(this.el));
-    return ensureAllScriptsAreLoaded()
-      .then(() => {
-        let videoPlayer = videojs(videoIdProperty, {
-          "controls": true,
-          "autoplay": true,
-          "preload": "auto"
-        }).width('100%');
-        if (advertising) {
-          this.advertising(videoPlayer, videoIdProperty);
-        }
-      });
-  }
+	addVideo() {
+		let videoIdProperty = 'test-video-' + videoElementIdOrder++;
+		this.el = document.createElement('video');
+		this.el.setAttribute('poster', this.posterImage);
+		this.el.setAttribute('src', this.rendition.url);
+		this.el.setAttribute('id', videoIdProperty);
+		this.el.className = Array.isArray(this.classes) ? this.classes.join(' ') : this.classes;
+		this.el.classList.add('o-video--videojs');
+		this.containerEl.appendChild(this.el);
+		this.el.addEventListener('playing', () => pauseOtherVideos(this.el));
+		return ensureAllScriptsAreLoaded()
+			.then(() => {
+				let videoPlayer = videojs(videoIdProperty, {"controls": true,"autoplay": true,"preload": "auto"}).width('100%');
+				if(advertising) {
+					this.advertising(videoPlayer, videoIdProperty);
+				}
+			});
+	}
 
-  getVideoBrand() {
-    if (!this.brightcoveData.tags || this.brightcoveData.tags.length === 0) {
-      return false;
-    } else {
-      let filtered = this.brightcoveData.tags.filter(val => val.indexOf('brand:') !== -1 || val.indexOf('Brand:') !== -1);
-      if (filtered.length > 0) {
-        try {
-          return filtered.pop().substring(6);
-        } catch (e) {
-          return false;
-        }
-      } else {
-        return false;
-      }
-    }
-  }
+	getVideoBrand() {
+		if(!this.brightcoveData.tags || this.brightcoveData.tags.length === 0) {
+			return false;
+		} else {
+			let filtered = this.brightcoveData.tags.filter(val => val.indexOf('brand:') !== -1 || val.indexOf('Brand:') !== -1);
+			if(filtered.length > 0) {
+				try {
+					 return filtered.pop().substring(6);
+				}
+				catch (e) {
+					 return false;
+				}
+			} else {
+				return false;
+			}
+		}
+	}
 
-  advertising(player, videoIdProperty) {
-    let advertisingUrl = `http://pubads.g.doubleclick.net/gampad/ads?env=vp&gdfp_req=1&impl=s&output=xml_vast2&iu=${this.targeting.site}&sz=${this.targeting.sizes}&unviewed_position_start=1&scp=pos%3D${this.targeting.position}&ttid=${this.targeting.videoId}`;
-    if (this.targeting.brand) {
-      advertisingUrl += `&brand=${encodeURIComponent(this.targeting.brand)}`;
-    }
+	advertising(player, videoIdProperty) {
+		let advertisingUrl = `http://pubads.g.doubleclick.net/gampad/ads?env=vp&gdfp_req=1&impl=s&output=xml_vast2&iu=${this.targeting.site}&sz=${this.targeting.sizes}&unviewed_position_start=1&scp=pos%3D${this.targeting.position}&ttid=${this.targeting.videoId}`;
+		if(this.targeting.brand) {
+			advertisingUrl += `&brand=${encodeURIComponent(this.targeting.brand)}`;
+		}
 
-    player.ima({
-      id: videoIdProperty,
-      adTagUrl: advertisingUrl
-    });
-    player.ima.requestAds();
-  }
+		player.ima({
+			id: videoIdProperty,
+			adTagUrl: advertisingUrl
+		});
+		player.ima.requestAds();
+	}
 
-  addPlaceholder() {
-    this.placeholderEl = document.createElement('img');
-    this.placeholderEl.setAttribute('src', this.posterImage);
-    this.placeholderEl.className = Array.isArray(this.classes) ? this.classes.join(' ') : this.classes;
-    this.containerEl.classList.add('o-video--placeholder');
+	addPlaceholder() {
+		this.placeholderEl = document.createElement('img');
+		this.placeholderEl.setAttribute('src', this.posterImage);
+		this.placeholderEl.className = Array.isArray(this.classes) ? this.classes.join(' ') : this.classes;
+		this.containerEl.classList.add('o-video--placeholder');
 
-    this.containerEl.appendChild(this.placeholderEl);
+		this.containerEl.appendChild(this.placeholderEl);
 
-    let titleEl;
-    if (this.opts.placeholderTitle) {
-      titleEl = document.createElement('div');
-      titleEl.className = 'o-video__title';
-      titleEl.textContent = this.brightcoveData.name;
-      this.containerEl.appendChild(titleEl);
-    }
+		let titleEl;
+		if (this.opts.placeholderTitle) {
+			titleEl = document.createElement('div');
+			titleEl.className = 'o-video__title';
+			titleEl.textContent = this.brightcoveData.name;
+			this.containerEl.appendChild(titleEl);
+		}
 
-    if (this.opts.playButton) {
+		if (this.opts.playButton) {
 
-      const playButtonEl = document.createElement('button');
-      playButtonEl.className = 'o-video__play-button';
-      this.containerEl.appendChild(playButtonEl);
+			const playButtonEl = document.createElement('button');
+			playButtonEl.className = 'o-video__play-button';
+			this.containerEl.appendChild(playButtonEl);
 
-      const playIconEl = document.createElement('i');
-      playIconEl.className = 'o-video__play-button-icon';
-      playButtonEl.appendChild(playIconEl);
+			const playIconEl = document.createElement('i');
+			playIconEl.className = 'o-video__play-button-icon';
+			playButtonEl.appendChild(playIconEl);
 
-      playButtonEl.addEventListener('click', () => {
-        this.containerEl.removeChild(playButtonEl);
-        if (titleEl) {
-          this.containerEl.removeChild(titleEl);
-        }
-        this.removePlaceholder();
-        this.addVideo();
-        this.el.focus();
-      });
-    }
-  }
+			playButtonEl.addEventListener('click', () => {
+				this.containerEl.removeChild(playButtonEl);
+				if (titleEl) {
+					this.containerEl.removeChild(titleEl);
+				}
+				this.removePlaceholder();
+				this.addVideo();
+				this.el.focus();
+			});
+		}
+	}
 
-  removePlaceholder() {
-    this.containerEl.classList.remove('o-video--placeholder');
-    this.containerEl.removeChild(this.placeholderEl);
-  }
+	removePlaceholder() {
+		this.containerEl.classList.remove('o-video--placeholder');
+		this.containerEl.removeChild(this.placeholderEl);
+	}
 
 }
 
