d9491bdc9a6792582df926c44768f5a79348ba9b
Matt Hinchliffe <matt.hinchliffe@ft.com>
2016-06-29 15:36:12 +0100

    Remove sections navigation, remove redundant grid markup and apply new colour scheme.

---
+++ demos/src/footer.js
@@ -0,0 +1,59 @@
+module.exports = {
+	"theme": "theme-dark",
+	"link_sections": [
+		{
+			"title": "Support",
+			"items": [
+				{ "text": "Help", "href": "#" },
+				{ "text": "About Us", "href": "#" }
+			]
+		},
+		{
+			"title": "Legal & Privacy",
+			"items": [
+				{ "text": "Terms & Conditions", "href": "#" },
+				{ "text": "Privacy", "href": "#" },
+				{ "text": "Cookies", "href": "#" },
+				{ "text": "Copyright", "href": "#" }
+			]
+		},
+		{
+			"title": "Services",
+			"items": [
+				{ "text": "Conferences & Events", "href": "#" },
+				{ "text": "Individual Subscriptions", "href": "#" },
+				{ "text": "Group Subscriptions", "href": "#" },
+				{ "text": "Republishing", "href": "#" },
+				{ "text": "Contracts & Tenders", "href": "#" }
+			]
+		},
+		{
+			"items": [
+				{ "text": "Analyst Research", "href": "#" },
+				{ "text": "Executive Job Search", "href": "#" },
+				{ "text": "Advertise with the FT", "href": "#" },
+				{ "text": "Follow the FT on Twitter", "href": "#" }
+			]
+		},
+		{
+			"title": "Tools",
+			"items": [
+				{ "text": "Portfolio", "href": "#" },
+				{ "text": "Today's Paper", "href": "#" },
+				{ "text": "Alerts Hub", "href": "#" },
+				{ "text": "Lexicon", "href": "#" },
+				{ "text": "MBA Rankings", "href": "#" }
+			]
+		},
+		{
+			"items": [
+				{ "text": "Economic Calendar", "href": "#" },
+				{ "text": "Newsfeed", "href": "#" },
+				{ "text": "Newsletters", "href": "#" },
+				{ "text": "Currency Converter", "href": "#" },
+				{ "text": "Ebooks", "href": "#" }
+			]
+		}
+	],
+	"copyrightYear": "2016"
+};
