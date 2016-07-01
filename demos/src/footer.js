7380a6213f94fd99774ee5e5609358e492da49ae
Matt Hinchliffe <matt.hinchliffe@ft.com>
2016-07-01 16:09:25 +0100

    Switch to a single list per link set so that we can use column layout

--- demos/src/footer.js
+++ demos/src/footer.js
@@ -27,11 +27,7 @@ const data = {
 					{ "text": "Individual Subscriptions", "href": "#" },
 					{ "text": "Group Subscriptions", "href": "#" },
 					{ "text": "Republishing", "href": "#" },
-					{ "text": "Contracts & Tenders", "href": "#" }
-				]
-			},
-			{
-				"items": [
+					{ "text": "Contracts & Tenders", "href": "#" },
 					{ "text": "Analyst Research", "href": "#" },
 					{ "text": "Executive Job Search", "href": "#" },
 					{ "text": "Advertise with the FT", "href": "#" },
@@ -45,11 +41,7 @@ const data = {
 					{ "text": "Today's Paper", "href": "#" },
 					{ "text": "Alerts Hub", "href": "#" },
 					{ "text": "Lexicon", "href": "#" },
-					{ "text": "MBA Rankings", "href": "#" }
-				]
-			},
-			{
-				"items": [
+					{ "text": "MBA Rankings", "href": "#" },
 					{ "text": "Economic Calendar", "href": "#" },
 					{ "text": "Newsfeed", "href": "#" },
 					{ "text": "Newsletters", "href": "#" },
@@ -66,47 +58,27 @@ const data = {
 					{ "text": "Analyse Africa", "href": "#" },
 					{ "text": "Board IQ", "href": "#" },
 					{ "text": "Corporate Learning Alliance", "href": "#" },
-					{ "text": "DPN: Deutsche Pensions & Investment Nachrichten", "href": "#" }
-				]
-			},
-			{
-				"items": [
+					{ "text": "DPN: Deutsche Pensions & Investment Nachrichten", "href": "#" },
 					{ "text": "ExecSense", "href": "#" },
 					{ "text": "FDI Intelligence", "href": "#" },
 					{ "text": "Financial Advisor IQ", "href": "#" },
 					{ "text": "FT Chinese", "href": "#" },
-					{ "text": "FT Live", "href": "#" }
-				]
-			},
-			{
-				"items": [
+					{ "text": "FT Live", "href": "#" },
 					{ "text": "FT Property Listings", "href": "#" },
 					{ "text": "FT Advisor", "href": "#" },
 					{ "text": "Fund Fire", "href": "#" },
 					{ "text": "Global Risk Regulator", "href": "#" },
-					{ "text": "How to Spend It", "href": "#" }
-				]
-			},
-			{
-				"items": [
+					{ "text": "How to Spend It", "href": "#" },
 					{ "text": "Ignites", "href": "#" },
 					{ "text": "Ignites Asia", "href": "#" },
 					{ "text": "Ignites Europe", "href": "#" },
 					{ "text": "Investors Chronicle", "href": "#" },
-					{ "text": "Mandate Wire", "href": "#" }
-				]
-			},
-			{
-				"items": [
+					{ "text": "Mandate Wire", "href": "#" },
 					{ "text": "New York Insitute of Finance", "href": "#" },
 					{ "text": "Non Executive Directors Club", "href": "#" },
 					{ "text": "Pensions Expert", "href": "#" },
 					{ "text": "Professional Wealth Management", "href": "#" },
-					{ "text": "125 Club", "href": "#" }
-				]
-			},
-			{
-				"items": [
+					{ "text": "125 Club", "href": "#" },
 					{ "text": "The Banker", "href": "#" },
 					{ "text": "The Banker Database", "href": "#" },
 					{ "text": "This is Africa", "href": "#" }
@@ -118,13 +90,11 @@ const data = {
 };
 
 module.exports = function () {
-	// add an ID to each title
-	let id = 0;
+	const ITEMS_PER_COLUMN = 5;
 
-	data.matrix.forEach(lists => {
-		lists.forEach(list => {
-			list.title && id++;
-			list.titleId = id;
+	data.matrix.forEach(row => {
+		row.forEach(column => {
+			column.size = Math.ceil(column.items.length / ITEMS_PER_COLUMN);
 		});
 	});
 
