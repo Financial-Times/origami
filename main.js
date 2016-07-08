c8d6bc27c56a65fff374b2047b4ad763b5d22aeb
Ben Fletcher <ben.fletcher@ft.com>
2016-07-08 11:11:52 +0100

    add date formatting option

--- main.js
+++ main.js
@@ -95,6 +95,8 @@ ODate.prototype.update = function() {
 
 	if (format === 'today-or-yesterday-or-nothing') {
 		 dateString = ODate.asTodayOrYesterdayOrNothing(date);
+	} else if (format === 'date') {
+		dateString = ODate.format(date, 'date');
 	} else {
 		 dateString = ODate.ftTime(date);
 	}
