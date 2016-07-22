65d5ea7b8a04df3c0df997916a14fbb9a3319d1e
Matt Hinchliffe <matt.hinchliffe@ft.com>
2016-07-22 12:20:31 +0100

    Refactor simple header to use existing components and introduce the concept of a 'takeover' to the top row

--- demos/src/simple-header.js
+++ demos/src/simple-header.js
@@ -1,52 +1,5 @@
-const drawer = require('./drawer');
+const header = require('./header');
 
 module.exports = function () {
-	return {
-		hasMenu: true,
-		hasMyFT: true,
-		isSignedIn: true,
-
-
-
-		nav: {
-			desktop: [
-				{
-					name: 'Home',
-					url: '#',
-					selected: true
-				},
-				{
-					name: 'World',
-					url: '#'
-				},
-				{
-					name: 'UK',
-					url: '#'
-				},
-				{
-					name: 'Companies',
-					url: '#'
-				},
-				{
-					name: 'Markets',
-					url: '#'
-				},
-				{
-					name: 'Opinion',
-					url: '#'
-				},
-				{
-					name: 'Work & Careers',
-					url: '#'
-				},
-				{
-					name: 'Life & Arts',
-					url: '#'
-				}
-			]
-
-		},
-
-		drawer: drawer()
-	};
+	return header();
 };
