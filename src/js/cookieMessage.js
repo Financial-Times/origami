a8310f4f133d2b565f0827d513a5bca937c192c2
Edward Chen <edward@sessiondigital.com>
2016-08-17 17:08:59 +0100

    building the basic functionality

--- src/js/cookieMessage.js
+++ src/js/cookieMessage.js
@@ -2,6 +2,45 @@ class CookieMessage {
 
 	constructor (CookieMessageEl) {
 		this.CookieMessageEl = CookieMessageEl;
+
+        return this.setup();
+	}
+
+	setup () {
+        const message = document.querySelector('.o-cookie-message');
+
+		if (userHasConsentedToCookies()) {
+		    hideMessage();
+			return;
+		}
+
+        setupMessage();
+
+        function setupMessage () {
+            document.querySelector('.cookie-message__close-btn').addEventListener('click', flagUserAsConsentingToCookies);
+        }
+
+		function hideMessage () {
+			message.classList.add('cookie-message--hidden');
+		}
+
+		function flagUserAsConsentingToCookies () {
+			sessionStorage.setItem('COOKIE_CONSENT', 1);
+			hideMessage();
+		}
+
+		function userHasConsentedToCookies () {
+			if (sessionStorage.getItem('COOKIE_CONSENT') == 1) {
+				return true;
+			}
+
+			// HACK: Whilst FT.com is still around auto-opt user in if they have accepted over there.
+			if (/\bcookieconsent=accepted(?:;|$)/.test(document.cookie)){
+				flagUserAsConsentingToCookies();
+				return true;
+			}
+			return false;
+		}
 	}
 
 	static init (rootEl) {
