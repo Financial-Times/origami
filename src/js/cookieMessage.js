8a82116f4ede741441d2205d769733a4043623ee
Edward Chen <edward@sessiondigital.com>
2016-08-22 09:10:46 +0100

    convert spaces to tabs

--- src/js/cookieMessage.js
+++ src/js/cookieMessage.js
@@ -1,11 +1,11 @@
 class CookieMessage {
 
 	constructor (CookieMessageEl) {
-	    if (!CookieMessageEl) {
-            CookieMessageEl = document.querySelector('[data-o-component="o-cookie-message"]');
-        } else if (typeof CookieMessageEl === 'string') {
-            CookieMessageEl = document.querySelector(CookieMessageEl);
-        }
+		if (!CookieMessageEl) {
+			CookieMessageEl = document.querySelector('[data-o-component="o-cookie-message"]');
+		} else if (typeof CookieMessageEl === 'string') {
+			CookieMessageEl = document.querySelector(CookieMessageEl);
+		}
 
 		this.CookieMessageEl = CookieMessageEl;
 
@@ -21,32 +21,32 @@ class CookieMessage {
 		CookieMessage.setupMessage();
 	}
 
-    static setupMessage () {
-        document.querySelector('[data-o-component="o-cookie-message-close"]').addEventListener('click', this.flagUserAsConsentingToCookies);
-    }
+	static setupMessage () {
+		document.querySelector('[data-o-component="o-cookie-message-close"]').addEventListener('click', this.flagUserAsConsentingToCookies);
+	}
 
-    static hideMessage () {
-        const message = document.querySelector('[data-o-component="o-cookie-message"]');
-        message.classList.add('o-cookie-message--hidden');
-    }
+	static hideMessage () {
+		const message = document.querySelector('[data-o-component="o-cookie-message"]');
+		message.classList.add('o-cookie-message--hidden');
+	}
 
-    static flagUserAsConsentingToCookies () {
-        localStorage.setItem('COOKIE_CONSENT', 1);
-        CookieMessage.hideMessage();
-    }
+	static flagUserAsConsentingToCookies () {
+		localStorage.setItem('COOKIE_CONSENT', 1);
+		CookieMessage.hideMessage();
+	}
 
-    static userHasConsentedToCookies () {
-        if (localStorage.getItem('COOKIE_CONSENT') === '1') {
-            return true;
-        }
+	static userHasConsentedToCookies () {
+		if (localStorage.getItem('COOKIE_CONSENT') === '1') {
+			return true;
+		}
 
-        // HACK: Whilst FT.com is still around auto-opt user in if they have accepted over there.
-        if (/\bcookieconsent=accepted(?:;|$)/.test(document.cookie)){
-            this.flagUserAsConsentingToCookies();
-            return true;
-        }
-        return false;
-    }
+		// HACK: Whilst FT.com is still around auto-opt user in if they have accepted over there.
+		if (/\bcookieconsent=accepted(?:;|$)/.test(document.cookie)){
+			this.flagUserAsConsentingToCookies();
+			return true;
+		}
+		return false;
+	}
 
 	static init (rootEl) {
 		if (!rootEl) {
