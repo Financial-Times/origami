7d26373bbe369cf17ad0e164a317bd1e2a6d2b4c
rowanbeentje <rowan@beent.je>
2016-08-26 14:23:26 +0100

    Improve ad error handling logic and add comments to startAds()

--- src/js/ads.js
+++ src/js/ads.js
@@ -149,10 +149,21 @@ class VideoAds {
 	}
 
 	startAds() {
+
+		// For ads to play correctly both the video and the advert video need to be ready to
+		// play; this function needs to be called after the two flags in adsManagerLoadedHandler()
+		// and playAdEventHandler() have been set.
+		// So if the video hasn't loaded yet, wait until it has.
 		if (!this.videoLoaded) {
 			return;
+		}
+
+		// If ads have failed to load, which resets the advertising support flag, play the video
+		// instead; otherwise, wait until the ads have loaded.
+		if (!this.video.opts.advertising) {
+			this.video.videoEl.play();
 		} else if (!this.adsLoaded) {
-			return this.video.videoEl.play();
+			return;
 		}
 
 		try {
@@ -232,6 +243,7 @@ class VideoAds {
 		}
 		this.video.placeholderEl && this.video.placeholderEl.removeEventListener('click', this.playAdEventHandler);
 		this.video.opts.advertising = false;
+		this.startAds();
 	}
 
 	contentPauseRequestHandler() {
