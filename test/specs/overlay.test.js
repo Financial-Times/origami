bcd6cb950be0aad1d60247657cdde4a04f418cf6
Adam Onishi <onishiweb@gmail.com>
2016-10-06 10:51:59 +0100

    Remove o-tweet (deprecated) from tests

--- test/specs/overlay.test.js
+++ test/specs/overlay.test.js
@@ -242,7 +242,7 @@ describe("smoke-tests (./overlay.js)", () => {
 
 	it('should be able to inject content from a url', done => {
 		const mod = new Overlay('testOverlay', {
-			src: 'https://origami-build.ft.com/files/o-tweet@0.2.5/demos/demo.html',
+			src: 'https://origami-build.ft.com/files/o-card@2.2.3/demos/standard.html',
 			trigger: document.querySelector('.o-overlay-trigger')
 		});
 
@@ -250,7 +250,7 @@ describe("smoke-tests (./overlay.js)", () => {
 			let overlays = document.querySelectorAll('.o-overlay');
 			expect(overlays.length).to.be(1);
 
-			expect(mod.content.innerHTML).to.contain('<div class="o-tweet__h-card">');
+			expect(mod.content.innerHTML).to.contain('<div class="o-card__container">');
 
 			mod.close();
 			overlays = document.querySelectorAll('.o-overlay');
