e9f14636f2ec56870458d99e24500eeed7261bed
Mark Stephens <Mark.Stephens@ft.com>
2016-10-18 13:02:40 +0100

    1.1.15

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const send = require('./src/javascript/core/send');
  * The version of the tracking module.
  * @type {string}
  */
-const version = '1.1.14';
+const version = '1.1.15';
 /**
  * The source of this event.
  * @type {string}
