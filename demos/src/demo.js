8c1fc9452bb6e6be23af0c4c0491d950611c6847
Adam Onishi <onishiweb@gmail.com>
2016-10-25 16:46:53 +0100

    Header styling

--- demos/src/demo.js
+++ demos/src/demo.js
@@ -1,10 +1,7 @@
 /*global require*/
-import './../../main.js';
 
 function initDemos() {
 	document.addEventListener('DOMContentLoaded', function() {
 		document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
 	});
 }
-
-initDemos();
