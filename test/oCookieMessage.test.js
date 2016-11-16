27e6e255c9d081100ba7654e14b36d7a3ecbeb82
alicebartlett <alice.bartlett@gmail.com>
2016-11-16 15:53:24 +0000

    Add stubs for future tests

--- test/oCookieMessage.test.js
+++ test/oCookieMessage.test.js
@@ -6,10 +6,28 @@ import proclaim from 'proclaim';
 import CookieMessage from './../src/js/cookieMessage';
 
 describe("CookieMessage", () => {
-	it('is defined', () => {
-		proclaim.isFunction(CookieMessage);
+	it("injects the FT legal cookie message into itself");
+	it("does not inject the FT legal cookie message if data-o-cookie-message-use-custom-html is present");
+
+	describe("dateIsWithinLastThreeMonths", () => {
+		it("returns true if the date passed in is within the last 3 months");
+		it("returns false if the date passed in is longer than the last three months");
 	});
-	it('has a static init method', () => {
-		proclaim.isFunction(CookieMessage.init);
+
+	describe("userHasConsentedToCookies", () => {
+		it("calls flagUserAsConsentingToCookies if they have consented using the old o-cookies way");
+		it("checks the value of COOKIE_CONSENT is within the last three months");
+		it("returns false if there is nothing in COOKIE_CONSENT");
+		it("returns true if dateIsWithinLastThreeMonths(COOKIE_CONSENT) returns true");
+		it("returns false if dateIsWithinLastThreeMonths(COOKIE_CONSENT) returns false")
+	});
+
+	describe("flagUserAsConsentingToCookies", () => {
+		it("sets a value in localStorage called COOKIE_CONSENT to the result of Date.now");
+		it("calls hideMessage");
+	});
+
+	describe("hideMessage", () => {
+		it("removes the o-cookie-message--active class from the CookieMessageEl");
 	});
 });
