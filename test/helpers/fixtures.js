748ee412d340ae73677b3efc1d9a109796a47b94
alicebartlett <alice.bartlett@gmail.com>
2016-11-16 15:07:51 +0000

    Add tests for standardised Origami component behaviour
    These tests are related to initialisation, and should be very similar for all
    Origami components and not specific to oCookieMessage, so I'm putting them in
    their own file.
    
    Component specific behaviour can live in oCookieMessgae.test.js

---
+++ test/helpers/fixtures.js
@@ -0,0 +1,32 @@
+let sandboxEl;
+
+function createSandbox() {
+	if (document.querySelector('.sandbox')) {
+		sandboxEl = document.querySelector('.sandbox');
+	} else {
+		sandboxEl = document.createElement('div');
+		sandboxEl.setAttribute('class', 'sandbox');
+		document.body.appendChild(sandboxEl);
+	}
+}
+
+function reset() {
+	sandboxEl.innerHTML = '';
+}
+
+function insert(html) {
+	createSandbox();
+	sandboxEl.innerHTML = html;
+}
+
+
+function htmlCode () {
+	const html = `<div class="cookie-message-container"><div data-o-component="o-cookie-message" class='o-cookie-message o-cookie-message--banner-centric'>
+  </div></div>`;
+	insert(html);
+}
+
+export {
+	htmlCode,
+	reset
+ };
