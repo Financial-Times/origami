e116c8668a26f9702645808d6d97ad312d653a5a
Alice Bartlett <alice.bartlett@ft.com>
2016-11-25 15:16:08 +0000

    add istanbul

--- karma.conf.js
+++ karma.conf.js
@@ -21,7 +21,8 @@ module.exports = function(config) {
 			'karma-mocha',
 			'karma-phantomjs-launcher',
 			'karma-webpack',
-			'karma-sinon'
+			'karma-sinon',
+			'karma-coverage'
 		],
 
 
@@ -45,10 +46,13 @@ module.exports = function(config) {
 
 
 		// test results reporter to use
-		// possible values: 'dots', 'progress'
+		// possible values: 'dots', 'progress', 'coverage'
 		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
-		reporters: ['progress'],
+		reporters: ['progress', 'coverage'],
 
+		coverageReporter: {
+			type : 'text-summary'
+		},
 
 		// web server port
 		port: 9876,
@@ -91,6 +95,13 @@ module.exports = function(config) {
 					{
 						test: /\.json$/,
 						loader: 'json'
+					}
+				],
+				postLoaders: [
+					{ //delays coverage til after tests are run, fixing transpiled source coverage error
+						test: /\.js$/,
+						exclude: /(test|node_modules|bower_components)\//,
+						loader: 'istanbul-instrumenter'
 					}
 				],
 				noParse: [
