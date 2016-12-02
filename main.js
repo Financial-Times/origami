327885931604467dddc70b45431a8c453d1f24e6
Joanna Kao <joannaskao@gmail.com>
2016-12-02 08:39:55 -0500

    don't need to assign igaudio instance to a var (never used)

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const constructAll = () => {
   for (let i = 0; i < audio_components.length; i++) {
     const a = audio_components[i];
     const audioURL = a.getElementsByTagName('source')[0].getAttribute('src');
-    const igaudio = new IGAudio(a, audioURL);
+    new IGAudio(a, audioURL);
   }
 
   document.removeEventListener('o.DOMContentLoaded', constructAll);
