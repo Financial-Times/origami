6f70e1010473f2d2607d7c252c9d2ecb639a8f06
Joanna Kao <joannaskao@gmail.com>
2016-12-02 08:12:07 -0500

    don't keep track of IGAudio instances. no need

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
 import IGAudio from './src/js/audio';
 
 const constructAll = () => {
-  const IGAudioObjects = [];
-
   const audio_components = document.querySelectorAll('.ig-audio');
 
   for (let i = 0; i < audio_components.length; i++) {
     const a = audio_components[i];
     const audioURL = a.getElementsByTagName('source')[0].getAttribute('src');
     const igaudio = new IGAudio(a, audioURL);
-
-    // keep track of igaudio objects
-    IGAudioObjects.push(igaudio);
   }
 
   document.removeEventListener('o.DOMContentLoaded', constructAll);
