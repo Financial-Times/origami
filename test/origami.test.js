3e15785a0c03bb71e6689903861de66d1cc536e6
Alice Bartlett <alice.bartlett@ft.com>
2016-12-16 15:28:45 +0000

    Add standard Origami tests
    All origami components have some shared bits of API
    - Auto initialise on oDomContentLoaded
    - Have static init method
    - Return a single o-thing, or an array of o-things
    
    This commit adds these tests and a fixture for them.

---
+++ test/origami.test.js
@@ -0,0 +1,57 @@
+/* eslint-env mocha, sinon, proclaim */
+import proclaim from 'proclaim';
+import sinon from 'sinon/pkg/sinon';
+import * as fixtures from './helpers/fixtures';
+
+const oToggle = require('./../main');
+
+describe("oToggle", () => {
+	it('is defined', () => {
+		proclaim.equal(typeof oToggle, 'function');
+	});
+
+	it('has a static init method', () => {
+		proclaim.equal(typeof oToggle.init, 'function');
+	});
+
+	it("should autoinitialize", (done) => {
+		const initSpy = sinon.spy(oToggle, 'init');
+		document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
+		setTimeout(function(){
+			proclaim.equal(initSpy.called, true);
+			initSpy.restore();
+			done();
+		}, 100);
+	});
+
+	it("should not autoinitialize when the event is not dispached", () => {
+		const initSpy = sinon.spy(oToggle, 'init');
+		proclaim.equal(initSpy.called, false);
+	});
+
+	describe("initialisation", () => {
+		beforeEach(() => {
+			fixtures.htmlCode();
+		});
+
+		afterEach(() => {
+			fixtures.reset();
+		});
+
+		it("creates an array of oToggles when passed no arguments", () => {
+			const toggles = oToggle.init();
+			for (let toggle of toggles) {
+				proclaim.equal(toggle instanceof oToggle, true);
+			}
+		});
+
+		it("creates an array of Toggles limited to the selector when a selector is passed in", () => {
+			const toggles = oToggle.init('.toggle');
+			for (let toggle of toggles) {
+				proclaim.equal(toggle instanceof oToggle, true);
+			}
+
+			proclaim.equal(toggles.length, document.querySelector('.toggle').querySelectorAll('[data-o-component="o-toggle"]').length);
+		});
+	});
+});
