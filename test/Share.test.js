75f9a0f3f1d450e6bcd69c5268c44c340b87aa92
rowanbeentje <rowan@beent.je>
2016-12-19 11:28:30 +0000

    Add whatsapp link pattern, completing whatsapp support covered by the readme; add tests covering basic component use

--- test/Share.test.js
+++ test/Share.test.js
@@ -68,6 +68,55 @@ describe('links', () => {
 	});
 });
 
+describe('component', () => {
+	let twitterLinkEl;
+	let spy;
+
+	beforeEach(() => {
+		fixtures.insertShareComponent();
+		shareEl = document.querySelector('[data-o-component=o-share]');
+		new Share(shareEl);
+
+		spy = newWindowSpy();
+		window.open = spy.func;
+	});
+
+	it('generates markup when instantiating a component', () => {
+		expect(document.querySelector('.o-share__action--twitter')).to.exist;
+		expect(document.querySelector('.o-share__action--facebook')).to.exist;
+		expect(document.querySelector('.o-share__action--linkedin')).to.exist;
+		expect(document.querySelector('.o-share__action--whatsapp')).to.exist;
+		expect(document.querySelector('.o-share__action--googleplus')).to.exist;
+		expect(document.querySelector('.o-share__action--reddit')).to.exist;
+		expect(document.querySelector('.o-share__action--pinterest')).to.exist;
+		expect(document.querySelector('.o-share__action--url')).to.exist;
+	});
+
+	it('responds correctly to twitter clicks in the component', () => {
+		const twitterLinkElement = document.querySelector('.o-share__action--twitter a');
+		const event = document.createEvent('Event');
+		event.initEvent('click', true, true);
+		twitterLinkElement.dispatchEvent(event);
+
+		expect(spy.callCount).to.be(1);
+		expect(spy.calledWith[0]).to.be(twitterLinkElement.getAttribute('href'));
+		expect(spy.calledWith[1]).to.be('');
+		expect(spy.calledWith[2]).to.be('width=646,height=436');
+	});
+
+	it('responds correctly to url clicks in the component', () => {
+		expect(document.querySelector('.o-share-tooltip')).not.to.exist;
+
+		const urlLinkElement = document.querySelector('.o-share__action--url a');
+		const event = document.createEvent('Event');
+		event.initEvent('click', true, true);
+		urlLinkElement.dispatchEvent(event);
+
+		expect(document.querySelector('.o-share-tooltip')).to.exist;
+		expect(document.querySelector('.o-share__action--url input').value).to.equal('https://www.ft.com/content/test');
+	});
+});
+
 function newWindowSpy() {
 	const self = {
 		callCount: 0,
