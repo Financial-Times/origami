d200ec9b8c7737925cc5e607fb83994046c03bf6
Alice Bartlett <alice.bartlett@ft.com>
2017-01-16 15:18:54 +0000

    Add initial origami tests

---
+++ test/helpers/fixtures.js
@@ -0,0 +1,49 @@
+let sandboxEl;
+
+function createSandbox() {
+	if (document.querySelector('.sandbox')) {
+		sandboxEl = document.querySelector('.sandbox');
+	} else {
+		sandboxEl = document.createElement('div');
+		sandboxEl.setAttribute('class', 'sandbox');
+		document.body.appendChild(sandboxEl);
+	}
+}
+
+function reset() {
+	sandboxEl.innerHTML = '';
+}
+
+function insert(html) {
+	createSandbox();
+	sandboxEl.innerHTML = html;
+}
+
+
+function declarativeCode () {
+	const html = `<div class='tooltip-target' id="demo-tooltip-target">
+			Thing to point the tooltip at.
+		</div>
+
+		<div id='tooltip-demo' data-o-component="o-tooltip"
+		data-o-tooltip-arrow-position="top"
+		data-o-tooltip-target='demo-tooltip-target'>
+			Tooltip content
+		</div>
+	`;
+	insert(html);
+}
+
+function imperativeCode () {
+	const html = `<div class='tooltip-target' id="demo-tooltip-target">
+			Thing to point the tooltip at.
+		</div>
+	`;
+	insert(html);
+}
+
+export {
+	declarativeCode,
+	imperativeCode,
+	reset
+ };
