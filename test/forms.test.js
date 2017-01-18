8a8225e4a53691f1ce9188f3f31b779e5e5b4447
Adam Onishi <onishiweb@gmail.com>
2017-01-18 17:06:10 +0000

    Start testing

--- test/forms.test.js
+++ test/forms.test.js
@@ -6,6 +6,30 @@ import * as fixtures from './helpers/fixtures';
 const Forms = require('./../main');
 
 describe("Forms", () => {
-	it('xxxx', () => {
+	// beforeEach(() => {
+	// });
+
+	// afterEach(() => {
+	// });
+
+	it('sets default testEvent to blur', () => {
+		fixtures.htmlCode();
+
+		const formEl = document.querySelector('[data-o-component="o-forms"]');
+		const testForms = new Forms(formEl);
+
+		proclaim.equal(testForms.opts.testEvent, 'blur');
+	});
+
+	it('destroys itself when not initialised on a <form> element', () => {
+		const html = `<form><input type="text" data-o-component="o-forms" /></form>`;
+		fixtures.insert(html);
+
+		const formEl = document.querySelector('[data-o-component="o-forms"]');
+		const testForms = new Forms(formEl);
+
+		proclaim.isUndefined(testForms.FormEl);
+		proclaim.isUndefined(testForms.opts);
+		proclaim.isUndefined(testForms.validFormEls);
 	});
 });
--- test/helpers/fixtures.js
+++ test/helpers/fixtures.js
@@ -23,13 +23,13 @@ function insert(html) {
 function htmlCode () {
 	const html = `<div>
 		<h1>Basic Demo</h1>
-		<form>
-			<div class="o-forms" data-o-component="o-forms" id="element">
+		<form data-o-component="o-forms" id="element">
+			<div class="o-forms">
 				<label for="o-forms-standard" class="o-forms__label">Text input</label>
 				<input type="text" id="o-forms-standard" placeholder="placeholder" class="o-forms__text" />
 			</div>
 
-			<div class="o-forms" data-o-component="o-forms">
+			<div class="o-forms">
 				<label for="o-forms-standard" class="o-forms__label">Text input</label>
 				<input type="email" id="o-forms-standard" placeholder="placeholder" class="o-forms__text" />
 			</div>
@@ -40,6 +40,7 @@ function htmlCode () {
 }
 
 export {
+	insert,
 	htmlCode,
 	reset
  };
