e1e88de011ba5c55abe045b80ebb82ed99a90cdd
Adam Onishi <onishiweb@gmail.com>
2017-01-18 11:44:29 +0000

    Add correct event listeners based on settings and construction

--- src/js/forms.js
+++ src/js/forms.js
@@ -2,7 +2,65 @@ class Forms {
 
 	constructor (FormEl, opts) {
 		this.FormEl = FormEl;
-		this.opts = opts || {values: "default"};
+		this.opts = opts || { testEvent: "blur" };
+		this.validFormEls = [
+			HTMLFormElement,
+			HTMLInputElement,
+			HTMLSelectElement,
+			HTMLButtonElement,
+			HTMLTextAreaElement
+		];
+
+		console.log('hello forms!');
+
+		// Find the closest <form> element unless
+		// the constructed element is a <form> element already
+		this.ParentForm = (this.FormEl instanceof HTMLFormElement) ? this.FormEl : this.FormEl.closest('form');
+
+		// Add the event listeners
+		this.listeners();
+	}
+
+	// element.closest(form) - to find the form element
+	// Events: blur or submit
+
+	listeners() {
+		if (this.opts.testEvent === 'submit') {
+			this.ParentForm.addEventListener('submit', this.checkAllInputs, false);
+			return;
+		}
+
+		// If the constructed element is a form element add
+		// a listener on blur. Otherwise find any child inputs
+		if (this.isValidFormElement()) {
+			this.FormEl.addEventListener('blur', this.checkSingleInput, false);
+		} else {
+			this.findInputs().map((input) => {
+				input.addEventListener('blur', this.checkSingleInput, false);
+			});
+		}
+	}
+
+	checkAllInputs(event) {
+		event.preventDefault();
+
+		console.log('hello');
+	}
+
+	checkSingleInput(event) {
+		console.log('hello single');
+	}
+
+	checkInputValidity(input) {
+		console.log("input validity", input);
+	}
+
+	isValidFormElement() {
+		return this.validFormEls.some((element) => this.FormEl instanceof element);
+	}
+
+	findInputs() {
+		return Array.from(this.FormEl.querySelectorAll('input, select, textarea, button, form'));
 	}
 
 	static init (rootEl, opts) {
