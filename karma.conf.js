15997fda2350cdfdecd5c2caba1d89038204be21
Adam Onishi <onishiweb@gmail.com>
2017-01-19 12:15:02 +0000

    Fix polyfill.io url to include correct polyfills

--- karma.conf.js
+++ karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function(config) {
 
 		// list of files / patterns to load in the browser
 		files: [
-			'http://cdn.polyfill.io/v2/polyfill.js?features=fetch&flags=gated',
+			'https://cdn.polyfill.io/v2/polyfill.js?flags=gated&ua=safari/4',
 			'test/*.test.js'
 		],
 
