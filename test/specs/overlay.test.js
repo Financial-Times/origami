e5756001043add43e68afbba994328ac07cdb10c
Adam Onishi <onishiweb@gmail.com>
2017-01-27 12:06:02 +0000

    Fix old build service url

--- test/specs/overlay.test.js
+++ test/specs/overlay.test.js
@@ -162,7 +162,7 @@ describe("smoke-tests (./overlay.js)", () => {
 			});
 			o.fireCustomEvent(document.body, 'oLayers.new', {el: 'something'});
 
-			// expect(Overlay.prototype.close.callCount).to.be(4);
+			expect(Overlay.prototype.close.callCount).to.be(4);
 
 			Overlay.prototype.close = realCloseFunction;
 			currentOverlay.close();
@@ -281,7 +281,7 @@ describe("smoke-tests (./overlay.js)", () => {
 
 	it('should be able to inject content from a url', done => {
 		const mod = new Overlay('testOverlay', {
-			src: 'https://origami-build.ft.com/files/o-card@2.2.3/demos/standard.html',
+			src: 'https://www.ft.com/__origami/service/build/v2/files/o-card@2.2.3/demos/standard.html',
 			trigger: document.querySelector('.o-overlay-trigger')
 		});
 
