768fed681e463760d308bede76af8204e7516aae
Adam Onishi <onishiweb@gmail.com>
2017-02-15 15:27:01 +0000

    Remove mail from demos

--- src/js/share.js
+++ src/js/share.js
@@ -10,8 +10,7 @@ const socialUrls = {
 	googleplus: "https://plus.google.com/share?url={{url}}",
 	pinterest: "http://www.pinterest.com/pin/create/button/?url={{url}}&description={{title}}",
 	whatsapp: "whatsapp://send?text={{title}}%20({{titleExtra}})%20-%20{{url}}",
-	link: "{{url}}",
-	mail: "{{url}}"
+	link: "{{url}}"
 };
 
 /**
