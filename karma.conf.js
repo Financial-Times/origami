fbb3d79d915fa822bddb53c57795df8770ecd962
Jake Champion <jakechampion.jake2@googlemail.com>
2017-02-21 12:01:43 +0000

    use new polyfill domain

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function(config) {
 
 		// list of files / patterns to load in the browser
 		files: [
-			'http://polyfill.webservices.ft.com/v1/polyfill.js?ua=safari/4',
+			'https://polyfill.io/v2/polyfill.js?ua=safari/4',
 			'test/*.js'
 		],
 
