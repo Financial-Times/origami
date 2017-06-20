e5994bef62e2a3baa2e324ddc884d501e1fdf8c6
Matt Hinchliffe <i-like-robots@users.noreply.github.com>
2017-06-20 13:06:16 +0100

    Bugfix: bail if startAds method is called multiple times (V4) (#109)
    * Bugfix: bail if startAds method is called multiple times
    
    * Check loading spinner exists before attempting to remove it

--- src/js/ads.js
+++ src/js/ads.js
@@ -187,8 +187,10 @@ class VideoAds {
 		}
 
 		// Remove the preloading spinner
-		this.loadingStateEl.parentNode.removeChild(this.loadingStateEl);
-		this.loadingStateEl = null;
+		if (this.loadingStateEl) {
+			this.loadingStateEl.parentNode.removeChild(this.loadingStateEl);
+			this.loadingStateEl = null;
+		}
 
 		try {
 			// Initialize the ads manager. Ad rules playlist will start at this time.
