3de6f3977b81411dbd723929d1df71b6329f2f06
Gabrielle von Koss <gabivk@gmail.com>
2017-08-08 17:37:37 +0100

    almost there with expander..just missing the actual expanding

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-const expanders = document.querySelectorAll('.o-subs-card__read-more')
-
+const expanders = document.querySelectorAll('.o-subs-card__expander')
 const oExpander = require('o-expander');
-oExpander.init()
+
+const opts = {
+	shrinkTo: ['hidden'],
+	expandedToggleText: ['Read less'],
+	collapsedToggleText: ['Read more'],
+	toggleState: 'all',
+	toggleSelector: '.o-subs-card__read-more',
+	toggleClassName: 'o-subs-card__read-more',
+	contentClassName: 'o-subs-card__copy-details'
+}
 
 expanders.forEach((expander) => {
-	console.log(expander);
+	expander.setAttribute('data-o-component', 'o-expander');
+	oExpander.init(expander, opts);
+
 	expander.addEventListener('oExpander.expand', (e) => { console.log(e)});
 	expander.addEventListener('oExpander.collapse', (e) => { console.log(e)});
 })
