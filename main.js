f6967393aa93ee2953ea6312b8c8652ffdbd93b8
Gabrielle von Koss <gabivk@gmail.com>
2017-08-09 11:10:39 +0100

    incorporated and functional expander

--- main.js
+++ main.js
@@ -2,19 +2,18 @@ const expanders = document.querySelectorAll('.o-subs-card__expander')
 const oExpander = require('o-expander');
 
 const opts = {
-	shrinkTo: ['hidden'],
-	expandedToggleText: ['Read less'],
-	collapsedToggleText: ['Read more'],
+	shrinkTo: 'hidden',
+	expandedToggleText: 'Read less',
+	collapsedToggleText: 'Read more',
 	toggleState: 'all',
 	toggleSelector: '.o-subs-card__read-more',
-	toggleClassName: 'o-subs-card__read-more',
-	contentClassName: 'o-subs-card__copy-details'
+	contentClassName: 'o-subs-card__copy-details',
 }
 
 expanders.forEach((expander) => {
 	expander.setAttribute('data-o-component', 'o-expander');
 	oExpander.init(expander, opts);
 
-	expander.addEventListener('oExpander.expand', (e) => { console.log(e)});
-	expander.addEventListener('oExpander.collapse', (e) => { console.log(e)});
+	expander.addEventListener('oExpander.expand', () => {});
+	expander.addEventListener('oExpander.collapse', () => {});
 })
