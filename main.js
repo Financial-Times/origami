8793934cfd33fa0f9588486cae89660eaaebea81
Gabrielle von Koss <gabivk@gmail.com>
2017-08-09 14:16:40 +0100

    main js?

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
-const expanders = document.querySelectorAll('.o-subs-card__expander')
 const oExpander = require('o-expander');
 
-const opts = {
-	shrinkTo: 'hidden',
-	expandedToggleText: 'Read less',
-	collapsedToggleText: 'Read more',
-	toggleState: 'all',
-	toggleSelector: '.o-subs-card__read-more',
-	contentClassName: 'o-subs-card__copy-details',
+const SubsCard = function () {
+	const expanders = document.querySelectorAll('.o-subs-card__expander')
+	const opts = {
+		shrinkTo: 'hidden',
+		expandedToggleText: 'Read less',
+		collapsedToggleText: 'Read more',
+		toggleState: 'all',
+		toggleSelector: '.o-subs-card__read-more',
+		contentClassName: 'o-subs-card__copy-details',
+	}
+
+	expanders.forEach((expander) => {
+		expander.setAttribute('data-o-component', 'o-expander');
+		oExpander.init(expander, opts	);
+
+		expander.addEventListener('oExpander.expand', () => {});
+		expander.addEventListener('oExpander.collapse', () => {});
+	})
 }
 
-expanders.forEach((expander) => {
-	expander.setAttribute('data-o-component', 'o-expander');
-	oExpander.init(expander, opts);
+const constructAll = function() {
+	SubsCard();
+	document.removeEventListener('o.DOMContentLoaded', constructAll);
+};
 
-	expander.addEventListener('oExpander.expand', () => {});
-	expander.addEventListener('oExpander.collapse', () => {});
-})
+document.addEventListener('o.DOMContentLoaded', constructAll);
+
+module.exports = SubsCard;
