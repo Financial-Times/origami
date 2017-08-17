0e137dca8a39ca5fff9b32971477fdb5b145fb6b
Rowan Manning <accounts@rowanmanning.co.uk>
2017-08-17 11:06:17 +0100

    Split and rename the themes (#4)
    This offers more flexibility, and matches theme names for other
    components. The following changes were made:
    
      - `o-banner--small`: New theme to resize banner
      - `o-banner--marketing-secondary`: Use secondary marketing colors
      - `o-banner--marketing-primary`: Use primary marketing colors
    
    To match designs, the marketing banner themes must be combined with the
    small theme. E.g.
    
    ```
    <div class="o-banner o-banner--small o-banner--marketing-secondary">
        ...
    </div>
    ```

--- demos/src/imperative.js
+++ demos/src/imperative.js
@@ -15,24 +15,17 @@ function initDemos() {
 			});
 		});
 
-		document.getElementById('banner-promo').addEventListener('click', () => {
+		document.getElementById('banner-small').addEventListener('click', () => {
 			new Banner(null, {
-				contentLong: `
-					<p><b>Save 25% today</b> Limited time only.  Resubscribe now and get:</p>
-					<ul>
-						<li>Reduced price full Standard FT digital package</li>
-						<li>Access to the FT on your desktop and mobile devices</li>
-						<li>Our 5-year company financials archive</li>
-					</ul>
-				`,
-				contentShort: '<p><b>Save 25% today</b> Limited time only.</p>',
-				buttonLabel: 'Subscribe now',
-				buttonUrl: '#subscribe-button',
-				theme: 'promo'
+				contentLong: 'Try the new compact homepage. A list view of today\'s homepage with fewer images.',
+				contentShort: 'Try the new compact homepage.',
+				buttonLabel: 'Try it now',
+				buttonUrl: '#try-button',
+				theme: 'small'
 			});
 		});
 
-		document.getElementById('banner-promo-standout').addEventListener('click', () => {
+		document.getElementById('banner-marketing-secondary').addEventListener('click', () => {
 			new Banner(null, {
 				contentLong: `
 					<p><b>Save 25% today</b> Limited time only.  Resubscribe now and get:</p>
@@ -45,7 +38,24 @@ function initDemos() {
 				contentShort: '<p><b>Save 25% today</b> Limited time only.</p>',
 				buttonLabel: 'Subscribe now',
 				buttonUrl: '#subscribe-button',
-				theme: ['promo', 'promo-standout']
+				theme: ['small', 'marketing-secondary']
+			});
+		});
+
+		document.getElementById('banner-marketing-primary').addEventListener('click', () => {
+			new Banner(null, {
+				contentLong: `
+					<p><b>Save 25% today</b> Limited time only.  Resubscribe now and get:</p>
+					<ul>
+						<li>Reduced price full Standard FT digital package</li>
+						<li>Access to the FT on your desktop and mobile devices</li>
+						<li>Our 5-year company financials archive</li>
+					</ul>
+				`,
+				contentShort: '<p><b>Save 25% today</b> Limited time only.</p>',
+				buttonLabel: 'Subscribe now',
+				buttonUrl: '#subscribe-button',
+				theme: ['small', 'marketing-primary']
 			});
 		});
 
