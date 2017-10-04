562844a661ace830ed7be9b065fabee569e5094e
Jake Champion <jakechampion.jake2@googlemail.com>
2017-10-04 10:50:57 +0100

    import oToggle instead of requiring

--- test/origami.test.js
+++ test/origami.test.js
@@ -3,7 +3,7 @@ import proclaim from 'proclaim';
 import sinon from 'sinon/pkg/sinon';
 import * as fixtures from './helpers/fixtures';
 
-const oToggle = require('./../main');
+import oToggle from './../main';
 
 describe("oToggle", () => {
 	it('is defined', () => {
