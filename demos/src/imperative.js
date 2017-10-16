36213e148471731bea34b376a1ba2594b5c60cc4
Rowan Manning <accounts@rowanmanning.co.uk>
2017-10-16 16:25:55 +0100

    Overhaul the visual styling of marketing banners (#20)
    * Update demos to reflect new text in designs
    
    * Update colours to reflect the new designs
    
    * Add styles for headings
    
    * Fix lint errors
    
    * Tweaks based on conversation with design

--- demos/src/imperative.js
+++ demos/src/imperative.js
@@ -28,19 +28,22 @@ function initDemos() {
 		document.getElementById('banner-marketing-secondary').addEventListener('click', () => {
 			new Banner(null, {
 				contentLong: `
-					<h1>You qualify for a 25% subscription discount.</h1>
-					<p>Pay just £3.99 per week for annual Standard  Digital access.</p>
+					<header class="o-banner__heading">
+						<p>Limited time only</p>
+						<h1>You qualify for a special offer: Save 33%</h1>
+					</header>
+					<p>Pay just $4.29 per week for annual Standard  Digital access.</p>
 					<ul>
-						<li>Access FT.com on your desktop, tablet &amp; mobile</li>
-						<li>5 year company financials archive</li>
-						<li>Personalised email briefings and market moving news</li>
+						<li>Global news and opinion from experts in 50+ countries</li>
+						<li>Access on desktop and mobile</li>
+						<li>Market-moving news, politics, tech, the arts and more</li>
 					</ul>
 				`,
 				contentShort: `
-					<h1>You qualify for a 25% subscription discount.</h1>
-					<p>Pay just £3.99 per week for annual Standard  Digital access.</p>
+					<h1>You qualify for a special offer: Save 33%</h1>
+					<p>Pay just $4.29 per week for annual Standard  Digital access.</p>
 				`,
-				buttonLabel: 'Save 25% now',
+				buttonLabel: 'Save 33% now',
 				buttonUrl: '#save-button',
 				theme: ['small', 'marketing-secondary']
 			});
@@ -49,19 +52,22 @@ function initDemos() {
 		document.getElementById('banner-marketing-primary').addEventListener('click', () => {
 			new Banner(null, {
 				contentLong: `
-					<h1>You qualify for a 25% subscription discount.</h1>
-					<p>Pay just £3.99 per week for annual Standard  Digital access.</p>
+					<header class="o-banner__heading">
+						<p>Limited time only</p>
+						<h1>You qualify for a special offer: Save 33%</h1>
+					</header>
+					<p>Pay just $4.29 per week for annual Standard  Digital access.</p>
 					<ul>
-						<li>Access FT.com on your desktop, tablet &amp; mobile</li>
-						<li>5 year company financials archive</li>
-						<li>Personalised email briefings and market moving news</li>
+						<li>Global news and opinion from experts in 50+ countries</li>
+						<li>Access on desktop and mobile</li>
+						<li>Market-moving news, politics, tech, the arts and more</li>
 					</ul>
 				`,
 				contentShort: `
-					<h1>You qualify for a 25% subscription discount.</h1>
-					<p>Pay just £3.99 per week for annual Standard  Digital access.</p>
+					<h1>You qualify for a special offer: Save 33%</h1>
+					<p>Pay just $4.29 per week for annual Standard  Digital access.</p>
 				`,
-				buttonLabel: 'Save 25% now',
+				buttonLabel: 'Save 33% now',
 				buttonUrl: '#save-button',
 				theme: ['small', 'marketing-primary']
 			});
--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import Banner from './src/js/banner';
 function constructAll () {
 	Banner.init();
 	document.removeEventListener('o.DOMContentLoaded', constructAll);
-};
+}
 
 document.addEventListener('o.DOMContentLoaded', constructAll);
 
