c879260d242e807ece50d63756cef3f950010782
Gabrielle von Koss <gabivk@gmail.com>
2018-02-08 15:12:48 +0000

    boilerplate springboard

---
+++ main.js
@@ -0,0 +1,11 @@
+// 
+// import oMessage from './src/js/omessage';
+//
+// const constructAll = function() {
+// 	oMessage.init();
+// 	document.removeEventListener('o.DOMContentLoaded', constructAll);
+// };
+//
+// document.addEventListener('o.DOMContentLoaded', constructAll);
+//
+// export default oMessage;
---
+++ test/oMessage.test.js
@@ -0,0 +1,8 @@
+/* eslint-env mocha */
+import proclaim from 'proclaim';
+import sinon from 'sinon/pkg/sinon';
+import * as fixtures from './helpers/fixtures';
+
+import oMessage from './../main';
+
+describe("oMessage", () => {}
