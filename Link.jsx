52508925e7bdf2cdb4a6ebf14d69e9b6cf2fadb4
Matt Hinchliffe <i-like-robots@users.noreply.github.com>
2018-06-20 12:05:17 +0100

    Allow custom link to override all link behaviour

--- Link.jsx
+++ Link.jsx
@@ -1,14 +1,14 @@
 import { h } from '@financial-times/x-engine';
 
-const BaseLink = ({ url, attrs = {}, children }) => (
-	<a href={url} {...attrs}>{children}</a>
-);
+const BaseLink = ({ url, attrs = {}, children }) => {
+	if (url) {
+		return <a href={url} {...attrs}>{children}</a>;
+	} else {
+		return <span {...attrs}>{children}</span>;
+	}
+};
 
 export default ({ customElements = {}, ...props }) => {
-	if (!props.url) {
-		return <span>{props.children}</span>;
-	}
-
 	const Link = customElements.Link || BaseLink;
 	return <Link {...props} />;
 };
