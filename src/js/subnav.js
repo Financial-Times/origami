5e933427fbab5c40855de72bde5be4639f59c4f8
Gabrielle von Koss <gabivk@gmail.com>
2018-07-10 16:34:52 +0100

    remove arrow flash (#309)
    * remove excess calculation
    
    * do a proper fix, to cover all cases
    
    * linting
    
    * avoid repeating logic
    
    * handle edge cases
    
    * keep food jokes to myself
    
    * no more waiting
    
    * rm disabling

--- src/js/subnav.js
+++ src/js/subnav.js
@@ -7,11 +7,30 @@ function init(headerEl) {
 		return;
 	}
 
-	const wrapper = subnav.querySelector('[data-o-header-subnav-wrapper]');
 	const buttons = Array.from(subnav.getElementsByTagName('button'));
+	const wrapper = subnav.querySelector('[data-o-header-subnav-wrapper]');
 
 	let scrollWidth;
-	let clientWidth;
+	let wrapperWidth = wrapper.clientWidth;
+
+	function checkCurrentPosition() {
+		const currentSelection = wrapper.querySelector('[aria-current]');
+		if (currentSelection) {
+			let currentSelectionEnd = currentSelection.getBoundingClientRect().right;
+
+			//if the current selection is wider than the end of the wrapper
+			if (currentSelectionEnd > wrapperWidth) {
+				// check by how much
+				let diff = currentSelectionEnd - wrapperWidth;
+				// if the difference is greater than half of the wrapper, scroll to the end of the current selection.
+				diff = (diff > wrapperWidth / 2) ? currentSelectionEnd : wrapperWidth / 2;
+
+				wrapper.scrollTo(diff, 0);
+			}
+
+			scrollable();
+		}
+	}
 
 	function direction(button) {
 		return button.className.match(/left|right/).pop();
@@ -19,13 +38,12 @@ function init(headerEl) {
 
 	function scrollable() {
 		scrollWidth = wrapper.scrollWidth;
-		clientWidth = wrapper.clientWidth;
 
 		buttons.forEach(button => {
 			if (direction(button) === 'left') {
 				button.disabled = wrapper.scrollLeft === 0;
 			} else {
-				const remaining = scrollWidth - clientWidth - wrapper.scrollLeft;
+				const remaining = scrollWidth - wrapperWidth - wrapper.scrollLeft;
 				// Allow a little difference as scrollWidth is fast, not accurate.
 				button.disabled = remaining <= 1;
 			}
@@ -38,27 +56,13 @@ function init(headerEl) {
 		if (direction(e.currentTarget) === 'left') {
 			distance = (wrapper.scrollLeft > distance ? distance : wrapper.scrollLeft) * -1;
 		} else {
-			const remaining = scrollWidth - clientWidth - wrapper.scrollLeft;
+			const remaining = scrollWidth - wrapperWidth - wrapper.scrollLeft;
 			distance = remaining > distance ? distance : remaining;
 		}
 
 		wrapper.scrollLeft = wrapper.scrollLeft + distance;
 
 		scrollable();
-	}
-
-	function scrollToCurrent() {
-		const currentSelection = wrapper.querySelector('[aria-current]');
-
-		if(currentSelection) {
-			let currentSelectionEnd = currentSelection.getBoundingClientRect().right;
-
-			let wrapperWidth = wrapper.getBoundingClientRect().width;
-
-			if (currentSelectionEnd > wrapperWidth) {
-				wrapper.scrollTo(currentSelectionEnd, 0);
-			}
-		}
 	}
 
 	wrapper.addEventListener('scroll', oUtils.throttle(scrollable, 100));
@@ -68,8 +72,7 @@ function init(headerEl) {
 		button.onclick = scroll;
 	});
 
-	scrollable();
-	scrollToCurrent();
+	checkCurrentPosition();
 }
 
 export default { init };
