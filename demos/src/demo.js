363c3aefabe52936e7ee66d5344476087308dd97
Rowan Manning <accounts+github@rowanmanning.com>
2018-07-25 12:26:04 +0100

    Fix the demo branding hack (#12)
    In the build service, the brand _does_ get added to the CSS URL so the
    JavaScript is adding it twice. Currently demos are broken and this
    should fix it

--- demos/src/demo.js
+++ demos/src/demo.js
@@ -2,7 +2,15 @@
 import './../../main.js';
 
 //mega hack because we currently have no way of requesting brands for origami.json dependencies
-document.querySelector('link').href += "&brand=internal";
+[...document.querySelectorAll('link')].forEach(link => {
+	const isBuildServiceLink = (
+		link.href.includes('origami-build.ft.com') ||
+		link.href.includes('ft.com/__origami/service/build')
+	);
+	if (isBuildServiceLink && !link.href.includes('brand=')) {
+		link.href += '&brand=internal';
+	}
+});
 
 document.addEventListener('DOMContentLoaded', () => {
 	document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
