d05e362efdc7abe1e76755aed8bde302102ceabf
notlee <lee.moody@ft.com>
2018-08-06 14:13:08 +0100

    Indentation and other tweaks

--- src/js/syntax-highlight.js
+++ src/js/syntax-highlight.js
@@ -1,11 +1,11 @@
 import throwError from './helpers';
 
 import prism from 'prism/prism.js';
+import diff from './languages/prism-diff.js';
 import loadLanguages from 'prism/components/index.js';
 require('prism/components/prism-json.js');
 require('prism/components/prism-scss.js');
 require('prism/components/prism-css.js');
-require('./languages/prism-diff.js');
 
 class SyntaxHighlight {
 	/**
@@ -15,7 +15,8 @@ class SyntaxHighlight {
 	 * @param {String} options.language - The language to tokenise the code for
 	 */
 	constructor (syntaxEl, options) {
-		delete window.Prism;
+		delete window.Prism; // Prism creates a global object which we remove https://github.com/PrismJS/prism/blob/v1.15.0/prism.js#L6
+		prism.languages.diff = diff; // Assign custom diff language
 		this.syntaxElement = syntaxEl;
 		this.opts = Object.assign({
 			language: '',
