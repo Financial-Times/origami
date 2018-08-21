4bed31a8b40a445329a78f6ad6f0d14950dd5865
Matt Hinchliffe <i-like-robots@users.noreply.github.com>
2018-08-21 11:36:50 +0100

    Add notice to open demos in new window when viewed inside an iframe (#3)

--- demos/src/demo.js
+++ demos/src/demo.js
@@ -1,9 +1,26 @@
 import LazyLoad from './../../main.js';
 
-// Viewport based demos cannot be setup declaratively because the document element
-// and body element cannot currently be modified.
-new LazyLoad(document.documentElement);
-
 document.addEventListener('DOMContentLoaded', function () {
+	const doc = document.documentElement;
+
+	// Viewport based demos cannot be viewed inside the registry iframes because
+	// they will attempt to fit the content height (which we haven't loaded yet!)
+	if (doc.classList.contains('demo-popout') && window.self !== window.top) {
+		doc.style.height = '200px';
+		doc.style.overflow = 'hidden';
+
+		const p = document.createElement('p');
+		p.classList.add('demo-popout-message');
+		p.textContent = 'Please open this demo in a new window';
+		document.body.insertBefore(p, document.body.firstElementChild);
+
+		return;
+	}
+
+	// Viewport based demos cannot be setup declaratively because the document
+	// element and body element cannot currently be configured.
+	new LazyLoad(doc);
+
+	// Trigger initialisation of demos with declarative configuration
 	document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
 });
