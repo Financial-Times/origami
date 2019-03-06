7c4021aa68d318859da91ef0e293e25c2337c42e
Jake Champion <jakechampion.jake2@googlemail.com>
2019-03-06 15:19:30 +0000

    Update index.js

--- src/javascript/core/transports/index.js
+++ src/javascript/core/transports/index.js
@@ -1,8 +1,19 @@
+import xhr from './xhr';
+import sendBeacon from './send-beacon';
+import image from './image';
+const get = function (name) {
+	return this.mock || this[name];
+};
+
 export default {
-	xhr: require('./xhr'),
-	sendBeacon: require('./send-beacon'),
-	image: require('./image'),
-	get: function (name) {
-		return this.mock || this[name];
-	}
+	xhr,
+	sendBeacon,
+	image,
+	get
+};
+export {
+	xhr,
+	sendBeacon,
+	image,
+	get
 };
