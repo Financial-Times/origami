6879ece84b784bf2ce3d2aa958b9a790f97dcb3d
Keran Braich <keran.braich@ft.com>
2019-05-17 15:43:11 +0100

    Update aria label to be more descriptive
    This was done so that the date/time is clearer to users that require a
    screen reader.

---
+++ test/markup.test.js
@@ -0,0 +1,42 @@
+/* eslint-env mocha */
+import proclaim from 'proclaim';
+import sinon from 'sinon/pkg/sinon';
+
+const oDate = require('../main');
+
+describe('o-date markup', () => {
+	let clock;
+
+	beforeEach(() => {
+		const fakeNow = new Date("Jul 14 2016 00:12:49");
+		clock = sinon.useFakeTimers(fakeNow);
+	});
+
+	afterEach(() => {
+		clock.restore();
+	});
+
+	describe('time-ago-abbreviated', () => {
+		it('updates the aria label with a non-abbreviated unit', () => {
+			const sandbox = document.createElement('div');
+			sandbox.innerHTML = '<time data-o-component="o-date" datetime="Jul 14 2016 00:01:49" class="o-date" data-o-date-format="time-ago-abbreviated"></time>'
+			const mockDateElement = sandbox.querySelector('[data-o-component="o-date"]');
+			new oDate(mockDateElement);
+
+			proclaim.equal(mockDateElement.getAttribute('aria-label'), '11 minutes ago');
+			proclaim.equal(mockDateElement.innerHTML, '11m ago');
+		});
+	});
+
+	describe('time-ago-abbreviated-limit-4-hours', () => {
+		it('updates the aria label with a non-abbreviated unit', () => {
+			const sandbox = document.createElement('div');
+			sandbox.innerHTML = '<time data-o-component="o-date" datetime="Jul 14 2016 00:01:49" class="o-date" data-o-date-format="time-ago-abbreviated-limit-4-hours"></time>'
+			const mockDateElement = sandbox.querySelector('[data-o-component="o-date"]');
+			new oDate(mockDateElement);
+
+			proclaim.equal(mockDateElement.getAttribute('aria-label'), '11 minutes ago');
+			proclaim.equal(mockDateElement.innerHTML, '11m ago');
+		});
+	});
+});
