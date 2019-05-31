55a0f705986693a3cb0bcdbdc3b824087598902d
Glynn Phillips <glynndominicphillips@gmail.com>
2019-05-31 15:09:17 +0100

    Add test coverage for markup changes
    Add some extra tests to cover the DOM manipulation

--- test/markup.test.js
+++ test/markup.test.js
@@ -4,39 +4,102 @@ import sinon from 'sinon/pkg/sinon';
 
 const ODate = require('../main');
 
-describe('o-date markup', () => {
+describe('o-date DOM', () => {
 	let clock;
+	let sandbox;
+	let mockDateElement;
 
 	beforeEach(() => {
-		const fakeNow = new Date("Jul 14 2016 00:12:49");
+		const fakeNow = new Date();
+		const elevenMinutesAgo = new Date(fakeNow);
+
+		elevenMinutesAgo.setMinutes(fakeNow.getMinutes() - 11);
 		clock = sinon.useFakeTimers(fakeNow);
+
+		sandbox = document.createElement('div');
+		sandbox.innerHTML = `<time data-o-component="o-date" datetime="${elevenMinutesAgo.toISOString()}" class="o-date"></time>`;
+		mockDateElement = sandbox.querySelector('[data-o-component="o-date"]');
 	});
 
 	afterEach(() => {
 		clock.restore();
+		sandbox = null;
+		mockDateElement = null;
+	});
+
+
+	describe('time-ago-limit-4-hours', () => {
+		beforeEach(() => {
+			mockDateElement.dataset.odateformat = 'time-ago-limit-4-hours';
+			new ODate(mockDateElement);
+		});
+
+		it('renders the date in the element', () => {
+			proclaim.equal(mockDateElement.innerHTML, '11 minutes ago');
+		});
 	});
 
 	describe('time-ago-abbreviated', () => {
-		it('updates the aria label with a non-abbreviated unit', () => {
-			const sandbox = document.createElement('div');
-			sandbox.innerHTML = '<time data-o-component="o-date" datetime="Jul 14 2016 00:01:49" class="o-date" data-o-date-format="time-ago-abbreviated"></time>';
-			const mockDateElement = sandbox.querySelector('[data-o-component="o-date"]');
-			new ODate(mockDateElement);
 
-			proclaim.equal(mockDateElement.getAttribute('aria-label'), '11 minutes ago');
+		beforeEach(() => {
+			mockDateElement.dataset.oDateFormat = 'time-ago-abbreviated';
+			new ODate(mockDateElement);
+		});
+
+		it('renders the date in the element', () => {
 			proclaim.equal(mockDateElement.innerHTML, '11m ago');
+		});
+
+		it('adds a aria label with a non-abbreviated unit', () => {
+			proclaim.equal(mockDateElement.getAttribute('aria-label'), '11 minutes ago');
 		});
 	});
 
 	describe('time-ago-abbreviated-limit-4-hours', () => {
-		it('updates the aria label with a non-abbreviated unit', () => {
-			const sandbox = document.createElement('div');
-			sandbox.innerHTML = '<time data-o-component="o-date" datetime="Jul 14 2016 00:01:49" class="o-date" data-o-date-format="time-ago-abbreviated-limit-4-hours"></time>';
-			const mockDateElement = sandbox.querySelector('[data-o-component="o-date"]');
+		beforeEach(() => {
+			mockDateElement.dataset.oDateFormat = 'time-ago-abbreviated-limit-4-hours';
 			new ODate(mockDateElement);
+		});
 
-			proclaim.equal(mockDateElement.getAttribute('aria-label'), '11 minutes ago');
+		it('renders the date in the element', () => {
 			proclaim.equal(mockDateElement.innerHTML, '11m ago');
+		});
+
+		it('adds a the aria label with a non-abbreviated unit', () => {
+			proclaim.equal(mockDateElement.getAttribute('aria-label'), '11 minutes ago');
+		});
+	});
+
+	describe('today-or-yesterday-or-nothing', () => {
+		beforeEach(() => {
+			mockDateElement.dataset.oDateFormat = 'today-or-yesterday-or-nothing';
+			new ODate(mockDateElement);
+		});
+
+		it('renders the date in the element', () => {
+			proclaim.equal(mockDateElement.innerHTML, 'today');
+		});
+	});
+
+	describe('date-only', () => {
+		beforeEach(() => {
+			mockDateElement.dataset.odateformat = 'date-only';
+			new ODate(mockDateElement);
+		});
+
+		it('renders the date in the element', () => {
+			proclaim.equal(mockDateElement.innerHTML, '11 minutes ago');
+		});
+	});
+
+	describe('time-ago-no-seconds', () => {
+		beforeEach(() => {
+			mockDateElement.dataset.odateformat = 'time-ago-no-seconds';
+			new ODate(mockDateElement);
+		});
+
+		it('renders the date in the element', () => {
+			proclaim.equal(mockDateElement.innerHTML, '11 minutes ago');
 		});
 	});
 });
