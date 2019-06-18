c4a7fe0a215b18eb5f45f84cb3d2aabc0b271b6b
Keran Braich <keran.braich@ft.com>
2019-06-18 14:23:30 +0100

    Update tests for `CommentCount` class

--- test/count.test.js
+++ test/count.test.js
@@ -1,159 +1,94 @@
 /* eslint-env mocha */
-import proclaim from 'proclaim';
-import * as fixtures from './helpers/fixtures';
-
+import * as assert from 'proclaim';
+import sinon from 'sinon/pkg/sinon';
 import {CommentCount} from '../main';
 
-describe("Count", () => {
-	it("is defined", () => {
-		proclaim.equal(typeof CommentCount, 'function');
-	});
+sinon.assert.expose(assert, {
+	includeFail: false,
+	prefix: ''
+});
 
-	it("has a static init method", () => {
-		proclaim.equal(typeof CommentCount.init, 'function');
+describe("CommentCount", () => {
+	it("exports a class constructor", () => {
+		assert.isFunction(CommentCount);
+		assert.throws(CommentCount, TypeError);
+	})
+
+	describe("new CommentCount(countEl, opts)", () => {
+		let mockDataAttributeOptions;
+		let mockCountElement;
+		let count;
+
+		beforeEach(() => {
+			mockDataAttributeOptions = {
+				isMockDataAttributeOptions: true
+			};
+			sinon.stub(CommentCount, 'getDataAttributes').returns(mockDataAttributeOptions);
+
+			const element = document.createElement('div');
+			element.innerHTML = '<div data-o-component=o-comments-count" data-o-comments-count-article-id="id"></div>';
+			mockCountElement = element.querySelector('[data-o-comments-count-article-id="id"]');
+
+			count = new CommentCount(mockCountElement);
+		});
+
+		afterEach(() => {
+			CommentCount.getDataAttributes.restore();
+		});
+
+		it("fetches options set via HTML data attributes", () => {
+			assert.calledOnce(CommentCount.getDataAttributes);
+			assert.calledWithExactly(CommentCount.getDataAttributes, mockCountElement);
+		});
+
+		describe(".options", () => {
+			it("is a defaulted options object", () => {
+				assert.isObject(count.options);
+				assert.deepEqual(count.options, {
+					isMockDataAttributeOptions: true
+				});
+				assert.notStrictEqual(count.options, mockDataAttributeOptions);
+			});
+		});
+
+		describe(".countEl", () => {
+			it("is set to the `countElement` that was passed into the constructor", () => {
+				assert.strictEqual(count.countEl, mockCountElement);
+			});
+		});
 	});
 
 	describe("._renderCount()", () => {
-		describe("when element doesn't exist", () => {
-			let count;
-
-			beforeEach(() => {
-				fixtures.htmlCode();
-				count = new CommentCount({
-					articleIds: 'fake-id'
-				});
-			});
-
-			afterEach(() => {
-				fixtures.reset();
-			});
-
-			it("will throw an error", () => {
-				proclaim.throws(() => count._renderCount(), 'Element must be a HTMLElement');
-			});
-		});
-
-		describe("when element doesn't exist in the DOM", () => {
-			beforeEach(() => {
-				fixtures.htmlCode();
-			});
-
-			afterEach(() => {
-				fixtures.reset();
-			});
-
-			it("will throw an error", () => {
-				proclaim.throws(() => new CommentCount({
-					element: '#imNotReal',
-					articleIds: 'fake-id'
-				}), 'Element must be a HTMLElement');
-			});
-		});
-
-		describe("when element does exist", () => {
-			let count;
-			let rootElement;
-
-			beforeEach(() => {
-				fixtures.htmlCode();
-				count = new CommentCount({
-					element: '#element',
-					articleIds: 'fake-id'
-				});
-
-				rootElement = count.element;
-			});
-
-			afterEach(() => {
-				fixtures.reset();
-			});
-
+		describe("when element exists", () => {
 			it("renders the count within the element", () => {
-				proclaim.isDefined(rootElement.innerHTML);
-			});
-		});
-	});
+				sinon.stub(CommentCount.prototype, '_fetchCount').returns(10);
 
-	describe(".getCount()", () => {
-		it("is defined", () => {
-			proclaim.equal(typeof new CommentCount().getCount, 'function');
-		});
+				const element = document.createElement('div');
+				element.innerHTML = '<div data-o-component="o-comments-count" data-o-comments-count-article-id="id"></div>';
+				const mockCountElement = element.querySelector('[data-o-comments-count-article-id="id"]');
 
-		describe("when no id's exist", () => {
-			it("returns null", () => {
-				const count = new CommentCount({}).getCount();
+				const count = new CommentCount(mockCountElement);
 
-				proclaim.isNull(count);
+				assert.equal(count.countEl.innerHTML, 10);
+
+				CommentCount.prototype._fetchCount.restore();
 			});
 		});
 
-		describe("when passed a single id", () => {
-			it("returns a number", () => {
-				const count = new CommentCount({
-					articleIds: 'fake-id'
-				}).getCount();
-
-				proclaim.isTypeOf(count, 'number');
-			});
-
-			describe("when the id is invalid", () => {
-				it("returns null", () => {
-					const count = new CommentCount({
-						articleIds: 'invalid-id'
-					}).getCount();
-
-					proclaim.isNull(count);
-				});
+		describe("when element does not exist", () => {
+			it("will throw an error", () => {
+				assert.throws(() => new CommentCount(), 'Element must be a HTMLElement');
 			});
 		});
 
-		describe("when passed an array of articleIds", () => {
-			it("returns an object", () => {
-				const count = new CommentCount({
-					articleIds: ['fake-id', 'fake-id-2']
-				}).getCount();
+		describe("when element does not exist in the DOM", () => {
+			it("will throw an error", () => {
+				const element = document.createElement('div');
+				element.innerHTML = '<div data-o-component="o-comments-count"></div>';
+				const mockCountElement = element.querySelector('[data-o-comments-count-article-id="id"]');
 
-				proclaim.isObject(count);
-			});
-
-			it("sets the articleIds as keys in the object", () => {
-				const count = new CommentCount({
-					articleIds: ['fake-id', 'fake-id-2']
-				}).getCount();
-
-				proclaim.isDefined(count['fake-id']);
-				proclaim.isDefined(count['fake-id-2']);
-			});
-
-			it("sets the counts as the values in the object", () => {
-				const count = new CommentCount({
-					articleIds: ['fake-id', 'fake-id-2']
-				}).getCount();
-
-				proclaim.isNumber(count['fake-id']);
-				proclaim.isNumber(count['fake-id-2']);
-			});
-
-			it("removes any invalid id's", () => {
-				const count = new CommentCount({
-					articleIds: ['fake-id', 'invalid-id']
-				}).getCount();
-
-				proclaim.isUndefined(count['invalid-id']);
-				proclaim.isDefined(count['fake-id']);
-			});
-
-			it("returns null if all id's are invalid", () => {
-				// I am not sure on this case
-				// Would it be better to return an empty object?
-
-				const count = new CommentCount({
-					articleIds: ['invalid-id', 'invalid-id']
-				}).getCount();
-
-				proclaim.isNull(count);
+				assert.throws(() => new CommentCount(mockCountElement), 'Element must be a HTMLElement');
 			});
 		});
 	});
 });
-
