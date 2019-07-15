aa094b057dad0580ee5f271a7bf52a18a957f685
Keran Braich <keran.braich@ft.com>
2019-07-15 10:29:41 +0100

    Delete unused exports

--- src/count/main.js
+++ src/count/main.js
@@ -1,7 +1,6 @@
 const config = require('./src/javascripts/config');
 const oCommentApi = require('o-comment-api');
 const defaultConfig = require('./config.js');
-const oCommentUtilities = require('o-comment-utilities');
 const Widget = require('./src/javascripts/widget');
 
 /**
@@ -14,47 +13,8 @@ config.set(defaultConfig);
  */
 oCommentApi.setConfig('cache', true);
 
-
 /**
  * Widget.js exposed as main constructor
  * @type {object}
  */
 module.exports = Widget;
-
-/**
- * Adds or overrides configuration options.
- *
- * @param  {string|object} keyOrObject Key or actually an object with key-value pairs.
- * @param  {anything} value Optional. Should be specified only if keyOrObject is actually a key (string).
- * @return {undefined}
- */
-module.exports.setConfig = function () {
-	config.set.apply(this, arguments);
-};
-
-module.exports.utilities = oCommentUtilities;
-module.exports.dataService = oCommentApi;
-
-/**
- * Enables logging.
- * @return {undefined}
- */
-module.exports.enableLogging = function () {
-	oCommentUtilities.logger.enable.apply(this, arguments);
-};
-
-/**
- * Disables logging.
- * @return {undefined}
- */
-module.exports.disableLogging = function () {
-	oCommentUtilities.logger.disable.apply(this, arguments);
-};
-
-/**
- * Sets logging level.
- * @return {undefined}
- */
-module.exports.setLoggingLevel = function () {
-	oCommentUtilities.logger.setLevel.apply(this, arguments);
-};
