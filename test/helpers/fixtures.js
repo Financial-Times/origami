0886756dae3d39c7d853df18478dbb8dfb2de22f
Keran Braich <keran.braich@ft.com>
2019-08-01 16:52:40 +0100

    Add test markup for comment count

--- test/helpers/fixtures.js
+++ test/helpers/fixtures.js
@@ -19,16 +19,24 @@ function insert(html) {
 	sandboxEl.innerHTML = html;
 }
 
-
-function htmlCode () {
+function streamMarkup () {
 	const html = `<div>
-		<div class="o-comments" data-o-component="o-comments" id="element"></div>
+		<div data-o-component="o-comments" data-o-comments-article-id="id"></div>
+	</div>
+	`;
+	insert(html);
+}
+
+function countMarkup () {
+	const html = `<div>
+		<div data-o-component="o-comments" data-o-comments-count data-o-comments-article-id="id"></div>
 	</div>
 	`;
 	insert(html);
 }
 
 export {
-	htmlCode,
-	reset
+	reset,
+	streamMarkup,
+	countMarkup
 };
