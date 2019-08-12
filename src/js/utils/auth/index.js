08d0489bbd9b1a43fee8e94d72365fecf0e9fe1f
Tunca Bergmen <5130615+tbergmen@users.noreply.github.com>
2019-08-12 14:11:54 +0100

    Add comments to explain responses from comments api

--- src/js/utils/auth/index.js
+++ src/js/utils/auth/index.js
@@ -1,14 +1,17 @@
 const getJsonWebToken = () => fetch('https://comments-api.ft.com/user/auth/', {
 	credentials: 'include'
 }).then(response => {
+	// user is not signed in or session token is invalid
 	if (response.status === 404) {
 		return { token: undefined, userIsSignedIn: false };
 	}
 
+	// user is signed in but has no display name
 	if (response.status === 205) {
 		return { token: undefined, userIsSignedIn: true };
 	}
 
+	// user is signed in and has a pseudonym
 	if (response.ok) {
 		return response.json();
 	}
