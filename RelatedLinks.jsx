412c06bb97422d2e40938864dad3e68a77ed8ae2
Maggie Allen <maggie.allen@ft.com>
2019-10-11 12:03:37 +0100

    related content links can use url or relatedUrl as the href

--- RelatedLinks.jsx
+++ RelatedLinks.jsx
@@ -1,15 +1,18 @@
 import { h } from '@financial-times/x-engine';
 
-const renderLink = ({ id, relativeUrl, type, title }, i) => (
-	<li
-		key={`related-${i}`}
-		data-content-id={id}
-		className={`o-teaser__related-item o-teaser__related-item--${type}`}>
-		<a data-trackable="related" href={relativeUrl}>
-			{title}
-		</a>
-	</li>
-);
+const renderLink = ({ id, type, title, url, relativeUrl }, i) => {
+	const displayUrl = relativeUrl || url;
+	return (
+		<li
+			key={`related-${i}`}
+			data-content-id={id}
+			className={`o-teaser__related-item o-teaser__related-item--${type}`}>
+			<a data-trackable="related" href={displayUrl}>
+				{title}
+			</a>
+		</li>
+	);
+}
 
 export default ({ relatedLinks = [] }) => (
 	relatedLinks && relatedLinks.length ? (
