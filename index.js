eea446a2e9a774dcbf5e7dcd735ba508ca03919f
Alice Bartlett <alice.bartlett@gmail.com>
2019-11-27 16:29:49 +0000

    Alphabetize dependencies

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ exports.component = Teaser;
 exports.package = require('../package.json');
 
 exports.dependencies = {
-	'o-normalise': '^1.6.0',
 	'o-date': '^2.11.0',
-	'o-typography': '^5.5.0',
-	'o-teaser': '^3.5.0',
 	'o-labels': '^4.2.1',
+	'o-normalise': '^1.6.0',
+	'o-teaser': '^3.5.0',
+	'o-typography': '^5.5.0',
 	'o-video': '^4.1.0'
 };
 
