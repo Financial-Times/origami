1564de0d501a171d327cbc9a1847af341f05088d
Jake Champion <jakechampion.jake2@googlemail.com>
2020-03-09 15:22:11 +0000

    Use correct package name for macos

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ module.exports = (function() {
           return require("@financial-times/sass-linux-ia32");
         } catch {
           try {
-            return require("@financial-times/sass-macos/x64");
+            return require("@financial-times/sass-macos-x64");
           } catch {
             throw new Error(
               "oax does not have a precompiled binary for the platform/architecture you are using. Please contact Origami or open an issue on https://github.com/Financial-Times/oax/issues"
