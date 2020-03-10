2d3e3b96ea1b1f190b7752dcd6e343514c97d1a8
Jake Champion <me@jakechampion.name>
2020-03-10 16:11:42 +0000

    create valid bat file for windows
    use .bat extension all the time because on windows we need the extension to be bat and on other platforms the extension does not matter because they use a shebang

--- postinstall.js
+++ postinstall.js
@@ -23,17 +23,46 @@ if (process.platform === "win32" && process.arch === "x64") {
 const platformSpecificSassPath = path.join(path.dirname(platformSpecificPackagePath), 'dart-sass')
 
 // We need to remove the sass entrypoint file if it exists already.
-if (fs.existsSync(path.join(__dirname, './sass'))) {
-  fs.unlinkSync(path.join(__dirname, './sass'));
+if (fs.existsSync(path.join(__dirname, './sass.bat'))) {
+  fs.unlinkSync(path.join(__dirname, './sass.bat'));
 }
 
 // Create our own sass entrypoint which uses absolute paths to the dartvm and snapshot.
 // This will ensure it works when symlinked to, which the entrypoint file in dart-sass
 // does not support on macos.
-fs.writeFileSync(path.join(__dirname, 'sass'), `
+if (process.platform === "win32") {
+  fs.writeFileSync(
+    path.join(__dirname, "sass.bat"),
+    `
+  @echo off
+  REM This script drives the standalone dart-sass package, which bundles together a
+  REM Dart executable and a snapshot of dart-sass.
+  
+  set SCRIPTPATH=%~dp0
+  set arguments=%*
+  "${path.join(platformSpecificSassPath, "/src/dart.exe")}" "${path.join(
+    platformSpecificSassPath,
+    "/src/sass.dart.snapshot"
+  )}" %arguments%
+`,
+    {
+      encoding: "utf-8",
+      mode: 0o777
+    }
+  );
+} else {
+  fs.writeFileSync(
+    path.join(__dirname, "sass.bat"),
+    `
 #!/bin/sh
-exec "${path.join(platformSpecificSassPath, '/src/dart')}" "${path.join(platformSpecificSassPath, '/src/sass.dart.snapshot')}" "$@"
-`, {
-  encoding: 'utf-8',
-  mode: 0o777
-})
+exec "${path.join(platformSpecificSassPath, "/src/dart")}" "${path.join(
+      platformSpecificSassPath,
+      "/src/sass.dart.snapshot"
+    )}" "$@"
+`,
+    {
+      encoding: "utf-8",
+      mode: 0o777
+    }
+  );
+}
