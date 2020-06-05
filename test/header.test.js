0ddd77bdd5f4def84327026c662ec1a8213060ce
Nick Colley <nick.colley@ft.com>
2020-06-05 11:59:19 +0100

    Ensure header link buttons work when pressing space.
    Some assistive technologies, like screen readers, suggest to press 'space'
    when interacting with a link with a role of 'button'.
    We need to ensure that we replicate this functionality that exists on a button element.
    In the future we should consider using the 'button' element instead.

--- test/header.test.js
+++ test/header.test.js
@@ -1,6 +1,7 @@
 /* eslint-env mocha */
 /* global proclaim */
 
+import { dispatch } from './helpers/events';
 import Header from '../src/js/header';
 
 describe('Header API', () => {
@@ -10,6 +11,7 @@ describe('Header API', () => {
 });
 
 describe('Header instance', () => {
+	let header;
 	let headerEl;
 	let containerEl;
 
@@ -17,21 +19,55 @@ describe('Header instance', () => {
 		containerEl = document.createElement('div');
 		document.body.appendChild(containerEl);
 		containerEl.innerHTML = `
-			<header class="o-header" data-o-component="o-header"></header>
+			<header class="o-header" data-o-component="o-header" data-o-header--no-js>
+				<a href="#o-header-drawer" class="o-header__top-link o-header__top-link--menu" aria-controls="o-header-drawer" title="Open drawer menu">
+					<span class="o-header__top-link-label">Open drawer menu</span>
+				</a>
+				<a href="#o-header-search" class="o-header__top-link o-header__top-link--search" aria-controls="o-header-search-js" title="Open search bar">
+					<span class="o-header__top-link-label">Open search bar</span>
+				</a>
+				<div id="o-header-search-js" class="o-header__row o-header__search" role="search" data-o-header-search>
+					<input class="o-header__search-term" id="o-header-search-term-js" name="q" type="text" placeholder="Search the FT">
+				</div>
+			</header>
+			<div class="o-header__drawer" id="o-header-drawer" role="navigation" aria-label="Drawer menu" data-o-header-drawer data-o-header-drawer--no-js></div>
 		`;
 		headerEl = containerEl.querySelector('.o-header');
+		header = new Header(headerEl);
 	});
 
 	afterEach(() => {
 		containerEl.removeChild(headerEl);
+		header = null;
 		headerEl = null;
 		containerEl = null;
 	});
 
 	it('constructor', () => {
-		const header = new Header(headerEl);
 		proclaim.isInstanceOf(header, Header);
 		proclaim.deepEqual(header.headerEl, headerEl);
 		proclaim.isNotNull(headerEl.getAttribute('data-o-header--js'));
+	});
+
+	it('shows the menu when activating menu button by pressing space', () => {
+		const menuButton = document.querySelector('[aria-controls="o-header-drawer"]');
+		const drawer = document.getElementById('o-header-drawer');
+
+		proclaim.equal(drawer.getAttribute('aria-hidden'), 'true');
+
+		dispatch(menuButton, 'keydown', { keyCode: 32 });
+
+		proclaim.equal(drawer.getAttribute('aria-hidden'), 'false');
+	});
+
+	it('shows the search when activating search button by pressing space', () => {
+		const searchButton = document.querySelector('[aria-controls="o-header-search-js"]');
+		const search = document.getElementById('o-header-search-js');
+
+		proclaim.equal(search.getAttribute('aria-hidden'), 'true');
+
+		dispatch(searchButton, 'keydown', { keyCode: 32 });
+
+		proclaim.equal(search.getAttribute('aria-hidden'), 'false');
 	});
 });
--- test/mega.test.js
+++ test/mega.test.js
@@ -1,19 +1,8 @@
 /* eslint-env mocha */
 /* global proclaim */
 
+import { dispatch, waitFor } from './helpers/events';
 import mega from '../src/js/mega';
-
-function dispatch (target, type) {
-	target.dispatchEvent(new Event(type, { bubbles: true }));
-}
-
-function waitFor (target, events) {
-	const promises = events.map(event => {
-		return new Promise(resolve => target.addEventListener(event, resolve));
-	});
-
-	return Promise.all(promises);
-}
 
 describe('Mega', () => {
 	let containerEl;
