d15264db34c2dcc88aa4f9f21af74374dafc3dac
Jake Champion <me@jakechampion.name>
2020-06-09 14:49:01 +0100

    update to the latest version sass - version 1.26.8

--- sass-windows-x64/dart-sass/sass.bat
+++ sass-windows-x64/dart-sass/sass.bat
@@ -4,4 +4,4 @@ REM Dart executable and a snapshot of dart-sass.
 
 set SCRIPTPATH=%~dp0
 set arguments=%*
-"%SCRIPTPATH%\src\dart.exe" "%SCRIPTPATH%\src\sass.dart.snapshot" %arguments%
+"%SCRIPTPATH%\src\dart.exe" "%SCRIPTPATH%\src\sass.snapshot" %arguments%
