a3846e9fe5fcf8d2219fa85e60dd9dc3b19bea44
Jake Champion <me@jakechampion.name>
2020-08-07 16:24:47 +0100

    Add unit tests for Store class

--- test/core/store.test.js
+++ test/core/store.test.js
@@ -5,14 +5,54 @@ import Store from '../../src/javascript/core/store';
 
 describe('Core.Store', function () {
 
-	describe('init()', function () {
+	describe('constructor', function () {
+
+		it('throws an error if not given a name for the store', function() {
+			proclaim.throws(function() {
+				new Store;
+			});
+		});
+
+		it('throws an error if name for the store is not a string', function() {
+			proclaim.throws(function() {
+				new Store(137);
+			});
+			proclaim.throws(function() {
+				new Store({});
+			});
+			proclaim.throws(function() {
+				new Store(true);
+			});
+			proclaim.throws(function() {
+				new Store([]);
+			});
+			proclaim.throws(function() {
+				new Store(undefined);
+			});
+			proclaim.throws(function() {
+				new Store(null);
+			});
+			proclaim.throws(function() {
+				new Store(function(){});
+			});
+		});
+
+		it('throws an error if name for the store is an empty string', function() {
+			proclaim.throws(function() {
+				new Store('');
+			});
+		});
+
+		it('constructs a store if given a name which is a string', function() {
+			proclaim.doesNotThrow(function() {
+				new Store('test');
+			});
+		});
+
 		it('should use local storage by default', function () {
 			proclaim.equal(new Store('test').storage._type, 'localStorage');
 		});
 	});
-
-	const store = new Store('test');
-
 	describe('write()', function () {
 		it('should save a value', function () {
 			proclaim.equal(store.write('TESTING').read(), 'TESTING');
