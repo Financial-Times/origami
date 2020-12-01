ded5111d259777d7e59215bfd44fac03de2b5f09
Estefania Morton <estefania.morton@ft.com>
2020-12-01 08:48:56 +0000

    storybook x-teaser argument fixes

--- content-package.js
+++ content-package.js
@@ -1,8 +1,6 @@
 const { presets } = require('../')
 
-exports.args = Object.assign(require('../__fixtures__/content-package.json'), presets.Hero, {
-	modifiers: 'centre'
-})
+exports.args = Object.assign(require('../__fixtures__/content-package.json'), presets.Hero)
 
 // This reference is only required for hot module loading in development
 // <https://webpack.js.org/concepts/hot-module-replacement/>
--- package-item.js
+++ package-item.js
@@ -1,9 +1,6 @@
 const { presets } = require('../')
 
-exports.args = Object.assign(require('../__fixtures__/package-item.json'), presets.Hero, {
-	parentTheme: 'extra-article',
-	modifiers: 'centre'
-})
+exports.args = Object.assign(require('../__fixtures__/package-item.json'), presets.Hero)
 
 // This reference is only required for hot module loading in development
 // <https://webpack.js.org/concepts/hot-module-replacement/>
