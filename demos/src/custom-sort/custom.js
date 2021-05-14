9f3a757257372f2357283a491164b814707b67ad
Jake Champion <me@jakechampion.name>
2021-05-14 12:36:04 +0100

    remove single space indent to make `obt verify` happy

--- demos/src/custom-sort/custom.js
+++ demos/src/custom-sort/custom.js
@@ -288,7 +288,7 @@ window.customSort = function customSort(query, populateResults) {
  * @param {string} query - Text which was typed into the autocomplete by the user
  * @returns {[string, boolean][]} An array of arrays which contain two items, the first is the character in the suggestion, the second is a boolean which indicates whether the character should be highlighted.
  */
- window.customHighlighter = function customHighlighter(suggestion, query) {
+window.customHighlighter = function customHighlighter(suggestion, query) {
 	const result = suggestion.split('');
 
 	const matchIndex = suggestion.toLocaleLowerCase().indexOf(query);
