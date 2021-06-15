8491fb019d9a4a258367255bf991c77877d572f6
Jake Champion <me@jakechampion.name>
2021-06-15 10:38:08 +0100

    Add link to PR which would remove the need for the hack file -- https://github.com/facebook/jest/pull/11569

--- test/js/helpers/hack.js
+++ test/js/helpers/hack.js
@@ -3,5 +3,5 @@
 // pretty-format makes use of `global`...
 // -> Line 10 of https://unpkg.com/browse/pretty-format@27.0.2/build/plugins/AsymmetricMatcher.js
 // The line in the file above is `var Symbol = global['jest-symbol-do-not-touch'] || global.Symbol;`
-
+// This Pull-Request would fix the issue -- https://github.com/facebook/jest/pull/11569
 window.global = window;
