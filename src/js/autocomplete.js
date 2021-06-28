38112a39841b84a15ea4e5dfb15b822000dc7ea7
Jake Champion <me@jakechampion.name>
2021-06-28 16:29:44 +0100

    update the comment about using a fork of accessible-autocomplete

--- src/js/autocomplete.js
+++ src/js/autocomplete.js
@@ -1,6 +1,10 @@
-// We use our own fork of accessible-autocomplete because of this bug -- https://github.com/alphagov/accessible-autocomplete/issues/210#issuecomment-845860595
-// Our fork uses this change -- https://github.com/alphagov/accessible-autocomplete/pull/491
-// If the bug is fixed in accessible-autocomplete then we can stop using our fork
+// We use our own fork of accessible-autocomplete because the main package is not being actively maintained and has bugs which we needed to fix
+// There is a changelog for the fixes we've added -- https://github.com/Financial-Times/accessible-autocomplete/blob/master/CHANGELOG.md#210---2021-05-24
+// Below are the pull-requests to accessible-autocomplete which would fix the bugs:
+// https://github.com/alphagov/accessible-autocomplete/pull/497
+// https://github.com/alphagov/accessible-autocomplete/pull/491
+// https://github.com/alphagov/accessible-autocomplete/pull/496
+// If the above pull-requests are merged and published, then we can stop using our fork
 import accessibleAutocomplete from '@financial-times/accessible-autocomplete';
 
 /**
