7bc02b5c617cb9ef5c899e3580e6ef883d4e718d
Charlotte Payne <charlotte.payne@ft.com>
2021-07-01 15:47:56 +0100

    Add literal leading space before live blog status
    * Mirror the change to the relative timestamp component
    * o-labels and o-teaser have been updated and now have only 2px
    margin between the status dot and the status text. This does not match
    the design specification of 4px between the two elements.
    * Origami team requested that x-teaser be updated with the leading space
    rather than o-labels because all consumes of o-labels are not known.
    * References:
    Design spec - https://www.figma.com/file/MyHQ1qdwYyek5IBdhEEaND/FT-UI-Library?node-id=318%3A816
    Updated o-date with reduced space:
    https://github.com/Financial-Times/o-labels/blob/master/src/scss/_placeholders.scss#L48-L63

--- LiveBlogStatus.jsx
+++ LiveBlogStatus.jsx
@@ -15,6 +15,6 @@ const LiveBlogModifiers = {
 export default ({ status }) =>
 	status && status !== 'closed' ? (
 		<div className={`o-teaser__timestamp o-teaser__timestamp--${LiveBlogModifiers[status]}`}>
-			<span className="o-teaser__timestamp-prefix">{LiveBlogLabels[status]}</span>
+			<span className="o-teaser__timestamp-prefix">{` ${LiveBlogLabels[status]} `}</span>
 		</div>
 	) : null
