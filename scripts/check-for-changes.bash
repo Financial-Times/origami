813c8e46d9e6e423fad3a1322d043edc6422cd69
chee <chee@snoot.club>
2021-09-16 11:55:07 +0100

    chore: use git status porcelain
    using `git diff` meant it would not fail if newly created
    files were missing

--- scripts/check-for-changes.bash
+++ scripts/check-for-changes.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ $(git diff | wc -l) -gt 0 ]; then
+if [ $(git status --porcelain | wc -l) -gt 0 ]; then
 	echo something changed
 	echo
 	echo 'run "npm install" and "npm run regenerate" and commit the changes'
