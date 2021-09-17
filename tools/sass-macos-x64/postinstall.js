1ead8387da7f57fa66c8e49592a282359ba35e8c
Jake Champion <me@jakechampion.name>
2021-09-17 16:37:18 +0100

    fix: rewrite postinstall scripts to not depend on os and cpu fields in package.json

--- tools/sass-macos-x64/postinstall.js
+++ tools/sass-macos-x64/postinstall.js
@@ -1,23 +1,21 @@
 "use strict";
 
-const { name, os, cpu } = require("./package.json");
+const { name } = require("./package.json");
+const os = "darwin";
+const cpu = "";
 
-if (os && !os.includes(process.platform)) {
+if (os && os === process.platform) {
   throw new Error(
     `${name} does not support the platform you are using. You are using: "${
       process.platform
-    }" and ${name} supports: ${os.join(
-      ","
-    )}. If you think this is a mistake, please contact Origami or open an issue on https://github.com/Financial-Times/sass/issues`
+    }" and ${name} supports: ${os}. If you think this is a mistake, please contact Origami or open an issue on https://github.com/Financial-Times/sass/issues`
   );
 }
 
-if (cpu && !cpu.includes(process.arch)) {
+if (cpu && cpu === process.arch) {
   throw new Error(
     `${name} does not support the cpu you are using. You are using: "${
       process.arch
-    }" and ${name} supports: ${cpu.join(
-      ","
-    )}. If you think this is a mistake, please contact Origami or open an issue on https://github.com/Financial-Times/sass/issues`
+    }" and ${name} supports: ${cpu}. If you think this is a mistake, please contact Origami or open an issue on https://github.com/Financial-Times/sass/issues`
   );
 }
