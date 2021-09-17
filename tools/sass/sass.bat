d441f023e78c28ef9fb4562a182dce3010b8c7ea
fox <origamiserviceuser@ft.com>
2021-09-17 16:57:33 +0000

    chore: regenerate config

--- tools/sass/sass.bat
+++ tools/sass/sass.bat
@@ -1,3 +1,3 @@
 
 #!/bin/sh
-exec "/Users/jake.champion/Code/origami/tools/sass-macos-x64/dart-sass/src/dart" "/Users/jake.champion/Code/origami/tools/sass-macos-x64/dart-sass/src/sass.snapshot" "$@"
+exec "/home/runner/work/origami/origami/tools/sass-linux-x64/dart-sass/src/dart" "/home/runner/work/origami/origami/tools/sass-linux-x64/dart-sass/src/sass.snapshot" "$@"
