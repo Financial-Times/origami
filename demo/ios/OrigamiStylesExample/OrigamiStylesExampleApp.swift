9bb970b16aab7ce72c1566144e1b7f6d208288e4
Fernando Ortiz <fernando.ortiz.consultant@ft.com>
2021-09-20 16:28:29 -0300

    Add a demo project for iOS

---
+++ demo/ios/OrigamiStylesExample/OrigamiStylesExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrigamiStylesExampleApp.swift
+//  OrigamiStylesExample
+//
+//  Created by Fernando Ortiz - Parser on 17/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct OrigamiStylesExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
