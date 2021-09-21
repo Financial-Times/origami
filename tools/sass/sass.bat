a18e335d3d8c1aee6157622db974a9999c99d735
chee <chee@snoot.club>
2021-09-21 12:37:39 +0100

    chore: fix the lint script name

--- tools/sass/sass.bat
+++ tools/sass/sass.bat
@@ -1,3 +1,2 @@
-
 #!/bin/sh
-exec "/home/runner/work/origami/origami/tools/sass-linux-x64/dart-sass/src/dart" "/home/runner/work/origami/origami/tools/sass-linux-x64/dart-sass/src/sass.snapshot" "$@"
+exec "/home/chee/projects/origami/origami/tools/sass-linux-x64/dart-sass/src/dart" "/home/chee/projects/origami/origami/tools/sass-linux-x64/dart-sass/src/sass.snapshot" "$@"
