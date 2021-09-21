19097c73568a897f342a2ad0eb7d1b769c832252
chee <chee@snoot.club>
2021-09-21 13:57:18 +0100

    chore: move sass and updater into obt

--- tools/origami-build-tools/lib/helpers/sass.js
+++ tools/origami-build-tools/lib/helpers/sass.js
@@ -7,15 +7,15 @@ const linux64bit = process.platform === 'linux' && process.arch === 'x64';
 const linux32bit = process.platform === 'linux' && process.arch === 'ia32';
 
 if (windows64bit) {
-	module.exports = '../../exe/sass-windows-x64';
+	module.exports = '../../exe/sass/windows-x64/src/sass.bat';
 } else if (windows32bit) {
-	module.exports = '../../exe/sass-windows-ia32';
+	module.exports = '../../exe/sass/windows-ia32/src/sass.bat';
 } else if (mac) {
-	module.exports = '../../exe/sass-macos-x64';
+	module.exports = '../../exe/sass/macos-x64/src/sass';
 } else if (linux64bit) {
-	module.exports = '../../exe/sass-linux-x64';
+	module.exports = '../../exe/sass/linux-x64/src/sass';
 } else if (linux32bit) {
-	module.exports = '../../exe/sass-linux-ia32';
+	module.exports = '../../exe/sass/linux-ia32/src/sass';
 } else {
 	throw new Error(
 		'origami-build-tools/exe/sass does not have a precompiled binary for the platform/architecture you are using. Please contact Origami or open an issue on https://github.com/Financial-Times/sass/issues'
