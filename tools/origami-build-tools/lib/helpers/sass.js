96d9b4cf33841816463fb8e548cd7aade847ff0c
chee <chee@snoot.club>
2021-09-21 16:51:37 +0100

    chore: fix sass paths

--- tools/origami-build-tools/lib/helpers/sass.js
+++ tools/origami-build-tools/lib/helpers/sass.js
@@ -1,4 +1,5 @@
 'use strict';
+const resolve = require('path').resolve;
 
 const windows64bit = process.platform === 'win32' && process.arch === 'x64';
 const windows32bit = process.platform === 'win32' && process.arch === 'ia32';
@@ -7,15 +8,30 @@ const linux64bit = process.platform === 'linux' && process.arch === 'x64';
 const linux32bit = process.platform === 'linux' && process.arch === 'ia32';
 
 if (windows64bit) {
-	module.exports = '../../exe/sass/windows-x64/src/sass.bat';
+	module.exports = resolve(
+		__dirname,
+		'../../exe/sass/windows-x64/dart-sass/sass.bat'
+	);
 } else if (windows32bit) {
-	module.exports = '../../exe/sass/windows-ia32/src/sass.bat';
+	module.exports = resolve(
+		__dirname,
+		'../../exe/sass/windows-ia32/dart-sass/sass.bat'
+	);
 } else if (mac) {
-	module.exports = '../../exe/sass/macos-x64/src/sass';
+	module.exports = resolve(
+		__dirname,
+		'../../exe/sass/macos-x64/dart-sass/sass'
+	);
 } else if (linux64bit) {
-	module.exports = '../../exe/sass/linux-x64/src/sass';
+	module.exports = resolve(
+		__dirname,
+		'../../exe/sass/linux-x64/dart-sass/sass'
+	);
 } else if (linux32bit) {
-	module.exports = '../../exe/sass/linux-ia32/src/sass';
+	module.exports = resolve(
+		__dirname,
+		'../../exe/sass/linux-ia32/dart-sass/sass'
+	);
 } else {
 	throw new Error(
 		'origami-build-tools/exe/sass does not have a precompiled binary for the platform/architecture you are using. Please contact Origami or open an issue on https://github.com/Financial-Times/sass/issues'
