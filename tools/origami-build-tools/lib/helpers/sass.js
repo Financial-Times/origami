ef3201a2d258499392960a7835a92952d77d75f8
chee <chee@snoot.club>
2021-09-21 13:50:29 +0100

    chore: add the sass binaries directly to obt

---
+++ tools/origami-build-tools/lib/helpers/sass.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const windows64bit = process.platform === 'win32' && process.arch === 'x64';
+const windows32bit = process.platform === 'win32' && process.arch === 'ia32';
+const mac = process.platform === 'darwin';
+const linux64bit = process.platform === 'linux' && process.arch === 'x64';
+const linux32bit = process.platform === 'linux' && process.arch === 'ia32';
+
+if (windows64bit) {
+	module.exports = '../../exe/sass-windows-x64';
+} else if (windows32bit) {
+	module.exports = '../../exe/sass-windows-ia32';
+} else if (mac) {
+	module.exports = '../../exe/sass-macos-x64';
+} else if (linux64bit) {
+	module.exports = '../../exe/sass-linux-x64';
+} else if (linux32bit) {
+	module.exports = '../../exe/sass-linux-ia32';
+} else {
+	throw new Error(
+		'origami-build-tools/exe/sass does not have a precompiled binary for the platform/architecture you are using. Please contact Origami or open an issue on https://github.com/Financial-Times/sass/issues'
+	);
+}
