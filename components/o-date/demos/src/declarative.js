133c6d2cbf59657a33df65b7d4c22ad5dc67733b
chee <chee@snoot.club>
2021-10-05 17:27:20 +0100

    chore: set datetime directly on elements

--- components/o-date/demos/src/declarative.js
+++ components/o-date/demos/src/declarative.js
@@ -1,16 +1,12 @@
 import ODate from '../../main.js';
 
 const now = new Date();
-const sixHoursAgo = new Date();
-sixHoursAgo.setHours(now.getHours() - 6);
+const today = new Date();
 const dates = document.querySelectorAll('time:not([datetime])');
+today.setHours(now.getHours() - 6);
 
 for (let i = 0; i < dates.length; i++) {
-	// the last 2 are being set to a specific time because otherwise the demo
-	// changes on every build and upsets percy the hedgehog
-	const date =
-		i < dates.length - 2 ? sixHoursAgo.toISOString() : '1912-04-15T05:18Z';
-	dates[i].setAttribute('datetime', date);
+	dates[i].setAttribute('datetime', today.toISOString());
 }
 
 ODate.init();
