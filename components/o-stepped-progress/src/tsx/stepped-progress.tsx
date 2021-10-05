6c3a2f0bb094dd4cbfd767be1a706e740e45cd6f
chee <chee@snoot.club>
2021-10-05 18:40:21 +0100

    chore: reuse stepped-progress javascript

--- components/o-stepped-progress/src/tsx/stepped-progress.tsx
+++ components/o-stepped-progress/src/tsx/stepped-progress.tsx
@@ -1,7 +1,8 @@
+import {statusTexts, classNames} from '../js/stepped-progress-step';
+
 export interface SteppedProgressStep {
 	label: string;
-	current?: boolean;
-	state?: 'complete' | 'error';
+	state?: 'complete' | 'error' | 'current';
 	url?: string;
 }
 
@@ -9,51 +10,32 @@ export interface SteppedProgressProps {
 	steps: SteppedProgressStep[];
 }
 
-function getStepClasses({current, state}) {
-	let classes = ['o-stepped-progress__step'];
-	if (current) {
-		classes.push('o-stepped-progress__step--current');
-	}
-	if (state) {
-		classes.push(`o-stepped-progress__step--${state}`);
-	}
-	return classes.join(' ');
-}
-
-function getStateLabel({current, state}) {
-	let label = current ? 'current step' : '';
-	if (state) {
-		label += ` (${state})`;
-	}
-	return label;
-}
-
 export function SteppedProgress({steps}: SteppedProgressProps) {
 	return (
-		<div class="o-stepped-progress" data-o-component="o-stepped-progress">
-			<ol class="o-stepped-progress__steps">
-				{steps.map(({label, current = false, state, url}, index) => {
+		<div className="o-stepped-progress" data-o-component="o-stepped-progress">
+			<ol className="o-stepped-progress__steps">
+				{steps.map(({label, state, url}, index) => {
 					const labelElement = (
-						<span class="o-stepped-progress__label">
+						<span className="o-stepped-progress__label">
 							{label}
-							{state || current ? (
-								<span class="o-stepped-progress__status">
-									{getStateLabel({current, state})}
+							{state ? (
+								<span className="o-stepped-progress__status">
+									{statusTexts[state]}
 								</span>
 							) : (
 								''
 							)}
 						</span>
 					);
-					const classes = getStepClasses({current, state});
+
 					return (
 						<li key={label + index}>
 							{url ? (
-								<a href={url} className={classes}>
+								<a href={url} className={classNames[state]}>
 									{labelElement}
 								</a>
 							) : (
-								<span className={classes}>{labelElement}</span>
+								<span className={classNames[state]}>{labelElement}</span>
 							)}
 						</li>
 					);
