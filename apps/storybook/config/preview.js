1074447cf6ed80f96ea95d3c2029eb7ba0aefa5f
chee <chee@snoot.club>
2021-10-06 11:27:53 +0100

    docs: add backgrounds for some of our ft colours

--- apps/storybook/config/preview.js
+++ apps/storybook/config/preview.js
@@ -1,9 +1,24 @@
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+	actions: {argTypesRegex: "^on[A-Z].*"},
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
+	backgrounds: {
+		default: "page background",
+		values: [
+			{
+				name: "page background",
+				value: "var(--o-colors-page-background)",
+			},
+			...["slate", "candy", "white", "black", "velvet", "lemon"].map(name => {
+				return {
+					name,
+					value: `var(--o-colors-${name})`,
+				}
+			}),
+		],
+	},
 }
