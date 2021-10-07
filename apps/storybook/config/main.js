08e30da898bfba073b8fb0fb4afc3b77c162214c
chee <chee@snoot.club>
2021-10-07 23:41:04 +0100

    chore: i hate everything

--- apps/storybook/config/main.js
+++ apps/storybook/config/main.js
@@ -42,4 +42,26 @@ module.exports = {
 			shouldExtractLiteralValuesFromEnum: true,
 		},
 	},
+	webpackFinal: async config => {
+		config.module.rules.push({
+			test: /\.tsx?$/,
+			exclude: /node_modules/,
+			use: [
+				{
+					loader: require.resolve("babel-loader"),
+					options: {
+						presets: [
+							require("@babel/preset-typescript").default,
+							require("@babel/preset-react").default,
+							require("@babel/preset-env").default,
+						],
+					},
+				},
+				require.resolve("react-docgen-typescript-loader"),
+			],
+		})
+
+		config.resolve.extensions.push(".ts", ".tsx")
+		return config
+	},
 }
