c3b2dd9da5d4b2132aac7edf0c56216cd8cee614
chee <chee@snoot.club>
2021-10-07 22:26:52 +0100

    chore: update script

--- scripts/build-website.bash
+++ scripts/build-website.bash
@@ -1,7 +1,6 @@
 #!/bin/bash
 rm -r origami.ft.com/
-cd apps/website/ && make build
-mv _site ../../origami.ft.com
+cd apps/website/ && npm run build
 cd ../storybook/
 npm ci
 npm run build-storybook
