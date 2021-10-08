7cbf571b476d506e26826d4e423637488d313d18
chee <chee@snoot.club>
2021-10-08 15:50:25 +0100

    chore: try providing a Step component

--- components/o-stepped-progress/src/tsx/stepped-progress.tsx
+++ components/o-stepped-progress/src/tsx/stepped-progress.tsx
@@ -1,48 +1,48 @@
 import {statusTexts, classNames} from '../js/stepped-progress-step';
 
-export interface SteppedProgressStep {
+export interface StepProps {
 	label: string;
 	state?: 'complete' | 'error' | 'current';
 	url?: string;
 }
 
-export interface SteppedProgressProps {
-	steps: SteppedProgressStep[];
+export function Step({label, state, url}: StepProps) {
+	const labelElement = (
+		<span className="o-stepped-progress__label">
+			{label}
+			{state ? (
+				<span className="o-stepped-progress__status">{statusTexts[state]}</span>
+			) : (
+				''
+			)}
+		</span>
+	);
+
+	let stepClasses = `o-stepped-progress__step ${classNames[state]}`;
+
+	return (
+		<li>
+			{url ? (
+				<a href={url} className={stepClasses}>
+					{labelElement}
+				</a>
+			) : (
+				<span className={stepClasses}>{labelElement}</span>
+			)}
+		</li>
+	);
 }
 
-export function SteppedProgress({steps}: SteppedProgressProps) {
+export interface SteppedProgressProps {
+	// Some day hopefully we can allow only Step to be children
+	// https://github.com/microsoft/TypeScript/issues/21699
+	children: JSX.Element | JSX.Element[];
+}
+
+export function SteppedProgress({children}: SteppedProgressProps) {
 	return (
 		<div className="o-stepped-progress" data-o-component="o-stepped-progress">
-			<ol className="o-stepped-progress__steps">
-				{steps.map(({label, state, url}, index) => {
-					const labelElement = (
-						<span className="o-stepped-progress__label">
-							{label}
-							{state ? (
-								<span className="o-stepped-progress__status">
-									{statusTexts[state]}
-								</span>
-							) : (
-								''
-							)}
-						</span>
-					);
-
-					let stepClasses = `o-stepped-progress__step ${classNames[state]}`;
-
-					return (
-						<li key={label + index}>
-							{url ? (
-								<a href={url} className={stepClasses}>
-									{labelElement}
-								</a>
-							) : (
-								<span className={stepClasses}>{labelElement}</span>
-							)}
-						</li>
-					);
-				})}
-			</ol>
+			<ol className="o-stepped-progress__steps">{children}</ol>
 		</div>
 	);
 }
--- components/o-stepped-progress/stories/stepped-progress.stories.tsx
+++ components/o-stepped-progress/stories/stepped-progress.stories.tsx
@@ -1,6 +1,6 @@
 import {withDesign} from 'storybook-addon-designs';
 import {useEffect} from 'react';
-import {SteppedProgress} from '../src/tsx/stepped-progress';
+import {SteppedProgress, Step} from '../src/tsx/stepped-progress';
 import './stepped-progress.scss';
 import javascript from '@financial-times/o-stepped-progress';
 import withHtml from 'origami-storybook-addon-html';
@@ -8,6 +8,7 @@ import withHtml from 'origami-storybook-addon-html';
 export default {
 	title: 'Stepped Progress',
 	component: SteppedProgress,
+	subcomponents: {Step},
 	decorators: [withDesign, withHtml],
 	parameters: {
 		design: {
@@ -15,36 +16,36 @@ export default {
 			url: 'https://www.figma.com/file/xxJEMk0fnzEhoDFaKtM5fz/Stepped-progress-bar-component?node-id=6%3A3537',
 		},
 	},
-	args: {
-		steps: [{label: 'default', state: 'complete', current: false}],
-	},
 };
 
-const Story = args => {
-	useEffect(() => void javascript.init(), []);
-	return <SteppedProgress {...args} />;
+export const Normal = () => {
+	useEffect(() => javascript.init(), []);
+	return (
+		<SteppedProgress>
+			<Step label="Wake up" state="complete" />
+			<Step
+				label="Eat a egg"
+				state="complete"
+				url="https://en.wikipedia.org/wiki/Egg_as_food"
+			/>
+			<Step label="Leave the house" state="current" />
+			<Step label="Defeat everyone" />
+		</SteppedProgress>
+	);
 };
 
-export const Example = Story.bind({});
-Example.args = {
-	steps: [
-		{label: 'Wake up', state: 'complete'},
-		{label: 'Eat a egg', state: 'complete'},
-		{label: 'Leave the house', state: 'current'},
-		{label: 'Defeat everyone'},
-	],
-};
-
-export const ErrorStep = Story.bind({});
-ErrorStep.args = {
-	steps: [
-		{label: 'Wake up', state: 'complete'},
-		{
-			label: 'Eat a egg',
-			state: 'complete',
-			url: 'https://en.wikipedia.org/wiki/Egg_as_food',
-		},
-		{label: 'Leave the house', state: 'error'},
-		{label: 'Defeat everyone'},
-	],
+export const Error = () => {
+	useEffect(() => javascript.init(), []);
+	return (
+		<SteppedProgress>
+			<Step label="Wake up" state="complete" />
+			<Step
+				label="Eat a egg"
+				state="complete"
+				url="https://en.wikipedia.org/wiki/Egg_as_food"
+			/>
+			<Step label="Leave the house" state="error" />
+			<Step label="Defeat everyone" />
+		</SteppedProgress>
+	);
 };
