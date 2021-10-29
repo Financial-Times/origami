995860c772fcc5bec59f2da8d0989a40ced7e603
chee <chee@snoot.club>
2021-10-29 06:07:07 +0100

    backstage: add heading slugs to docs so the links work

--- apps/storybook/addons/markdown-tabs/register.js
+++ apps/storybook/addons/markdown-tabs/register.js
@@ -1,14 +1,46 @@
 import React, {useState, useEffect, createElement} from "react"
+import {renderToStaticMarkup} from "react-dom/server"
 import {addons, types} from "@storybook/addons"
 import {DocsWrapper, DocsContent} from "@storybook/components"
 import marksy from "marksy"
 import {Prism as SyntaxHighlighter} from "react-syntax-highlighter"
 import "./style.scss"
 import syntaxStyle from "./syntax-style.js"
+import slugger from "github-slugger"
+
+function textContent(node) {
+	if (["string", "number"].includes(typeof node)) {
+		return String(node)
+	}
+	if (node instanceof Array) {
+		return node.map(textContent).join("")
+	}
+	if (typeof node === "object" && node) {
+		return textContent(node.props.children)
+	}
+}
 
 const compile = marksy({
 	createElement,
 	elements: {
+		h1: ({children}) => (
+			<h1 id={slugger.slug(textContent(children))}>{children}</h1>
+		),
+		h2: ({children}) => (
+			<h2 id={slugger.slug(textContent(children))}>{children}</h2>
+		),
+		h3: ({children}) => (
+			<h3 id={slugger.slug(textContent(children))}>{children}</h3>
+		),
+		h4: ({children}) => (
+			<h4 id={slugger.slug(textContent(children))}>{children}</h4>
+		),
+		h5: ({children}) => (
+			<h5 id={slugger.slug(textContent(children))}>{children}</h5>
+		),
+		h6: ({children}) => (
+			<h6 id={slugger.slug(textContent(children))}>{children}</h6>
+		),
 		code: ({language, code}) => (
 			<SyntaxHighlighter language={language} style={syntaxStyle}>
 				{code}
