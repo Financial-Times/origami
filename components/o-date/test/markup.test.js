43cbe14b164d48dc7f0b9e43fdade80b4d472549
Lee Moody <notlee@users.noreply.github.com>
2021-10-29 15:44:49 +0100

    feat: o-date, deprecate abbreviated time options.
    `time-ago-abbreviated` and `time-ago-abbreviated-limit-4-hours`
    options are now both deprecated. These options still work but
    no longer show an abbreviated date. They will be removed in a
    future major version.
    
    They were only used in one place and raised accessibility
    concerns multiple times.
    
    Closes: https://github.com/Financial-Times/origami/issues/203

--- components/o-date/test/markup.test.js
+++ components/o-date/test/markup.test.js
@@ -44,9 +44,6 @@ describe('o-date DOM', () => {
 					<!-- render date-only here, set on the parent "time" element -->
 					<span data-o-date-printer>
 					</span>
-					<!-- render an abbreviated, relative time ago here -->
-					<span data-o-date-printer data-o-date-format="time-ago-abbreviated">
-					</span>
 					<!-- render a custom format here, the absolute time -->
 					<span data-o-date-printer data-o-date-format="${customFormat}">
 					</span>
@@ -56,13 +53,7 @@ describe('o-date DOM', () => {
 
 			it('renders all dates in the element', () => {
 				proclaim.include(mockDateElement.textContent, '11 minutes ago');
-				proclaim.include(mockDateElement.textContent, '11m ago');
 				proclaim.include(mockDateElement.textContent, elevenMinutesAgoCustomFormat);
-			});
-
-			it('adds an aria-label attribute containing the ultimate date', () => {
-				const abbreviatedPrinter = mockDateElement.querySelector('[data-o-date-format="time-ago-abbreviated"]');
-				proclaim.equal(abbreviatedPrinter.getAttribute('aria-label'), '11 minutes ago');
 			});
 
 			it('adds a title attribute to all printers containing the full date', () => {
@@ -80,10 +71,6 @@ describe('o-date DOM', () => {
 				proclaim.equal(mockDateElement.innerHTML, '11 minutes ago');
 			});
 
-			it('does not add an aria-label attribute containing the same date', () => {
-				proclaim.equal(mockDateElement.getAttribute('aria-label'), null);
-			});
-
 			it('adds a title attribute containing the full date', () => {
 				proclaim.equal(mockDateElement.getAttribute('title'), elevenMinutesAgoDateTime);
 			});
@@ -99,28 +86,20 @@ describe('o-date DOM', () => {
 				proclaim.equal(mockDateElement.innerHTML, '11 minutes ago');
 			});
 
-			it('does not add an aria-label attribute containing the same date', () => {
-				proclaim.equal(mockDateElement.getAttribute('aria-label'), null);
-			});
-
 			it('adds a title attribute containing the full date', () => {
 				proclaim.equal(mockDateElement.getAttribute('title'), elevenMinutesAgoDateTime);
 			});
 		});
 
-		describe('time-ago-abbreviated', () => {
+		describe('time-ago-abbreviated (deprecated)', () => {
 
 			beforeEach(() => {
 				mockDateElement.dataset.oDateFormat = 'time-ago-abbreviated';
 				new ODate(mockDateElement);
 			});
 
-			it('renders the date in the element', () => {
-				proclaim.equal(mockDateElement.innerHTML, '11m ago');
-			});
-
-			it('adds an aria-label attribute with a non-abbreviated unit', () => {
-				proclaim.equal(mockDateElement.getAttribute('aria-label'), '11 minutes ago');
+			it('renders the date in the element without abbreviation (abbreviation is deprecated)', () => {
+				proclaim.equal(mockDateElement.innerHTML, '11 minutes ago');
 			});
 
 			it('adds a title attribute containing the full date', () => {
@@ -128,18 +107,14 @@ describe('o-date DOM', () => {
 			});
 		});
 
-		describe('time-ago-abbreviated-limit-4-hours', () => {
+		describe('time-ago-abbreviated-limit-4-hours (deprecated)', () => {
 			beforeEach(() => {
 				mockDateElement.dataset.oDateFormat = 'time-ago-abbreviated-limit-4-hours';
 				new ODate(mockDateElement);
 			});
 
-			it('renders the date in the element', () => {
-				proclaim.equal(mockDateElement.innerHTML, '11m ago');
-			});
-
-			it('adds an aria-label attribute with a non-abbreviated unit', () => {
-				proclaim.equal(mockDateElement.getAttribute('aria-label'), '11 minutes ago');
+			it('renders the date in the element without abbreviation (abbreviation is deprecated)', () => {
+				proclaim.equal(mockDateElement.innerHTML, '11 minutes ago');
 			});
 
 			it('adds a title attribute containing the full date', () => {
@@ -157,10 +132,6 @@ describe('o-date DOM', () => {
 				proclaim.equal(mockDateElement.innerHTML, 'today');
 			});
 
-			it('does not add an aria-label attribute containing the same date', () => {
-				proclaim.equal(mockDateElement.getAttribute('aria-label'), null);
-			});
-
 			it('adds a title attribute containing the full date', () => {
 				proclaim.equal(mockDateElement.getAttribute('title'), elevenMinutesAgoDateTime);
 			});
@@ -176,10 +147,6 @@ describe('o-date DOM', () => {
 				proclaim.equal(mockDateElement.innerHTML, '11 minutes ago');
 			});
 
-			it('does not add an aria-label attribute containing the same date', () => {
-				proclaim.equal(mockDateElement.getAttribute('aria-label'), null);
-			});
-
 			it('adds a title attribute containing the full date', () => {
 				proclaim.equal(mockDateElement.getAttribute('title'), elevenMinutesAgoDateTime);
 			});
@@ -193,10 +160,6 @@ describe('o-date DOM', () => {
 
 			it('renders the date in the element', () => {
 				proclaim.equal(mockDateElement.innerHTML, '11 minutes ago');
-			});
-
-			it('does not add an aria-label attribute containing the same date', () => {
-				proclaim.equal(mockDateElement.getAttribute('aria-label'), null);
 			});
 
 			it('adds a title attribute containing the full date', () => {
@@ -238,10 +201,6 @@ describe('o-date DOM', () => {
 				proclaim.equal(mockDateElement.innerHTML, '5 hours ago');
 			});
 
-			it('does not add an aria-label attribute containing the same date', () => {
-				proclaim.equal(mockDateElement.getAttribute('aria-label'), null);
-			});
-
 			it('adds a title attribute containing the full date', () => {
 				proclaim.equal(mockDateElement.getAttribute('title'), fiveHoursAgoDateTime);
 			});
@@ -257,28 +216,20 @@ describe('o-date DOM', () => {
 				proclaim.equal(mockDateElement.innerHTML, '5 hours ago');
 			});
 
-			it('does not add an aria-label attribute containing the same date', () => {
-				proclaim.equal(mockDateElement.getAttribute('aria-label'), null);
-			});
-
 			it('adds a title attribute containing the full date', () => {
 				proclaim.equal(mockDateElement.getAttribute('title'), fiveHoursAgoDateTime);
 			});
 		});
 
-		describe('time-ago-abbreviated', () => {
+		describe('time-ago-abbreviated (deprecated)', () => {
 
 			beforeEach(() => {
 				mockDateElement.dataset.oDateFormat = 'time-ago-abbreviated';
 				new ODate(mockDateElement);
 			});
 
-			it('renders the date in the element', () => {
-				proclaim.equal(mockDateElement.innerHTML, '5h ago');
-			});
-
-			it('adds an aria-label attribute with a non-abbreviated unit', () => {
-				proclaim.equal(mockDateElement.getAttribute('aria-label'), '5 hours ago');
+			it('renders the date in the element without abbreviation (abbreviation is deprecated)', () => {
+				proclaim.equal(mockDateElement.innerHTML, '5 hours ago');
 			});
 
 			it('adds a title attribute containing the full date', () => {
@@ -286,7 +237,7 @@ describe('o-date DOM', () => {
 			});
 		});
 
-		describe('time-ago-abbreviated-limit-4-hours', () => {
+		describe('time-ago-abbreviated-limit-4-hours (deprecated)', () => {
 			beforeEach(() => {
 				mockDateElement.dataset.oDateFormat = 'time-ago-abbreviated-limit-4-hours';
 				new ODate(mockDateElement);
@@ -294,10 +245,6 @@ describe('o-date DOM', () => {
 
 			it('renders nothing in the element', () => {
 				proclaim.equal(mockDateElement.innerHTML, '');
-			});
-
-			it('removes the aria-label attribute', () => {
-				proclaim.isNull(mockDateElement.getAttribute('aria-label'));
 			});
 
 			it('removes the title attribute', () => {
@@ -319,10 +266,6 @@ describe('o-date DOM', () => {
 				proclaim.equal(mockDateElement.innerHTML, 'today');
 			});
 
-			it('does not add an aria-label attribute containing the same date', () => {
-				proclaim.equal(mockDateElement.getAttribute('aria-label'), null);
-			});
-
 			it('adds a title attribute containing the full date', () => {
 				proclaim.equal(mockDateElement.getAttribute('title'), fiveHoursAgoDateTime);
 			});
@@ -336,10 +279,6 @@ describe('o-date DOM', () => {
 
 			it('renders the date in the element', () => {
 				proclaim.equal(mockDateElement.innerHTML, '5 hours ago');
-			});
-
-			it('does not add an aria-label attribute containing the same date', () => {
-				proclaim.equal(mockDateElement.getAttribute('aria-label'), null);
 			});
 
 			it('adds a title attribute containing the full date', () => {
