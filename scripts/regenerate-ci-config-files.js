da710dc53146907ac90c7af49a22e8ae5e09a5f3
chee <chee@snoot.club>
2021-11-02 15:56:19 +0000

    backstage: simplify the test workflow now that we simply run npm lint,build,test
    this also removes the unused test.js

--- scripts/regenerate-ci-config-files.js
+++ scripts/regenerate-ci-config-files.js
@@ -23,18 +23,6 @@ let labelerFile = Mustache.render(labelerTemplate, {
 
 await writeFile(`.github/labeler.yml`, labelerFile)
 
-/**
- *
- * @param {string} workspace path of the workspace
- * @param {string} name name of the npm script to check for
- * @returns {Promise<boolean>}
- */
-async function hasScript(workspace, name) {
-	return (
-		typeof (await readPackage({cwd: workspace})).scripts?.[name] == "string"
-	)
-}
-
 await del([".github/workflows/percy-*.yml", ".github/workflows/test-*.yml"])
 
 let dotReleasePleaseManifest = {}
@@ -44,38 +32,16 @@ let releasePleaseConfig = {
 }
 
 for (let workspace of workspacePaths) {
-	let view = {
-		lint: "",
-		test: "",
-		build: "",
-		workspace,
-		percyTokenName: workspace.replace(/[/-]/g, "_").toUpperCase(),
-	}
-
-	if (await hasScript(workspace, "lint")) {
-		view.lint = `npx npm run -w ${workspace} lint`
-	} else {
-		view.lint = `npx npm exec -w ${workspace} -- origami-build-tools verify`
-	}
-
-	if (await hasScript(workspace, "build")) {
-		// view.build = `npx npm run -w ${workspace} build`
-	}
-
-	if (await hasScript(workspace, "test")) {
-		view.test = `npx npm run -w ${workspace} test`
-	} else {
-		view.test = `(npx npm x -w ${workspace} -- origami-build-tools test || npx npm x -w ${workspace} -- origami-build-tools test) && npx npm x -w ${workspace} -- a11y`
-	}
+	let percyTokenName = workspace.replace(/[/-]/g, "_").toUpperCase()
 
 	let workspaceFilename = workspace.replaceAll("/", "-")
 
-	let testFile = Mustache.render(testTemplate, view)
+	let testFile = Mustache.render(testTemplate, {workspace})
 
 	await writeFile(`.github/workflows/test-${workspaceFilename}.yml`, testFile)
 
 	if (workspace.startsWith("components/")) {
-		let percyFile = Mustache.render(percyTemplate, view)
+		let percyFile = Mustache.render(percyTemplate, {workspace, percyTokenName})
 		await writeFile(
 			`.github/workflows/percy-${workspaceFilename}.yml`,
 			percyFile
