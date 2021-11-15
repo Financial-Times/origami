52724b05d4f19788ed5ee561e3bcb1803edc9673
chee <chee@snoot.club>
2021-11-15 19:24:52 +0000

    backstage: add missing path extensions

--- components/o-overlay/src/js/overlay.js
+++ components/o-overlay/src/js/overlay.js
@@ -120,14 +120,14 @@ class Overlay {
 	 * @param {boolean} opts.compact - If true, the .o-overlay--compact class will be added to the overlay that reduces heading font-size and paddings in the content.
 	 * @param {string} opts.src - Either a url from which HTML to populate the overlay can be loaded, or a querySelector string identifying an element from which the textContent should be extracted.
 	 * @param {string} opts.html - String or HTMLElement. Raw HTML (cannot be set declaratively)
-	 * @param {String} opts.trigger - querySelector expression or HTMLElement. When there's a trigger set, a click event handler will be added to it that will open or close the overlay accordingly. (cannot be set declaratively)
-	 * @param {String} opts.zindex - Value of the CSS z-index property of the overlay. Default set via CSS: '10'
-	 * @param {Boolean} opts.preventclosing - Prevents closure of overlay via standard x button or escape key. For use when you are directing the user to somewhere else. Only valid with modal set to true.
-	 * @param {Boolean} opts.customclose - If you do not use the heading, but want to provide a close button in your html / src (with a class of o-overlay__close), setting customclose to true will attach o-overlay's close handler function to that button.
-	 * @param {String} opts.parentnode - Should be a query selector for a DOM element. If set, the overlay will be appended as a child of this rather than the document body or target. If multiple nodes are matched, it will use the first. If nothing matches this selector, the body will be used.
-	 * @param {Boolean} opts.nested - If set to true, the resize, escape, and layer listeners will not be set up. This boolean should be used in conjunction with the parentnode setting to allow an overlay to be positioned within a DOM element rather than overlaid on top of everything. Default: false.
-	 * @param {Boolean} opts.nofocus - If set to true, the tabindex will not be set on the wrapper element. Useful in conjunction with the nested and parentnode options. Default: false.
-	 * @param {Boolean} opts.fullscreen - If set to true, the overlay will display full screen. This overlay disables scroll on the underlying document and is dismissible with the back button.
+	 * @param {string} opts.trigger - querySelector expression or HTMLElement. When there's a trigger set, a click event handler will be added to it that will open or close the overlay accordingly. (cannot be set declaratively)
+	 * @param {string} opts.zindex - Value of the CSS z-index property of the overlay. Default set via CSS: '10'
+	 * @param {boolean} opts.preventclosing - Prevents closure of overlay via standard x button or escape key. For use when you are directing the user to somewhere else. Only valid with modal set to true.
+	 * @param {boolean} opts.customclose - If you do not use the heading, but want to provide a close button in your html / src (with a class of o-overlay__close), setting customclose to true will attach o-overlay's close handler function to that button.
+	 * @param {string} opts.parentnode - Should be a query selector for a DOM element. If set, the overlay will be appended as a child of this rather than the document body or target. If multiple nodes are matched, it will use the first. If nothing matches this selector, the body will be used.
+	 * @param {boolean} opts.nested - If set to true, the resize, escape, and layer listeners will not be set up. This boolean should be used in conjunction with the parentnode setting to allow an overlay to be positioned within a DOM element rather than overlaid on top of everything. Default: false.
+	 * @param {boolean} opts.nofocus - If set to true, the tabindex will not be set on the wrapper element. Useful in conjunction with the nested and parentnode options. Default: false.
+	 * @param {boolean} opts.fullscreen - If set to true, the overlay will display full screen. This overlay disables scroll on the underlying document and is dismissible with the back button.
 	 * @throws {Error} Will throw an error if the `id` parameter is not unique.
 	 */
 	constructor(id, opts) {
@@ -260,6 +260,7 @@ class Overlay {
 			try {
 				wrapperEl.classList.add(...this.opts.class.split(' '));
 			} catch (error) {
+				// eslint-disable-next-line no-console
 				console.warn(`Could not add the classes: ${this.opts.class}`);
 			}
 		}
@@ -440,7 +441,7 @@ class Overlay {
 				 *
 				 * @event oOverlay#ready
 				 * @type {object}
-				 * @property {oOverlay} instance - the firing instance
+				 * @property {Overlay} instance - the firing instance
 				 */
 				this.broadcast('ready', 'oOverlay', {
 					instance: this
