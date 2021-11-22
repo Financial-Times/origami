81e79e271e09969492d5dc7729f1bfdcb08f8642
chee <chee@snoot.club>
2021-11-22 15:50:02 +0000

    backstage: Add a script to check the workspace has private set explicitly

---
+++ scripts/check-privacy.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+import * as workspaces from "./lib/workspaces.js"
+import {readPackage} from "read-pkg"
+
+let {exit} = process
+let {log} = console
+
+/** exit code */
+let code = 0
+
+let message = `
+make sure private is true for packages that should be published, and false for
+those that shouldn't.
+`
+
+for (let cwd of await workspaces.paths()) {
+	let pkg = await readPackage({cwd})
+	if (typeof pkg.private != "boolean") {
+		log(`${cwd}/package.json#private is not a boolean`)
+		code = 130
+	}
+}
+
+if (code != 0) {
+	log(message)
+}
+
+exit(code)
