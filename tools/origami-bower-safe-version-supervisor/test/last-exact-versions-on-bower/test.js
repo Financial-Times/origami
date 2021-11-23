50670745d724e819d3f7fd26d7ec9a4d24641b60
Jake Champion <me@jakechampion.name>
2021-11-23 22:53:14 +0000

    backstage: add tests for o-no

---
+++ tools/origami-bower-safe-version-supervisor/test/last-exact-versions-on-bower/test.js
@@ -0,0 +1,18 @@
+import tap from 'tap'
+import execa from 'execa'
+import dedentTabs from "dedent-tabs"
+const dedent = dedentTabs.default
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const oNoBinaryPath = join(__dirname, '../../index.js')
+
+tap.test('project which uses origami versions which are on both npm and bower', async t => {
+    const { stdout, stderr, exitCode } = await execa(oNoBinaryPath, {
+        reject: false
+    })
+    t.equal(exitCode, 0)
+    t.equal(stdout, '')
+    t.equal(stderr, '')
+})
