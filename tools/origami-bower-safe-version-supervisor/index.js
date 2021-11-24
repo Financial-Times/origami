fefe70d20087c834ae13e6c4468a7fc3a71cc319
Jake Champion <me@jakechampion.name>
2021-11-24 13:30:37 +0000

    backstage: switch to single quotes, just for fun

--- tools/origami-bower-safe-version-supervisor/index.js
+++ tools/origami-bower-safe-version-supervisor/index.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
-import process from "node:process"
-import {readPackageAsync} from "read-pkg"
-import semverExtra from "semver-extra"
-import origamiComponents from "./last-versions-available-on-bower.js"
-import majors from "major-versions"
-import isBounded from "semver-bounded"
-import exactVersion from "exact-version"
-import dedentTabs from "dedent-tabs"
+import process from 'node:process'
+import {readPackageAsync} from 'read-pkg'
+import semverExtra from 'semver-extra'
+import origamiComponents from './last-versions-available-on-bower.js'
+import majors from 'major-versions'
+import isBounded from 'semver-bounded'
+import exactVersion from 'exact-version'
+import dedentTabs from 'dedent-tabs'
 const dedent = dedentTabs.default
 
 function checkDependencies(dependencies) {
@@ -18,7 +18,7 @@ function checkDependencies(dependencies) {
 			if (name in origamiComponents) {
 				// Versions which end with `-bower` are only on bower and the version on npm which 
 				// is compatible will be the same version number but without `-bower` on the end.
-				const lastVersionAvailableOnBower = origamiComponents[name].replace("-bower", "")
+				const lastVersionAvailableOnBower = origamiComponents[name].replace('-bower', '')
 				const isUnbounded = versionRange === '*' || !isBounded.range(versionRange)
 				if (isUnbounded) {
 					dependenciesToDowngrade.push([name, lastVersionAvailableOnBower, versionRange])
@@ -83,7 +83,7 @@ async function main() {
 
 main()
 
-process.on("unhandledRejection", error => {
+process.on('unhandledRejection', error => {
 	console.error(error)
 	process.exitCode = 1
 })
