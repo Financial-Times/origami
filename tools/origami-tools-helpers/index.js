5c036507c071d96f98b98a2c808903f75e03cf90
chee <chee@snoot.club>
2021-11-30 16:35:55 +0000

    backstage: remove request for missing module

--- tools/origami-tools-helpers/index.js
+++ tools/origami-tools-helpers/index.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
 
-export * as files from './files.js'
-export { constructPolyfillUrl } from './construct-polyfill-url.js'
-export * as log from './log.js'
+export * as files from "./files.js"
+export * as log from "./log.js"
