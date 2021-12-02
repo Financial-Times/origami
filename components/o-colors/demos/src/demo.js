3a37406d6edf1a84845dfbf53c73083a39a49069
chee <chee@snoot.club>
2021-12-02 14:27:47 +0000

    docs: Use the navigator.clipboard api instead of execcommand

--- components/o-colors/demos/src/demo.js
+++ components/o-colors/demos/src/demo.js
@@ -1,58 +1,51 @@
-let clickToCopy = false;
+const oCopyClass = 'o-copy-true';
 
 function oColorsDemoPalette() {
 	const swatches = document.querySelectorAll('.swatch');
 
 	for (const swatch of swatches) {
 		const oColor = swatch.getAttribute('data-o-color');
-		const hexInput = swatch.querySelector('.hex');
-		const hex = window.getComputedStyle(document.body, null).getPropertyValue(`--o-colors-${oColor}`);
-		hexInput.value = hex ? hex.trim() : '';
+		const hexElement = swatch.querySelector('.hex');
 
-		if (clickToCopy) {
+		const hex = window
+			.getComputedStyle(document.body, null)
+			.getPropertyValue(`--o-colors-${oColor}`);
+
+		hexElement.textContent = hex ? hex.trim() : '';
+
+		if (document.body.classList.contains(oCopyClass)) {
 			swatch.addEventListener('click', oColorsCopy, false);
 		}
 	}
 }
 
+const sleep = time => new Promise(yay => setTimeout(yay, time));
+
 function oColorsCopy(event) {
 	// find target element
-	let input = event.target.querySelector('.hex');
-	let parent = event.target;
-	if (input === null && event.target.localName === 'input') {
-		input = event.target.parentNode.querySelector('.hex');
-		parent = event.target.parentNode;
-	}
-
-	// is element selectable?
-	if (input && input.select) {
-		// select text
-		input.focus();
-		input.select();
-
-		try {
-			// copy text
-			document.execCommand('copy');
-			input.blur();
+	const hexElement =
+		event.target.querySelector('.hex') || event.target.closest('.hex');
+	const container = hexElement.closest('.swatch');
+	// copy text
+	navigator.clipboard
+		.writeText(hexElement.textContent)
+		.then(() => {
 			// Setting the class changes the :after content to read "Copied!"
-			parent.classList.add('copied');
-			// Remove the class after 2s
-			setTimeout(function() {
-				parent.classList.remove('copied');
-			}, 2000);
-
-		} catch (err) {
+			container.classList.add('copied');
+			return sleep(2000);
+		})
+		.then(() => {
+			container.classList.remove('copied');
+		})
+		.catch(error => {
 			// eslint-disable-next-line no-console
-			console.log('Can not copy text. ', err);
-		}
-	}
+			console.log('Can not copy text. ', error);
+		});
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-	const randomInput = document.querySelectorAll('input')[0];
-	if (randomInput.select && document.execCommand) {
-		document.body.classList.add('o-copy-true');
-		clickToCopy = true;
+document.addEventListener('DOMContentLoaded', function () {
+	if (navigator.clipboard.writeText) {
+		document.body.classList.add(oCopyClass);
 	}
 
 	oColorsDemoPalette();
