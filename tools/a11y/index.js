a988bb27ada31ec49ab0dc7ecdc8fef20b2e5a6d
Jake Champion <me@jakechampion.name>
2021-12-09 19:28:12 +0000

    feat: remove pa11y and use axe directly
    This changes the a11y test suite to use axe directly with the axe/playwright system instead of using pa11y which was causing us some issues which we could not find a solution for. Pa11y began to return lots of false positives which were not detected when running the same test using several other accessibility testing tools, including using the exact same rules with Axe directly that Pa11y as passing to axe.

--- tools/a11y/index.js
+++ tools/a11y/index.js
@@ -1,77 +1,55 @@
 #!/usr/bin/env node
 
 import path from 'node:path'
-import {EOL} from 'node:os'
-import isCI from 'is-ci'
-import dedentTabs from "dedent-tabs"
 import {globby as glob} from "globby"
-import pa11y from 'pa11y'
-import colors from 'colors';
-const { bold } = colors;
-const dedent = dedentTabs.default
+import AxeBuilderPlaywright from '@axe-core/playwright'
+import playwright from 'playwright'
+import { pathToFileURL } from 'url';
+import { prettyPrintAxeReport } from 'axe-result-pretty-print';
+const AxeBuilder = AxeBuilderPlaywright.default;
 
 const cwd = process.cwd()
 
-const builtDemoPages = await glob(path.join(cwd, '/demos/local/*.html'), {onlyFiles: true})
+const builtDemoHtmlFiles = await glob(path.join(cwd, '/demos/local/*.html'), {onlyFiles: true})
 
-const pa11yRulesToIgnore = []
+const axeRulesToIgnore = [
 // ignoring the href="#" error
-pa11yRulesToIgnore.push('WCAG2AA.Principle2.Guideline2_4.2_4_1.G1,G123,G124.NoSuchID')
 // pa11y demos are for pa11y only and may include multiple versions
 // of a landmark component like o-footer
-pa11yRulesToIgnore.push('landmark-one-main')
-pa11yRulesToIgnore.push('landmark-no-duplicate-contentinfo')
-pa11yRulesToIgnore.push('landmark-unique')
+'landmark-one-main',
+'landmark-no-duplicate-contentinfo',
+'landmark-unique',
 // disable https://dequeuniversity.com/rules/axe/3.5/region?application=axeAPI
-pa11yRulesToIgnore.push('region')
-// disable https://dequeuniversity.com/rules/axe/3.5/bypass?application=axeAPI
-pa11yRulesToIgnore.push('bypass')
-// pa11y demos are for pa11y only and do not have a heading
-pa11yRulesToIgnore.push('page-has-heading-one')
+'region',
+// // disable https://dequeuniversity.com/rules/axe/3.5/bypass?application=axeAPI
+'bypass',
+// // pa11y demos are for pa11y only and do not have a heading
+'page-has-heading-one',
 // FIXME audio caption - the newspaper doesn't provide these, so either can
 //                       we. but in the future it would be good to remove this,
 //                       and provide captions.
-pa11yRulesToIgnore.push('audio-caption')
+'audio-caption',
+];
 
 const errors = [];
-for (const page of builtDemoPages) {
-	console.log(bold(`Running accessibility checks on ${page}.`));
-	const pageAccessibilityErrors = await runPa11y(page)
-	if (pageAccessibilityErrors) errors.push(pageAccessibilityErrors)
-	console.log();
+
+const browser = await playwright.chromium.launch();
+for (const file of builtDemoHtmlFiles) {
+	const context = await browser.newContext();
+	const page = await context.newPage();
+	await page.goto(pathToFileURL(file).toString());
+
+	const results = await new AxeBuilder({ page }).disableRules(axeRulesToIgnore).analyze();
+	prettyPrintAxeReport({
+        violations: results.violations,
+        passes: results.passes,
+        url: file,
+    });
+	errors.push(...results.violations);
 }
+await browser.close();
 
 if (errors.length) {
-	console.error(errors.join(EOL + EOL))
+	console.error(`${errors.length} accessibility errors were detected.`)
 	process.exit(1)
-}
-
-async function runPa11y (page) {
-	// Run the Pa11y tests
-	const results = await pa11y(page, {
-		ignore: pa11yRulesToIgnore,
-		runners: ['axe']
-	})
-
-	if (results.issues.some(result => result.type === 'error')) {
-		// Process and return the results
-		const errors = results.issues.filter(result => result.type === 'error')
-			.map(result => {
-				return dedent`
-					• Error [${result.code}] ${result.message}
-						├── Selector: ${result.selector.replace(/\s+/g, ' ')}
-						└── Context: ${result.context.replace(/\s+/g, ' ')}`
-			})
-		if (isCI) {
-			// GitHub's annotation system requires newlines to be URI-encoded
-			const newLine = encodeURIComponent(EOL)
-			errors.forEach(error => {
-				const message = error.replaceAll(/\n/g, newLine)
-				console.log(`::error file=${page},line=1,col=1::${message}`)
-			})
-		}
-		return dedent`${bold(`Accessibility errors were found in ${page}`)}
-				${errors.join('\n')}
-				Failed Pa11y tests`
-	}
 }
