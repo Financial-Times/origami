f49145eab4acaed27d8268138082e3480af3f02f
Jake Champion <me@jakechampion.name>
2021-12-24 12:37:34 +0000

    fix: run o-date in the demos to give the teasers a relative time

--- components/o-teaser/demos/src/demo.js
+++ components/o-teaser/demos/src/demo.js
@@ -1,5 +1,8 @@
+import ODate from '@financial-times/o-date';
+
 function initDemos() {
 	document.addEventListener('DOMContentLoaded', function() {
+		ODate.init();
 		document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
 	});
 }
