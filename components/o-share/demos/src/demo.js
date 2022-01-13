52307e83cd88a7da17bca608874d592d35a52532
Jake Champion <me@jakechampion.name>
2022-01-13 11:20:40 +0000

    fix: use the o-share js render method for demos
    using the render method ensures that the demo markup is up-to-date with how sharing is meant to be implemented
    
    this resolves https://github.com/Financial-Times/origami/issues/557

--- components/o-share/demos/src/demo.js
+++ components/o-share/demos/src/demo.js
@@ -1,6 +1,3 @@
 import Share from '../../main.js';
-window.oShare = Share;
 
-document.addEventListener('DOMContentLoaded', function() {
-	document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
-});
+Share.init();
