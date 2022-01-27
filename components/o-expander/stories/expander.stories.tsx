adbe36a5131357f9542b2c16e7dc6fbf657e2d64
chee <chee@snoot.club>
2022-01-27 13:43:32 +0000

    docs: add an o-expander story

---
+++ components/o-expander/stories/expander.stories.tsx
@@ -0,0 +1,37 @@
+import {Expander} from '../src/tsx/expander';
+import javascript from '../main.js';
+import './expander.scss';
+import withHtml from 'origami-storybook-addon-html';
+import {useEffect} from 'react';
+
+export default {
+	title: 'Expander',
+	component: Expander,
+	decorators: [withHtml],
+	parameters: {
+		html: {},
+	},
+	args: {},
+};
+
+const Template = ({children, ...args}) => {
+	useEffect(() => void javascript.init(), []);
+	return (
+		<Expander {...args} header={<h3>This content has further details</h3>}>
+			<p>wow bet you didn't expect details like this!!</p>
+			<ul>
+				<li>one</li>
+				<li>two</li>
+				<li>three</li>
+				<li>four</li>
+			</ul>
+		</Expander>
+	);
+};
+
+const Example = Template.bind({});
+Example.args = {
+	expanded: false,
+};
+
+export {Example as Expander};
