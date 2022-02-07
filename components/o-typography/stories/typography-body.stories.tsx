8e4c6c46989ce88e923f11d7f9012d58b88e8d7d
Lee Moody <notlee@users.noreply.github.com>
2022-02-07 18:31:40 +0000

    feat: add o-typography Stories

---
+++ components/o-typography/stories/typography-body.stories.tsx
@@ -0,0 +1,35 @@
+import {withDesign} from 'storybook-addon-designs';
+import {P, Span, Sup, Sub, Strong, Em, Link} from '../src/tsx/typography';
+import './typography.scss';
+import withHtml from 'origami-storybook-addon-html';
+
+export default {
+	title: 'Components/o-typography',
+	component: P,
+	decorators: [withDesign, withHtml],
+    args: {},
+	parameters: {
+		design: {
+			type: 'figma',
+			url: 'https://www.figma.com/file/MyHQ1qdwYyek5IBdhEEaND/FT-UI-Library?node-id=0%3A1433',
+		},
+		guidelines: {
+			notion: 'f82c8bd1ef1f4f649a61d17d5d49750c',
+		},
+		html: {},
+	},
+};
+
+const Story = args => <>
+    <P {...args}>
+        <Span weight='bold'>Body</Span> - Lorem ipsum dolor sit amet, consectetur adipisicing elit. <Link href="#">Link</Link> a rem <Strong>excepturi</Strong> consequuntur commodi dolores ad <Em>laboriosam</Em> qui odit ipsum distinctio quos laborum dolore magnam iure rerum, enim deleniti saepe sunt.
+    </P>
+    <P {...args}>
+        Lorem ipsum dolor sit amet<Sup>Sup</Sup>, consectetur adipisicing elit<Sub>Sub</Sub>. <Link href="#">Link Necessitatibus asperiores</Link>.
+    </P>
+    <P {...args}>
+        Lorem ipsum dolor <Link href="#" external={true}>sit amet consectetur</Link> adipisicing elit. Non atque doloribus quaerat, esse ipsum doloremque.
+    </P>
+</>;
+
+export const Body = Story.bind({});
---
+++ components/o-typography/stories/typography-headings.stories.tsx
@@ -0,0 +1,31 @@
+import {withDesign} from 'storybook-addon-designs';
+import {Heading} from '../src/tsx/typography';
+import './typography.scss';
+import withHtml from 'origami-storybook-addon-html';
+
+export default {
+	title: 'Components/o-typography',
+	component: Heading,
+	decorators: [withDesign, withHtml],
+    args: {},
+	parameters: {
+		design: {
+			type: 'figma',
+			url: 'https://www.figma.com/file/MyHQ1qdwYyek5IBdhEEaND/FT-UI-Library?node-id=0%3A1433',
+		},
+		guidelines: {
+			notion: 'f82c8bd1ef1f4f649a61d17d5d49750c',
+		},
+		html: {},
+	},
+};
+
+export const UIHeadings = args => <>
+	<Heading level={1} copy={args.copy || 'Heading level 1'} />
+	<Heading level={2} copy={args.copy || 'Heading level 2'} />
+	<Heading level={3} copy={args.copy || 'Heading level 3'} />
+	<Heading level={4} copy={args.copy || 'Heading level 4'} />
+	<Heading level={5} copy={args.copy || 'Heading level 5'} />
+	<Heading level={6} copy={args.copy || 'Heading level 6'} />
+</>;
+UIHeadings.args = {copy: '' };
