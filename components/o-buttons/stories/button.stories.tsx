7eaba29e91e8887c176fa214a595e452fbf08181
Lee Moody <notlee@users.noreply.github.com>
2022-02-08 09:44:21 +0000

    backstage: move button pagination and group components to own story tsx file
    the `component` field in the default export is used to create controls

--- components/o-buttons/stories/button.stories.tsx
+++ components/o-buttons/stories/button.stories.tsx
@@ -1,11 +1,7 @@
 import {withDesign} from 'storybook-addon-designs';
 import {Button, LinkButton} from '../src/tsx/button';
-import {ButtonGroup} from '../src/tsx/group';
-import {ButtonPagination} from '../src/tsx/pagination';
 import './button.scss';
 import withHtml from 'origami-storybook-addon-html';
-import { nanoid } from 'nanoid'
-import { useState } from 'react';
 
 export default {
 	title: 'Components/o-buttons',
@@ -26,39 +22,8 @@ export default {
 	},
 };
 
-const ButtonPaginationStory = args => {
-	const configuredCurrentPage = args.pages.find(page => page.current);
-	const [currentPageSelection, setCurrentPageSelection] = useState(configuredCurrentPage ? configuredCurrentPage.number : 0);
-
-	function updatePages(currentPageSelection) {
-		setCurrentPageSelection(currentPageSelection);
-		args.pages.forEach(p => p.current = p.number === currentPageSelection);
-	}
-
-	args.pages.forEach(page => {
-		page.onClick = e => {
-			e.preventDefault();
-			updatePages(page.number);
-		};
-	});
-
-	args.nextPager.onClick = e => {
-		e.preventDefault();
-		updatePages(currentPageSelection + 1);
-	};
-
-	args.previousPager.onClick = e => {
-		e.preventDefault();
-		updatePages(currentPageSelection - 1);
-	};
-
-	return <ButtonPagination {...args} />
-};
 const ButtonStory = args => <Button {...args} />;
 const LinkButtonStory = args => <LinkButton {...args} />;
-const ButtonGroupStory = args => <ButtonGroup>
-	{args.buttons.map(buttonProps => <Button {...buttonProps} key={nanoid()}/>)}
-</ButtonGroup>;
 
 export const PrimaryButton = ButtonStory.bind({});
 PrimaryButton.args = {
@@ -113,41 +78,4 @@ IconOnlyButton.args = {
 	label: 'Next',
 	icon: 'arrow-right',
 	iconOnly: true,
-};
-
-export const GroupedButtons = ButtonGroupStory.bind({});
-GroupedButtons.args = {
-	buttons: [{
-		label: 'button one',
-		type: 'secondary',
-	}, {
-		label: 'button two',
-		type: 'secondary',
-	}, {
-		label: 'button three',
-		type: 'secondary',
-	}]
-};
-
-export const Pagination = ButtonPaginationStory.bind({});
-Pagination.args = {
-	type: 'secondary',
-	size: 'big',
-	previousPager: {
-		href: '#previous',
-		label: 'previous results'
-	},
-  	pages: Array(10).fill(null).map((page, index) => {
-		const number = index + 1;
-		const currentPageNumber = 3;
-		return {
-			href: '#',
-			current: number === currentPageNumber,
-			number,
-		};
-	}),
-	nextPager: {
-		href: '#next',
-		label: 'next results'
-	},
 };
