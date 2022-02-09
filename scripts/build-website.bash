894f62a7799302d43dcafcd325b57041101d5d87
chee <chee.rabbits@ft.com>
2022-02-09 11:35:08 +0000

    backstage: remove the line that moves a file that doesn't exist

--- scripts/build-website.bash
+++ scripts/build-website.bash
@@ -9,7 +9,6 @@ bundle exec jekyll build -d ../../origami.ft.com --incremental
 cd ../storybook/
 npm run build-storybook
 cd ../../
-cp serve.json origami.ft.com
 
 if ! [ -z "$SHOULD_DELETE_ALL_YOUR_FILES" ]; then
 	rm -r apps components libraries tools
