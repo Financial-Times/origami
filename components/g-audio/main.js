551528c996aa7894c644e53529e5f90f3208e99e
Joanna Kao <joannaskao@gmail.com>
2022-03-24 11:51:45 +0000

    fix: Fix g-audio init

--- components/g-audio/main.js
+++ components/g-audio/main.js
@@ -1,19 +1,10 @@
-import AudioPlayer from './src/js/audio.js';
+import gAudio from './src/js/audio.js';
 
-const constructAll = () => {
-	AudioPlayer.init();
-
-	const audio_components = document.querySelectorAll('.g-audio');
-
-	for (let i = 0; i < audio_components.length; i++) {
-		const a = audio_components[i];
-		const audioURL = a.getElementsByTagName('source')[0].getAttribute('src');
-		new AudioPlayer(a, audioURL);
-	}
-
+const constructAll = function () {
+	gAudio.init();
 	document.removeEventListener('o.DOMContentLoaded', constructAll);
 };
 
 document.addEventListener('o.DOMContentLoaded', constructAll);
 
-export default AudioPlayer;
+export default gAudio;
