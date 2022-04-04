fe9fa72584590d7984a55e4102863c024af93425
Esteban Kocian <egkolo@gmail.com>
2022-04-04 13:22:28 -0300

    ACQ-1536: add actions as a new component for drawer menu

---
+++ actionsUK.ts
@@ -0,0 +1,5 @@
+import { TNavAction } from '@financial-times/dotcom-types-navigation'
+
+const data: TNavAction[] = [{ id: 'subscribe', name: 'Subscribe for full access', url: '#' }]
+
+export default data
