fa53f7de0c2f2eae1ca1d760faee4b39844e0fd9
Ivo Murrell <ivo.murrell@ft.com>
2022-05-30 09:58:16 +0100

    feat: wip add stories for o-labels demos
    Still need to make a nice interface for controlling dates in indicator
    and timestamp labels (mostly in the Storybook UI but I also need to
    confirm whether the JSX template is too restrictive or not by forcing
    users to use o-date.)

---
+++ components/o-labels/stories/timestamps.stories.tsx
@@ -0,0 +1,30 @@
+import ODate from '@financial-times/o-date/main';
+import type {ComponentMeta, ComponentStory} from '@storybook/react';
+import withHtml from 'origami-storybook-addon-html';
+import {useEffect} from 'react';
+import {withDesign} from 'storybook-addon-designs';
+import {TimestampLabel as OTimestampLabel} from '../src/tsx/label';
+import './labels.scss';
+
+export default {
+	title: 'Components/o-labels',
+	component: OTimestampLabel,
+	decorators: [withDesign, withHtml],
+	argTypes: {
+		inverse: {control: 'boolean', defaultValue: false},
+	},
+} as ComponentMeta<typeof OTimestampLabel>;
+
+export const TimestampLabel: ComponentStory<typeof OTimestampLabel> = args => {
+	useEffect(() => {
+		let dates = ODate.init();
+		return function cleanup() {
+			dates = Array.isArray(dates) ? dates : [dates];
+			dates.forEach(date => date.destroy());
+		};
+	});
+	return <OTimestampLabel {...args} />;
+};
+TimestampLabel.args = {
+	dateTime: new Date(2022, 1, 10, 8, 30).toISOString(),
+};
