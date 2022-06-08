781cb03a7451654f8be3b9757b686c6c2d44c499
Akaki Mikaia <akaki.mikaia.1@iliauni.edu.ge>
2022-06-08 15:17:11 +0100

    fix: move InnerLayout to story.tsx, add demo styling and handle react errors in dev console

--- components/o-message/src/tsx/message.tsx
+++ components/o-message/src/tsx/message.tsx
@@ -1,5 +1,3 @@
-import { Fragment } from 'react';
-
 type MessageContent = {
 	detail: string;
 	highlight?: string;
@@ -28,10 +26,6 @@ type TextContentPropType = {
 	text: string;
 }
 
-const InnerLayout = ({ condition, wrapper, children }) => {
-	return condition ? wrapper(children) : children;
-}
-
 const AdditionalInfo = ({text}: TextContentPropType) => {
 	return <p className="o-message__content-additional">{text}</p>
 }
@@ -57,28 +51,20 @@ export function Message({
 	}
 
   return (
-		<InnerLayout
-			condition={inner}
-			// I had to add inline style to get the same feeling as in the registry.
-			// class demo-inner-message does not set those styles
-			wrapper={children => <div className="demo-inner-message" style={{margin: "auto", maxWidth: "400px"}}>{children}</div>}>
-			<Fragment>
-				<div className={`o-message o-message--${type} o-message--${state} ${inner ? "o-message--inner": ""}`} {...dataAttributes} data-o-component="o-message">
-					<div className="o-message__container">
-						<div className="o-message__content">
-							<p className="o-message__content-main">
-								{content.hightLight && <Highlight text={content.hightLight} />}
-								{content.detail}
-							</p>
-							{inner && <AdditionalInfo text={content.additionalInfo}/>}
-							<div className="o-message__actions">
-								<a href={primaryAction.url} className="o-message__actions__primary" target={primaryAction.openInNewWindow && '_blank'} >{primaryAction.copy}</a>
-								{state !== 'feedback' && <a href={secondaryAction.url} className="o-message__actions__secondary" target={secondaryAction.openInNewWindow && '_blank'}>{secondaryAction.copy}</a>}
-							</div>
-						</div>
-					</div>
+	<div className={`o-message o-message--${type} o-message--${state} ${inner ? "o-message--inner": ""}`} {...dataAttributes} data-o-component="o-message">
+		<div className="o-message__container">
+			<div className="o-message__content">
+				<p className="o-message__content-main">
+					{content.highlight && <Highlight text={content.highlight} />}
+					{content.detail}
+				</p>
+				{inner && <AdditionalInfo text={content.additionalInfo}/>}
+				<div className="o-message__actions">
+					<a href={primaryAction.url} className="o-message__actions__primary" target={primaryAction.openInNewWindow ? '_blank' : '_self'} >{primaryAction.text}</a>
+					{state !== 'feedback' && <a href={secondaryAction.url} className="o-message__actions__secondary" target={secondaryAction.openInNewWindow ? '_blank' : '_self'}>{secondaryAction.text}</a>}
 				</div>
-			</Fragment>
-		</InnerLayout>
+			</div>
+		</div>
+	</div>
   );
 }
