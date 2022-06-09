e283f1bf59e8ebd4b9678acbb3690abe026e6a64
Akaki Mikaia <akaki.mikaia.1@iliauni.edu.ge>
2022-06-09 11:00:30 +0100

    backstage: format files using prettier

--- components/o-message/src/tsx/message.tsx
+++ components/o-message/src/tsx/message.tsx
@@ -2,17 +2,25 @@ type MessageContent = {
 	detail: string;
 	highlight?: string;
 	additionalInfo?: string;
-}
+};
 
 type MessageAction = {
 	text: string;
 	url: string;
-	openInNewWindow?: boolean
-}
+	openInNewWindow?: boolean;
+};
 
 type MessageProps = {
 	type: 'action' | 'alert' | 'notice';
-	state: 'success' | 'neutral' | 'error' | 'feedback' | 'warning' | 'warning-light' | 'inform' | 'inform-inverse'
+	state:
+		| 'success'
+		| 'neutral'
+		| 'error'
+		| 'feedback'
+		| 'warning'
+		| 'warning-light'
+		| 'inform'
+		| 'inform-inverse';
 	content: MessageContent;
 	primaryAction?: MessageAction;
 	secondaryAction?: MessageAction;
@@ -20,20 +28,19 @@ type MessageProps = {
 	showCloseButton?: boolean;
 	autoOpen?: boolean;
 	parentElement?: string;
-}
+};
 
 type TextContentPropType = {
 	text: string;
-}
+};
 
 const AdditionalInfo = ({text}: TextContentPropType) => {
-	return <p className="o-message__content-additional">{text}</p>
-}
+	return <p className="o-message__content-additional">{text}</p>;
+};
 
 const Highlight = ({text}: TextContentPropType) => {
-	return <span className="o-message__content-highlight"> {text} </span>
-}
-
+	return <span className="o-message__content-highlight"> {text} </span>;
+};
 
 export function Message({
 	type = 'alert',
@@ -41,24 +48,41 @@ export function Message({
 	content = undefined,
 	primaryAction = undefined,
 	secondaryAction = undefined,
-	inner=false,
-	showCloseButton=true
+	inner = false,
+	showCloseButton = true,
 }: MessageProps) {
-  return (
-	<div className={`o-message o-message--${type} o-message--${state} ${inner ? "o-message--inner": ""}`} data-o-message-close={showCloseButton.toString()} data-o-component="o-message">
-		<div className="o-message__container">
-			<div className="o-message__content">
-				<p className="o-message__content-main">
-					{content.highlight && <Highlight text={content.highlight} />}
-					{content.detail}
-				</p>
-				{inner && <AdditionalInfo text={content.additionalInfo}/>}
-				<div className="o-message__actions">
-					<a href={primaryAction.url} className="o-message__actions__primary" target={primaryAction.openInNewWindow ? '_blank' : '_self'} >{primaryAction.text}</a>
-					{state !== 'feedback' && <a href={secondaryAction.url} className="o-message__actions__secondary" target={secondaryAction.openInNewWindow ? '_blank' : '_self'}>{secondaryAction.text}</a>}
+	return (
+		<div
+			className={`o-message o-message--${type} o-message--${state} ${
+				inner ? 'o-message--inner' : ''
+			}`}
+			data-o-message-close={showCloseButton.toString()}
+			data-o-component="o-message">
+			<div className="o-message__container">
+				<div className="o-message__content">
+					<p className="o-message__content-main">
+						{content.highlight && <Highlight text={content.highlight} />}
+						{content.detail}
+					</p>
+					{inner && <AdditionalInfo text={content.additionalInfo} />}
+					<div className="o-message__actions">
+						<a
+							href={primaryAction.url}
+							className="o-message__actions__primary"
+							target={primaryAction.openInNewWindow ? '_blank' : '_self'}>
+							{primaryAction.text}
+						</a>
+						{state !== 'feedback' && (
+							<a
+								href={secondaryAction.url}
+								className="o-message__actions__secondary"
+								target={secondaryAction.openInNewWindow ? '_blank' : '_self'}>
+								{secondaryAction.text}
+							</a>
+						)}
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-  );
+	);
 }
--- components/o-message/stories/message.stories.tsx
+++ components/o-message/stories/message.stories.tsx
@@ -1,7 +1,7 @@
 import withHtml from 'origami-storybook-addon-html';
 import {withDesign} from 'storybook-addon-designs';
-import {ComponentStory, ComponentMeta} from '@storybook/react'
-import { useEffect } from 'react';
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+import {useEffect} from 'react';
 import {Message} from '../src/tsx/message';
 import javascript from '../main';
 import './message.scss';
@@ -17,146 +17,153 @@ export default {
 		primaryAction: {
 			text: 'Button',
 			url: '#',
-			openInNewWindow: false
+			openInNewWindow: false,
 		},
 		secondaryAction: {
 			text: 'Text link',
 			url: '#',
-			openInNewWindow:  false
-		}
+			openInNewWindow: false,
+		},
 	},
-  } as ComponentMeta<typeof Message>;
+} as ComponentMeta<typeof Message>;
 
-
-const innerDecorator = (Story) => <div style={{ margin: "auto", maxWidth: "400px" }}>{Story()}</div>
+const innerDecorator = Story => (
+	<div style={{margin: 'auto', maxWidth: '400px'}}>{Story()}</div>
+);
 
 const Story = args => {
 	useEffect(() => {
-		let messages = javascript.init()
+		let messages = javascript.init();
 		return () => {
 			messages = Array.isArray(messages) ? messages : [messages];
 			messages.forEach(message => message.destroy());
-		}
-	}, [args.showCloseButton])
-	return <Message {...args} />
+		};
+	}, [args.showCloseButton]);
+	return <Message {...args} />;
 };
-
-
 
 export const AlertSuccess: ComponentStory<typeof Message> = Story.bind({});
-AlertSuccess.storyName = "Alert: Success"
+AlertSuccess.storyName = 'Alert: Success';
 AlertSuccess.args = {
 	type: 'alert',
-	state: "success",
+	state: 'success',
 	content: {
 		detail: 'The quick brown fox jumped over the lazy dogs!',
 		highlight: 'Hooray!',
 	},
 };
 
-export const AlertInnerSuccess: ComponentStory<typeof Message> = Story.bind({})
-AlertInnerSuccess.decorators = [innerDecorator]
-AlertInnerSuccess.storyName = "Alert Inner: Success"
+export const AlertInnerSuccess: ComponentStory<typeof Message> = Story.bind({});
+AlertInnerSuccess.decorators = [innerDecorator];
+AlertInnerSuccess.storyName = 'Alert Inner: Success';
 AlertInnerSuccess.args = {
 	type: 'alert',
-	state: "success",
+	state: 'success',
 	content: {
 		detail: 'The quick brown fox jumped over the lazy dogs!',
 		highlight: 'Hooray!',
-		additionalInfo: "Did you know that that sentence uses all of the letters in the alphabet at least once?"
+		additionalInfo:
+			'Did you know that that sentence uses all of the letters in the alphabet at least once?',
 	},
 	inner: true,
-}
+};
 
-export const AlertNeutral: ComponentStory<typeof Message> = Story.bind({})
-AlertNeutral.storyName = "Alert: Neutral"
+export const AlertNeutral: ComponentStory<typeof Message> = Story.bind({});
+AlertNeutral.storyName = 'Alert: Neutral';
 AlertNeutral.args = {
 	type: 'alert',
-	state: "neutral",
+	state: 'neutral',
 	content: {
-		detail: "There's a fox, and some lazy dogs. Many lazy dogs. Many, many, many lazy dogs. Only one fox. Many, many, many lazy dogs."
+		detail:
+			"There's a fox, and some lazy dogs. Many lazy dogs. Many, many, many lazy dogs. Only one fox. Many, many, many lazy dogs.",
 	},
-}
+};
 
-export const AlertInnerNeutral: ComponentStory<typeof Message> = Story.bind({})
-AlertInnerNeutral.decorators = [innerDecorator]
-AlertInnerNeutral.storyName = "Alert Inner: Neutral"
+export const AlertInnerNeutral: ComponentStory<typeof Message> = Story.bind({});
+AlertInnerNeutral.decorators = [innerDecorator];
+AlertInnerNeutral.storyName = 'Alert Inner: Neutral';
 AlertInnerNeutral.args = {
 	type: 'alert',
-	state: "neutral",
+	state: 'neutral',
 	content: {
 		detail: 'The quick brown fox did no jump over the lazy dogs!',
 		highlight: 'Meh.',
 	},
 	inner: true,
-}
+};
 
-
-export const AlertError: ComponentStory<typeof Message> = Story.bind({})
-AlertError.storyName = "Alert: Error"
+export const AlertError: ComponentStory<typeof Message> = Story.bind({});
+AlertError.storyName = 'Alert: Error';
 AlertError.args = {
 	type: 'alert',
-	state: "error",
+	state: 'error',
 	content: {
 		detail: 'The quick brown fox did not jump over the lazy dogs.',
 	},
-}
+};
 
-export const AlertInnerError: ComponentStory<typeof Message> = Story.bind({})
-AlertInnerError.decorators = [innerDecorator]
-AlertInnerError.storyName = "Alert Inner: Error"
+export const AlertInnerError: ComponentStory<typeof Message> = Story.bind({});
+AlertInnerError.decorators = [innerDecorator];
+AlertInnerError.storyName = 'Alert Inner: Error';
 AlertInnerError.args = {
 	type: 'alert',
-	state: "error",
+	state: 'error',
 	content: {
 		detail: 'The quick brown fox did not jump over the lazy dogs.',
 		highlight: 'Oops.',
-		additionalInfo: 'But that sentence still uses all of the letters in the alphabet at least once!'
+		additionalInfo:
+			'But that sentence still uses all of the letters in the alphabet at least once!',
 	},
 	inner: true,
-}
+};
 
-export const NoticeInform: ComponentStory<typeof Message> = Story.bind({})
-NoticeInform.storyName = "Notice: Inform"
+export const NoticeInform: ComponentStory<typeof Message> = Story.bind({});
+NoticeInform.storyName = 'Notice: Inform';
 NoticeInform.args = {
 	type: 'notice',
-	state: "inform",
+	state: 'inform',
 	content: {
-		detail: "There are lazy dogs, maybe in a field, maybe not. It's important that you are informed of this fact.",
+		detail:
+			"There are lazy dogs, maybe in a field, maybe not. It's important that you are informed of this fact.",
 	},
-}
+};
 
-export const NoticeInnerInform: ComponentStory<typeof Message> = Story.bind({})
-NoticeInnerInform.decorators = [innerDecorator]
-NoticeInnerInform.storyName = "Notice Inner: Inform"
+export const NoticeInnerInform: ComponentStory<typeof Message> = Story.bind({});
+NoticeInnerInform.decorators = [innerDecorator];
+NoticeInnerInform.storyName = 'Notice Inner: Inform';
 NoticeInnerInform.args = {
 	type: 'notice',
-	state: "inform",
+	state: 'inform',
 	content: {
-		detail: "There are lazy dogs, maybe in a field, maybe not. It's important that you are informed of this fact.",
+		detail:
+			"There are lazy dogs, maybe in a field, maybe not. It's important that you are informed of this fact.",
 	},
 	inner: true,
-	showCloseButton: false
-}
+	showCloseButton: false,
+};
 
-export const NoticeFeedback: ComponentStory<typeof Message> = Story.bind({})
-NoticeFeedback.storyName = "Notice: Feedback"
+export const NoticeFeedback: ComponentStory<typeof Message> = Story.bind({});
+NoticeFeedback.storyName = 'Notice: Feedback';
 NoticeFeedback.args = {
 	type: 'notice',
-	state: "feedback",
+	state: 'feedback',
 	content: {
-		detail: "There are lazy dogs, maybe in a field, maybe not. It's important that you are informed of this fact.",
+		detail:
+			"There are lazy dogs, maybe in a field, maybe not. It's important that you are informed of this fact.",
 	},
-}
+};
 
-export const NoticeInnerFeedback: ComponentStory<typeof Message> = Story.bind({})
-NoticeInnerFeedback.decorators = [innerDecorator]
-NoticeInnerFeedback.storyName = "Notice Inner: Feedback"
+export const NoticeInnerFeedback: ComponentStory<typeof Message> = Story.bind(
+	{}
+);
+NoticeInnerFeedback.decorators = [innerDecorator];
+NoticeInnerFeedback.storyName = 'Notice Inner: Feedback';
 NoticeInnerFeedback.args = {
 	type: 'notice',
-	state: "feedback",
+	state: 'feedback',
 	content: {
-		detail: "There are lazy dogs, maybe in a field, maybe not. It's important that you are informed of this fact. There may also be a fox. This is unconfirmed.",
+		detail:
+			"There are lazy dogs, maybe in a field, maybe not. It's important that you are informed of this fact. There may also be a fox. This is unconfirmed.",
 	},
 	inner: true,
-}
+};
