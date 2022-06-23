d668f9a08e96c4f27f1cbebad69a54750be18b93
chee <chee@snoot.club>
2022-06-23 13:19:25 +0100

    backstage: add an o-loading tsx template

---
+++ components/o-loading/src/tsx/loading.tsx
@@ -0,0 +1,10 @@
+export interface LoadingProps {
+	size?: 'mini' | 'small' | 'medium' | 'large';
+	theme?: 'light' | 'dark';
+}
+
+export function Loading({theme, size}: LoadingProps) {
+	return (
+		<div className={`o-loading o-loading--${theme} o-loading--${size}`}></div>
+	);
+}
