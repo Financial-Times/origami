be3b0e70475211f4e6a11f2493c7940224239df0
Akaki Mikaia <akaki.mikaia.1@iliauni.edu.ge>
2022-08-02 12:11:21 +0100

    backstage: improve intro doc

--- apps/storybook/wrapper/Intro.stories.tsx
+++ apps/storybook/wrapper/Intro.stories.tsx
@@ -6,16 +6,20 @@ export default {
 export const Intro = () => {
 	return (
 		<div>
-			<p>
-				💡 Limitation: Addons in composed Storybooks might not work as they
-				normally do in non-composed Storybook. But you can view each brand on
-				it's own url where addons work as expected.
-			</p>
 			<p>Currently supported brands: </p>
 			<ul>
 				<li>Core</li>
 				<li>Internal</li>
 			</ul>
+			<p>
+				💡 Limitation: Addons in composed Storybooks might not work as they
+				normally do in non-composed Storybook. But you can view each brand on
+				it's own url where addons work as expected.
+			</p>
+			<p>
+				💡 Tip: When you click 🌐 Browse components it will open set of brand components in a new tab,
+				 but this pages are served from GitHub pages and you will need to add `/` at the end of url to correctly load it.
+			</p>
 		</div>
 	)
 }
