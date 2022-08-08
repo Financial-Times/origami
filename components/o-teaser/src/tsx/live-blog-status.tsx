20a3dda8835c7187a840749d79c57d0f9411f3eb
Akaki Mikaia <akaki.mikaia.1@iliauni.edu.ge>
2022-08-08 14:17:18 +0100

    fix: fix undefined for status

--- components/o-teaser/src/tsx/live-blog-status.tsx
+++ components/o-teaser/src/tsx/live-blog-status.tsx
@@ -15,7 +15,7 @@ const LiveBlogModifiers = {
 export default (props: Status) =>
 	props.status && props.status !== 'closed' ? (
 		<div
-			className={`o-teaser__timestamp o-teaser__timestamp--${LiveBlogModifiers[status]}`}>
-			<span className="o-teaser__timestamp-prefix">{` ${LiveBlogLabels[status]} `}</span>
+			className={`o-teaser__timestamp o-teaser__timestamp--${LiveBlogModifiers[props.status]}`}>
+			<span className="o-teaser__timestamp-prefix">{` ${LiveBlogLabels[props.status]} `}</span>
 		</div>
 	) : null;
