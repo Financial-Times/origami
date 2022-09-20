37b9ed241be9b6848d9970eb8e5876b1d7cecc24
Lee Moody <notlee@users.noreply.github.com>
2022-09-20 10:05:54 +0100

    docs: add o-spacing storybook (#815)
    I do not think it makes sense to provide public tsx templates for o-spacing

---
+++ components/o-spacing/stories/spacing.stories.tsx
@@ -0,0 +1,19 @@
+import {withDesign} from 'storybook-addon-designs';
+import './spacing.scss';
+import {SpacingDemo} from './spacing-demo';
+import withHtml from 'origami-storybook-addon-html';
+
+export default {
+	title: 'Components/o-spacing',
+    component: SpacingDemo,
+	decorators: [withDesign, withHtml],
+	parameters: {
+		guidelines: {},
+		html: {},
+	},
+};
+
+export const Spacing = SpacingDemo.bind({});
+Spacing.args = {
+    name: 'm12'
+};
