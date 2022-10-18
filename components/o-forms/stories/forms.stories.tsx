b93b5bfa801f23d2c8baf5927ecffb58a254019b
Akaki Mikaia <akakimikaia@gmail.com>
2022-10-18 11:09:59 +0100

    backstage: format code with prettier and add IDs on input elements for better accessiblity

--- components/o-forms/stories/forms.stories.tsx
+++ components/o-forms/stories/forms.stories.tsx
@@ -1,10 +1,10 @@
 import withHtml from 'origami-storybook-addon-html';
-import { withDesign } from 'storybook-addon-designs';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { useEffect } from 'react';
-import { Forms } from '../src/tsx/forms';
+import {withDesign} from 'storybook-addon-designs';
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+import {useEffect} from 'react';
+import {Forms} from '../src/tsx/forms';
 import './forms.scss';
-import javascript from '../main.js'
+import javascript from '../main.js';
 
 export default {
 	title: 'Components/o-forms/radio-box-buttons',
@@ -13,8 +13,8 @@ export default {
 	parameters: {},
 	args: {
 		highlight: false,
-		centered: false
-	}
+		centered: false,
+	},
 } as ComponentMeta<typeof Forms>;
 
 const FormsStory = args => {
@@ -28,183 +28,178 @@ const FormsStory = args => {
 	return <Forms {...args} />;
 };
 
-export const BoxRadioButton: ComponentStory<typeof Forms> = FormsStory.bind(
-	{}
-);
+export const BoxRadioButton: ComponentStory<typeof Forms> = FormsStory.bind({});
 
-export const MultipleBoxRadioButton: ComponentStory<typeof Forms> = FormsStory.bind(
-	{}
-);
+export const MultipleBoxRadioButton: ComponentStory<typeof Forms> =
+	FormsStory.bind({});
 
-export const DisabledBoxRadioButton: ComponentStory<typeof Forms> = FormsStory.bind(
-	{}
-);
+export const DisabledBoxRadioButton: ComponentStory<typeof Forms> =
+	FormsStory.bind({});
 
-export const ErrorBoxRadioButton: ComponentStory<typeof Forms> = FormsStory.bind(
-	{}
-);
+export const ErrorBoxRadioButton: ComponentStory<typeof Forms> =
+	FormsStory.bind({});
 
-
-export const StateBoxRadioButton: ComponentStory<typeof Forms> = FormsStory.bind(
-	{}
-);
-
+export const StateBoxRadioButton: ComponentStory<typeof Forms> =
+	FormsStory.bind({});
 
 BoxRadioButton.args = {
 	inputs: [
 		{
-			type: "radio",
-			name: "negative",
-			value: "Daily",
-			checked: true
+			type: 'radio',
+			name: 'negative',
+			value: 'Daily',
+			checked: true,
+			id: 'Daily',
 		},
 		{
-			type: "radio",
-			name: "negative",
-			value: "Weekly"
-		}
+			type: 'radio',
+			name: 'negative',
+			value: 'Weekly',
+			id: 'Weekly',
+		},
 	],
 	formField: {
 		field: {
-			type: "radio-box",
+			type: 'radio-box',
 			aria: {
-				label: "negative-radio-box-group-title",
-				info: "negative-radio-box-group-info"
-			}
+				label: 'negative-radio-box-group-title',
+				info: 'negative-radio-box-group-info',
+			},
 		},
 		title: {
-			mainTitle: "Box Radio Button",
-			promptTitle: "This is a box radio button sample"
-		}
+			mainTitle: 'Box Radio Button',
+			promptTitle: 'This is a box radio button sample',
+		},
 	},
-
-}
-
+};
 
 MultipleBoxRadioButton.args = {
 	inputs: [
 		{
-			type: "radio",
-			name: "multiple",
-			value: "Daily"
+			type: 'radio',
+			name: 'multiple',
+			value: 'Daily',
+			id: 'Daily',
 		},
 		{
-			type: "radio",
-			name: "multiple",
-			value: "Weekly",
-			checked: true
+			type: 'radio',
+			name: 'multiple',
+			value: 'Weekly',
+			checked: true,
+			id: 'Weekly',
 		},
 		{
-			type: "radio",
-			name: "multiple",
-			value: "Monthly"
-		}
+			type: 'radio',
+			name: 'multiple',
+			value: 'Monthly',
+			id: 'Monthly',
+		},
 	],
 	formField: {
 		field: {
-			type: "radio-box",
+			type: 'radio-box',
 			aria: {
-				label: "multiple-radio-box-group-title"
-			}
+				label: 'multiple-radio-box-group-title',
+			},
 		},
 		title: {
-			mainTitle: "Multiple box-styled radio buttons"
+			mainTitle: 'Multiple box-styled radio buttons',
 		},
 	},
-
-}
-
+};
 
 DisabledBoxRadioButton.args = {
 	inputs: [
 		{
-			type: "radio",
-			name: "disabled",
-			value: "Daily",
+			type: 'radio',
+			name: 'disabled',
+			value: 'Daily',
 			checked: true,
-			disabled: true
+			disabled: true,
+			id: 'Daily',
 		},
 		{
-			type: "radio",
-			name: "disabled",
-			value: "Monthly",
-			disabled: true
-		}
+			type: 'radio',
+			name: 'disabled',
+			value: 'Monthly',
+			disabled: true,
+			id: 'Monthly',
+		},
 	],
 	formField: {
 		field: {
-			type: "radio-box",
+			type: 'radio-box',
 			aria: {
-				label: "disabled-radio-box-group-title"
-			}
+				label: 'disabled-radio-box-group-title',
+			},
 		},
 		title: {
-			mainTitle: "Disabled box-styled radio buttons"
+			mainTitle: 'Disabled box-styled radio buttons',
 		},
 	},
-
-}
-
+};
 
 ErrorBoxRadioButton.args = {
 	inputs: [
 		{
-			type: "radio",
-			name: "error",
-			value: "Yes"
+			type: 'radio',
+			name: 'error',
+			value: 'Yes',
+			id: 'Yes',
 		},
 		{
-			type: "radio",
-			name: "error",
-			value: "No"
-		}
+			type: 'radio',
+			name: 'error',
+			value: 'No',
+			id: 'No',
+		},
 	],
 	formField: {
 		field: {
-			type: "radio-box",
+			type: 'radio-box',
 			aria: {
-				label: "error-radio-box-group-title"
+				label: 'error-radio-box-group-title',
 			},
-			error: "Please select an option",
-			modifiers: ["invalid"]
+			error: 'Please select an option',
+			modifiers: ['invalid'],
 		},
 		title: {
-			mainTitle: "Error box-style radio buttons"
+			mainTitle: 'Error box-style radio buttons',
 		},
 	},
-
-}
-
+};
 
 StateBoxRadioButton.args = {
 	saved: false,
 	inputs: [
 		{
-			type: "radio",
-			name: "icon",
-			value: "Daily"
+			type: 'radio',
+			name: 'icon',
+			value: 'Daily',
+			id: 'Daily',
 		},
 		{
-			type: "radio",
-			name: "icon",
-			value: "Weekly",
-			checked: true
-		}
+			type: 'radio',
+			name: 'icon',
+			value: 'Weekly',
+			checked: true,
+			id: 'Weekly',
+		},
 	],
 	formField: {
 		field: {
-			type: "radio-box",
+			type: 'radio-box',
 			aria: {
-				label: "saving-state-group-title",
-				info: "saving-state-group-info"
+				label: 'saving-state-group-title',
+				info: 'saving-state-group-info',
 			},
-			modifiers: ["saving", "inline"],
-			state: "Saving",
-			icon: true
+			modifiers: ['saving', 'inline'],
+			state: 'Saving',
+			icon: true,
 		},
 		title: {
-			mainTitle: "Radio box with state",
-			promptTitle: "Icon available for saved/saving states"
+			mainTitle: 'Radio box with state',
+			promptTitle: 'Icon available for saved/saving states',
 		},
-	}
-}
+	},
+};
