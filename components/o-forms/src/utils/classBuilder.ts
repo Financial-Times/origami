b08046d1f3b288549f29ac8edf5095b378377a4b
Andy Little <69594249+andy-little@users.noreply.github.com>
2022-11-02 15:16:16 +0000

    backstage: fixed spelling

--- components/o-forms/src/utils/classBuilder.ts
+++ components/o-forms/src/utils/classBuilder.ts
@@ -1,12 +1,12 @@
 export const classBuilder = (base: string, addBaseToClasses: boolean = true ):[Function, Function] => {
 	const classes: string[] = [addBaseToClasses ? base : ''];
 
-	const addPropery = (property: string, addSeparator: boolean = true) => {
+	const addProperty = (property: string, addSeparator: boolean = true) => {
 		classes.push(`${base}${addSeparator ? '--' : ''}${property}`)
 	}
 
 	const getClasses = () => {
 		return classes.join(' ')
 	}
-	return [addPropery, getClasses]
+	return [addProperty, getClasses]
 }
