03aefdf28085929214e1385f9a82f911344bb085
Andy Little <69594249+andy-little@users.noreply.github.com>
2022-11-03 14:39:17 +0000

    backstage: added file input

--- components/o-forms/src/tsx/o-forms.ts
+++ components/o-forms/src/tsx/o-forms.ts
@@ -3,10 +3,12 @@ import {RadioBtns, RadioBtn} from './RadioBtns';
 import {RadioBoxLinks, RadioBoxLink} from './RadioBoxLinks';
 import {TextInput} from './TextInput';
 import {Select} from './Select';
+import {FileInput} from './FileInput';
 
 export {
 	Form,
 	TextInput,
+	FileInput,
 	RadioBtn,
 	RadioBtns,
 	RadioBoxLinks,
