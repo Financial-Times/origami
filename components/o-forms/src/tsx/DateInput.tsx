e4e51af751f538e9b0137ad696b745d9701c102f
Andy Little <69594249+andy-little@users.noreply.github.com>
2022-11-04 09:45:50 +0000

    backstage: added date input

--- components/o-forms/src/tsx/DateInput.tsx
+++ components/o-forms/src/tsx/DateInput.tsx
@@ -2,10 +2,108 @@ import uniqueId from 'lodash.uniqueid';
 import {classBuilder} from '../utils/classBuilder';
 import {InputProps, FormError, FormFieldset, TypeFormField} from './Form';
 
+type onChange = {
+	days: Function;
+	months: Function;
+	years: Function;
+};
 
-function DateInput(){
-	const fieldProps = {}
-	return <FormFieldset {...fieldProps}>
+type ref = {
+	days: any;
+	months: any;
+	years: any;
+};
 
-	</FormFieldset>
+type values = {
+	days: string;
+	months: string;
+	years: string;
+};
+
+interface DateInputProps extends TypeFormField, InputProps {
+	values?: values;
+	onChange?: onChange;
+	ref?: ref;
+	errorMessage?: string;
+}
+
+export function DateInput({
+	values,
+	onChange,
+	ref,
+	required,
+	disabled,
+	errorMessage,
+	title,
+	description,
+	isOptional,
+	inlineField,
+	isVerticalCenter,
+}: DateInputProps) {
+	const fieldProps = {
+		title,
+		description,
+		isOptional,
+		inlineField,
+		isVerticalCenter,
+	};
+	const dayId = uniqueId('day_');
+	const monthId = uniqueId('month_');
+	const yearId = uniqueId('year_');
+	const [addClass, getClasses] = classBuilder('o-forms-input');
+	if (errorMessage) addClass('invalid');
+	if (inlineField) addClass('inline'); /* I dont think fits other inputs  */
+	return (
+		<FormFieldset {...fieldProps}>
+			<span className={`o-forms-input--date ${getClasses()}`}>
+				<label htmlFor={dayId}>
+					<input
+						id={dayId}
+						type="text"
+						name="date"
+						value={values?.days}
+						className="o-forms-input__day-part"
+						inputMode="numeric"
+						pattern="0[1-9]|[12]\d|3[01]"
+						disabled={disabled}
+						required={required}
+						onChange={onChange ? event => onChange?.days(event) : null}
+						ref={ref?.days}
+					/>
+
+					<span className="o-forms-input__label"> DD </span>
+				</label>
+				<label htmlFor={monthId}>
+					<input
+						id={monthId}
+						type="text"
+						name="date"
+						value={values?.months}
+						pattern="0?[1-9]|1[012]"
+						disabled={disabled}
+						required={required}
+						onChange={onChange ? event => onChange?.months(event) : null}
+						ref={ref?.months}
+					/>
+
+					<span className="o-forms-input__label"> MM </span>
+				</label>
+				<label htmlFor={yearId}>
+					<input
+						id={yearId}
+						type="text"
+						name="date"
+						value={values?.years}
+						pattern="[0-9]{4}"
+						disabled={disabled}
+						required={required}
+						onChange={onChange ? event => onChange?.years(event) : null}
+						ref={ref?.years}
+					/>
+					<span className="o-forms-input__label"> YYYY </span>
+				</label>
+				{errorMessage && <FormError errorMessage={errorMessage} />}
+			</span>
+		</FormFieldset>
+	);
 }
--- components/o-forms/src/tsx/o-forms.ts
+++ components/o-forms/src/tsx/o-forms.ts
@@ -4,11 +4,13 @@ import {RadioBoxLinks, RadioBoxLink} from './RadioBoxLinks';
 import {TextInput} from './TextInput';
 import {Select} from './Select';
 import {FileInput} from './FileInput';
+import {DateInput} from './DateInput';
 
 export {
 	Form,
 	TextInput,
 	FileInput,
+	DateInput,
 	RadioBtn,
 	RadioBtns,
 	RadioBoxLinks,
