ca7cbe3f8575eb998adbd7971150d478b41c45af
Lee Moody <notlee@users.noreply.github.com>
2022-12-09 11:08:06 +0000

    backstage: add `debug:js` component script
    Support debugging tests by running them within the browser.

--- tools/test-javascript/index.js
+++ tools/test-javascript/index.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 
+import process from 'node:process'
 import execa from 'execa'
 import {globbySync as glob} from 'globby'
 import { dirname, basename, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { cwd } from 'node:process';
 
+const debug = process.argv[2] === '--debug';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const componentName = basename(cwd())
 const javaScriptTestFiles = await glob('./test/**/*.test.js', {onlyFiles: true})
@@ -14,9 +16,12 @@ const webTestRunnerConfigurationPath = join(__dirname, 'web-test-runner.config.m
 
 if (javaScriptTestFiles.length) {
   try {
-    await execa(`web-test-runner`, ['--group', componentName, '--config', webTestRunnerConfigurationPath],{
-      stdio: 'inherit'
-    })
+    await execa(`web-test-runner`, [
+        ...(debug ? ['--open', '--manual'] : []),
+        '--group', componentName, '--config', webTestRunnerConfigurationPath
+      ],
+      {stdio: 'inherit'}
+    )
   } catch {
     process.exitCode = 1;
   }
