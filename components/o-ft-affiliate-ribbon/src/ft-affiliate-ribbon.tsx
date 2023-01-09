34c5cc44abdd0e60712972de837985ab450b2fba
Ben <ben.freshwater@ft.com>
2023-01-09 11:58:32 +0000

    backstage: move ft-affiliate-ribbon component to src directory

---
+++ components/o-ft-affiliate-ribbon/src/ft-affiliate-ribbon.tsx
@@ -0,0 +1,15 @@
+export const FtAffiliateRibbon = () => {
+	return (
+		<div className="o-ft-affiliate-ribbon">
+			<div className="o-ft-affiliate-ribbon__container">
+				<div className="o-ft-affiliate-ribbon__row">
+					<a className="o-ft-affiliate-ribbon__logo" href="https://www.ft.com/" title="The Financial Times"
+					   target="_blank">
+						<span
+							className="o-ft-affiliate-ribbon__visually-hidden">A service from the Financial Times</span>
+					</a>
+				</div>
+			</div>
+		</div>
+	)
+}
--- components/o-ft-affiliate-ribbon/stories/ft-affiliate-ribon.stories.tsx
+++ components/o-ft-affiliate-ribbon/stories/ft-affiliate-ribon.stories.tsx
@@ -3,6 +3,7 @@ import './ft-affiliate-ribbon.scss';
 import {withDesign} from "storybook-addon-designs";
 
 import withHtml from 'origami-storybook-addon-html';
+import {FtAffiliateRibbon} from "../src/ft-affiliate-ribbon";
 
 export default {
 	title: 'Components/o-ft-affiliate-ribbon',
@@ -20,16 +21,3 @@ export const FtAffiliateRibbonStory: ComponentStory<typeof FtAffiliateRibbon> = 
 };
 FtAffiliateRibbonStory.storyName = 'Default FT Affiliate Ribbon';
 
-const FtAffiliateRibbon = () => {
-	return (
-		<div className="o-ft-affiliate-ribbon">
-			<div className="o-ft-affiliate-ribbon__container">
-				<div className="o-ft-affiliate-ribbon__row">
-					<a className="o-ft-affiliate-ribbon__logo" href="https://www.ft.com/" title="The Financial Times" target="_blank">
-						<span className="o-ft-affiliate-ribbon__visually-hidden">A service from the Financial Times</span>
-					</a>
-				</div>
-			</div>
-		</div>
-	)
-}
