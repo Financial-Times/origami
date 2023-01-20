9b1fc8aaa325c1a4e52c35a23468d8bdc59e72c2
ben.freshwater <ben.freshwater@ft.com>
2023-01-20 11:03:48 +0000

    docs: hide children

--- components/o-forms/stories/checkboxes.stories.tsx
+++ components/o-forms/stories/checkboxes.stories.tsx
@@ -6,10 +6,20 @@ import {Checkbox, Checkboxes} from '../src/tsx/o-forms';
 import './forms.scss';
 import javascript from '../main.js';
 
+const hideArg = {
+	table: {
+		disable: true,
+	},
+};
+
+
 export default {
 	title: 'Components/o-forms/checkboxes',
 	component: Checkboxes,
 	decorators: [withDesign, withHtml],
+	argTypes: {
+		children: hideArg
+	}
 } as ComponentMeta<typeof Checkboxes>;
 
 const Template: ComponentStory<typeof Checkboxes> = args => {
