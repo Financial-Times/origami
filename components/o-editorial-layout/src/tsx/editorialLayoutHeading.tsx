3eff4c55349d177ddc37b5045add8418a4ca5661
ben.freshwater <ben.freshwater@ft.com>
2023-01-24 16:35:34 +0000

    feat: introduce editorial layout heading

---
+++ components/o-editorial-layout/src/tsx/editorialLayoutHeading.tsx
@@ -0,0 +1,9 @@
+interface EditorialLayoutHeadingProps {
+	headingLevel: '1' | '2' | '3' | '4' | '5';
+	children: string | JSX.Element | JSX.Element[]
+}
+export const EditorialLayoutHeading = ({headingLevel, children}: EditorialLayoutHeadingProps) => {
+	const Heading: keyof JSX.IntrinsicElements = `h${headingLevel}`;
+	const headingClass = `o-editorial-layout-heading-${headingLevel}`
+	return <Heading className={headingClass}>{children}</Heading>;
+};
