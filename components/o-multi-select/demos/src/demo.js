bb83962d8d39cc76879cb7d1846fcf16ae15b535
Akaki Mikaia <akakimikaia@gmail.com>
2023-01-27 14:01:26 +0000

    backstage: core and progresively enhanced proof of concept

--- components/o-multi-select/demos/src/demo.js
+++ components/o-multi-select/demos/src/demo.js
@@ -1,4 +1,27 @@
-import '../../main.js';
+import MultiSelect from '../../src/js/multi-select.js';
+
+const constructAll = () => {
+	MultiSelect.init(null, {
+		multiSelectOptions: [
+			'Apple',
+			'Banana',
+			'Blueberry',
+			'Boysenberry',
+			'Cherry',
+			'Durian',
+			'Eggplant',
+			'Fig',
+			'Grape',
+			'Guava',
+			'Huckleberry',
+		],
+	});
+	document.removeEventListener('o.DOMContentLoaded', constructAll);
+};
+
+if (typeof document !== 'undefined') {
+	document.addEventListener('o.DOMContentLoaded', constructAll);
+}
 
 document.addEventListener("DOMContentLoaded", function () {
 	document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
--- components/o-multi-select/src/tsx/multi-select.tsx
+++ components/o-multi-select/src/tsx/multi-select.tsx
@@ -1,3 +1,6 @@
-export function MultiSelect() {
-	return (<div className="o-multi-select"data-o-component="o-multi-select"></div>)
+export function MultiSelect({options}: {options: string[]}) {
+		return (
+		<div className="o-multi-select" data-o-component="o-multi-select">
+		</div>
+	);
 }
