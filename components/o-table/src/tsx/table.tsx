87e8275c038222ec48e5485ed88cb3f5d38d0032
ben.freshwater <ben.freshwater@ft.com>
2023-02-23 11:52:10 +0000

    docs: introduce table caption and footnote story

--- components/o-table/src/tsx/table.tsx
+++ components/o-table/src/tsx/table.tsx
@@ -1,5 +1,19 @@
-export const Table = ({children}) => {
-	return <table className="o-table o-table--horizontal-lines" data-o-component="o-table">{children}</table>;
+interface TableProps {
+	children: string | JSX.Element | JSX.Element[];
+	compact?: boolean;
+	horizontalLines?: boolean;
+}
+
+export const Table = ({children, compact, horizontalLines = true}: TableProps) => {
+	const classNames = ['o-table'];
+
+	if(compact) {
+		classNames.push('o-table--compact');
+	}
+	if(horizontalLines) {
+		classNames.push('o-table--horizontal-lines');
+	}
+	return <table className={classNames.join(' ')} data-o-component="o-table">{children}</table>;
 };
 
 export const TableHead = args => {
@@ -43,6 +57,10 @@ export const TableData = (args: TableDataProps) => {
 
 export const TableFootnote = args => {
 	return <TableData className='o-table-footnote' {...args} />
+}
+
+export const TableCaption = ({children}) => {
+	return <caption className='o-table__caption'>{children}</caption>
 }
 
 export const NumericTableData = (args: TableDataProps) => {
--- components/o-table/stories/table.stories.tsx
+++ components/o-table/stories/table.stories.tsx
@@ -5,8 +5,8 @@ import {
 	NumericTableData,
 	NumericTableHeading,
 	Table,
-	TableBody,
-	TableData,
+	TableBody, TableCaption,
+	TableData, TableFoot, TableFootnote,
 	TableHead,
 	TableHeading,
 	TableRow
@@ -18,58 +18,79 @@ export default {
 	decorators: [withDesign, withHtml]
 };
 
-const Template = () => {
-	return <Table>
-		<TableHead className='example'>
-		<TableRow>
-			<TableHeading>Fruit</TableHeading>
-			<TableHeading>Genus</TableHeading>
-			<TableHeading>Characteristic</TableHeading>
-			<NumericTableHeading>
-				Cost&#xA0;(GBP)
-			</NumericTableHeading>
-			<NumericTableHeading>
-				Cost&#xA0;(EUR)
-			</NumericTableHeading>
-		</TableRow>
-		</TableHead>
-		<TableBody>
-		<TableRow>
-			<TableData>Dragonfruit</TableData>
-			<TableData>Stenocereus</TableData>
-			<TableData>Juicy</TableData>
-			<NumericTableData>3</NumericTableData>
-			<NumericTableData>2.72</NumericTableData>
-		</TableRow>
-		<TableRow>
-			<TableData>Durian</TableData>
-			<TableData>Durio</TableData>
-			<TableData>Smelly</TableData>
-			<NumericTableData>1.75</NumericTableData>
-			<NumericTableData>1.33</NumericTableData>
-		</TableRow>
-		<TableRow>
-			<TableData>Naseberry</TableData>
-			<TableData>Manilkara</TableData>
-			<TableData>Chewy</TableData>
-			<NumericTableData>2</NumericTableData>
-			<NumericTableData>1.85</NumericTableData>
-		</TableRow>
-		<TableRow>
-			<TableData>Strawberry</TableData>
-			<TableData>Fragaria</TableData>
-			<TableData>Sweet</TableData>
-			<NumericTableData>1.5</NumericTableData>
-			<NumericTableData>1.69</NumericTableData>
-		</TableRow>
-		<TableRow>
-			<TableData>Apple</TableData>
-			<TableData>Malus</TableData>
-			<TableData>Crunchy</TableData>
-			<NumericTableData>0.5</NumericTableData>
-			<NumericTableData>0.56</NumericTableData>
-		</TableRow>
-		</TableBody>
+const Template = (args) => {
+
+	return <Table {...args}>
+		{baseTableContents}
 	</Table>;
 }
-export const BaseTable = Template.bind({});
+export const BasicTable = Template.bind({});
+
+export const BasicTableWithCaptionAndFootnote = (args) => {
+	return <Table {...args}>
+		<TableCaption>
+			<h2 className="o-typography-heading-level-2">Table Caption</h2>
+		</TableCaption>
+		{baseTableContents}
+		<TableFoot>
+			<TableRow>
+				<TableFootnote>Source: The Origami team&apos;s love of fruit.</TableFootnote>
+			</TableRow>
+		</TableFoot>
+	</Table>
+}
+
+
+const baseTableContents = (
+	<>
+		<TableHead className='example'>
+			<TableRow>
+				<TableHeading>Fruit</TableHeading>
+				<TableHeading>Genus</TableHeading>
+				<TableHeading>Characteristic</TableHeading>
+				<NumericTableHeading>
+					Cost&#xA0;(GBP)
+				</NumericTableHeading>
+				<NumericTableHeading>
+					Cost&#xA0;(EUR)
+				</NumericTableHeading>
+			</TableRow>
+		</TableHead>
+		<TableBody>
+			<TableRow>
+				<TableData>Dragonfruit</TableData>
+				<TableData>Stenocereus</TableData>
+				<TableData>Juicy</TableData>
+				<NumericTableData>3</NumericTableData>
+				<NumericTableData>2.72</NumericTableData>
+			</TableRow>
+			<TableRow>
+				<TableData>Durian</TableData>
+				<TableData>Durio</TableData>
+				<TableData>Smelly</TableData>
+				<NumericTableData>1.75</NumericTableData>
+				<NumericTableData>1.33</NumericTableData>
+			</TableRow>
+			<TableRow>
+				<TableData>Naseberry</TableData>
+				<TableData>Manilkara</TableData>
+				<TableData>Chewy</TableData>
+				<NumericTableData>2</NumericTableData>
+				<NumericTableData>1.85</NumericTableData>
+			</TableRow>
+			<TableRow>
+				<TableData>Strawberry</TableData>
+				<TableData>Fragaria</TableData>
+				<TableData>Sweet</TableData>
+				<NumericTableData>1.5</NumericTableData>
+				<NumericTableData>1.69</NumericTableData>
+			</TableRow>
+			<TableRow>
+				<TableData>Apple</TableData>
+				<TableData>Malus</TableData>
+				<TableData>Crunchy</TableData>
+				<NumericTableData>0.5</NumericTableData>
+				<NumericTableData>0.56</NumericTableData>
+			</TableRow>
+		</TableBody>
+	</>)
