d8ce392e7f17b125dfa50f63652a5c9f31705bb5
ben.freshwater <ben.freshwater@ft.com>
2023-03-09 16:34:26 +0000

    feat: pass ids and labels down to enhanced component

---
+++ components/o-multi-select/src/utils/uniqueId.ts
@@ -0,0 +1,6 @@
+const uidBuilder = (componentName: string) => prefix => {
+	const uid = String(Math.random()).split('.')[1];
+	return `${componentName}-${prefix}${uid}`;
+};
+
+export const uniqueId = uidBuilder('o-multi-select');
