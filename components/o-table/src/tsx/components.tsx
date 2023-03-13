4126ba6406f47b1949a068bb5b35e9445fe161e7
ben.freshwater <ben.freshwater@ft.com>
2023-03-13 15:40:54 +0000

    feat: introduce filter with text input demo

--- components/o-table/src/tsx/components.tsx
+++ components/o-table/src/tsx/components.tsx
@@ -49,16 +49,20 @@ export const NumericTableData = (args: TableDataProps) => {
 }
 
 
-interface SelectFilterProps {
+interface FilterProps {
 	label: string;
 	id: string;
 	filterId: string;
 	filterColumn: string;
+}
+
+interface SelectFilterProps extends FilterProps {
 	children: JSX.Element | JSX.Element[];
+
 }
 
 export const SelectFilter = ({label, id, filterId, filterColumn, children}: SelectFilterProps) => {
-	return <>
+	return (
 		<Select name='Test' title={label} isOptional={false} inlineField={true} isVerticalCenter={true}
 				additionalAttributes={{
 					'data-o-table-filter-id': filterId,
@@ -67,5 +71,20 @@ export const SelectFilter = ({label, id, filterId, filterColumn, children}: Sele
 				}}>
 			{children}
 		</Select>
-	</>
+	)
+}
+export const TextInputFilter = ({id, label, filterId, filterColumn}: FilterProps) => {
+	return (
+		<label className="o-forms-field o-forms-field--inline o--if-js o-forms-field--demo">
+        	<span className="o-forms-title o-forms-title--vertical-center o-forms-title--shrink">
+          		<span className="o-forms-title__main">{label}</span>
+        	</span>
+
+			<span className="o-forms-input o-forms-input--text o-forms-input--invalid">
+				<input type="text" data-o-table-filter-id={filterId} data-o-table-filter-column={filterColumn}
+					   id={id}
+					   className="o-forms__text"/>
+        	</span>
+		</label>
+	)
 }
--- components/o-table/stories/tableWithFilter.stories.tsx
+++ components/o-table/stories/tableWithFilter.stories.tsx
@@ -2,7 +2,7 @@ import {Table} from "../src/tsx/table";
 import {withDesign} from "storybook-addon-designs";
 import withHtml from 'origami-storybook-addon-html';
 import {baseTableContents} from "./baseTableContents";
-import {SelectFilter} from "../src/tsx/components";
+import {SelectFilter, TextInputFilter} from "../src/tsx/components";
 import {useEffect} from "react";
 import javascript from '../main';
 
@@ -20,7 +20,8 @@ export const TableWithSelectFilter = (_args) => {
 		<>
 			<div id='demo-table-caption'>
 				<h2 className="o-typography-heading-level-2">What fruit is the frutiest?</h2>
-				<SelectFilter label='Filter by characteristic:' id='filter-select' filterId='fruit-filter' filterColumn='2'>
+				<SelectFilter label='Filter by characteristic:' id='filter-select' filterId='fruit-filter'
+							  filterColumn='2'>
 					<option value="">All</option>
 					<option value="Juicy">Juicy</option>
 					<option value="Smelly">Smelly</option>
@@ -28,6 +29,27 @@ export const TableWithSelectFilter = (_args) => {
 					<option value="Sweet">Sweet</option>
 					<option value="Crunchy">Crunchy</option>
 				</SelectFilter>
+			</div>
+			<div className='o-table-container'>
+				<Table id='fruit-filter' aria-describedby='demo-table-caption'>
+					{baseTableContents}
+				</Table>
+			</div>
+		</>
+	)
+}
+
+export const TableWithTextInput = (_args) => {
+	useEffect(() => {
+		javascript.init();
+	})
+	return (
+		<>
+			<div id='demo-table-caption'>
+				<h2 className="o-typography-heading-level-2">What fruit is the frutiest?</h2>
+
+				<TextInputFilter label='Filter by characteristic:' id='fruity-filter' filterId='fruit-filter'
+								 filterColumn='2'/>
 			</div>
 			<div className='o-table-container'>
 				<Table id='fruit-filter' aria-describedby='demo-table-caption'>
