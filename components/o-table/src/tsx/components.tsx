d2c16ffc11e310d6b32a9ded3c61945dfb1c7beb
ben.freshwater <ben.freshwater@ft.com>
2023-03-14 16:26:07 +0000

    fix: remove use of className im public table components

--- components/o-table/src/tsx/components.tsx
+++ components/o-table/src/tsx/components.tsx
@@ -15,37 +15,32 @@ export const TableRow = args => {
 
 interface TableHeadingProps {
 	children?: string | JSX.Element | JSX.Element[];
-	className?: string;
+	dataType?: 'text' | 'date' | 'number' | 'percent' | 'currency' | 'numeric';
 }
 
 export const TableHeading = (args: TableHeadingProps) => {
 	return <th scope="col" role="columnheader" {...args}/>
 }
-export const NumericTableHeading = (args: TableHeadingProps) => {
-	return <TableHeading className='o-table__cell--numeric' data-o-table-data-type="numeric"{...args}/>
-}
 
 interface TableDataProps {
 	children?: string | JSX.Element | JSX.Element[];
 	verticallyCenter?: boolean;
-	className?: string;
+	dataType?: 'text' | 'date' | 'number' | 'percent' | 'currency' | 'numeric';
+
 }
 
-export const TableData = ({verticallyCenter, className, ...args}: TableDataProps) => {
+export const TableData = ({verticallyCenter, ...args}: TableDataProps) => {
 	const classNames = [];
 	if (verticallyCenter) {
 		classNames.push('o-table__cell--vertically-center')
 	}
-	return <td className={`${className} {classNames.join(' ')}`}{...args}/>
+	return <td className={`${classNames.join(' ')}`}{...args}/>
 }
 export const TableFootnote = args => {
-	return <TableData className='o-table-footnote' {...args} />
+	return <td className='o-table-footnote' {...args} />
 }
 export const TableCaption = ({children}) => {
 	return <caption className='o-table__caption'>{children}</caption>
-}
-export const NumericTableData = (args: TableDataProps) => {
-	return <TableData className='o-table__cell--numeric' {...args}/>
 }
 
 
