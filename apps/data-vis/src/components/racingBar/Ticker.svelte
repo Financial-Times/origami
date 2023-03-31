944751f7eb8891f141617687dce10e7b09a62c1f
Akaki Mikaia <akakimikaia@gmail.com>
2023-03-31 15:15:19 +0100

    backstage: - attemnt on ticket transitions
    - ignore svelte project from prettier

--- apps/data-vis/src/components/racingBar/Ticker.svelte
+++ apps/data-vis/src/components/racingBar/Ticker.svelte
@@ -1,7 +1,9 @@
 <script>
-	import {slide} from "svelte/transition"
+	// TODO: comeback to animation again
+	// import {fly, slide} from "svelte/transition"
 
 	export let date
+
 	const months = [
 		"Jan",
 		"Feb",
@@ -19,19 +21,35 @@
 	$: dateContrtuct = new Date(date)
 	$: year = dateContrtuct.getFullYear()
 	$: month = months[dateContrtuct.getMonth()]
+
+	// const flyIn = {y: 30, duration: 2000}
+	// const flyOut = {y: -30, duration: 2000}
 </script>
 
 {#if date}
-	<p>
-		<span transition:slide>{month}</span>, <span transition:slide>{year}</span>
-	</p>
+	<div class="wrapper">
+		<p>
+			{month}, {year}
+			<!-- {#key month}<span in:fly={flyIn} out:fly={flyOut}>{month}</span>{/key}
+			{#key year}<span in:fly={flyIn} out:fly={flyOut}>{year}</span>{/key} -->
+		</p>
+	</div>
 {/if}
 
 <style lang="scss">
-	p {
+	.wrapper {
 		@include oTypographyBody();
 		position: absolute;
 		bottom: 20px;
 		right: 20px;
+		p {
+			min-width: 100px;
+			height: 50px;
+			font-size: 42px;
+			margin: 0;
+			// span {
+			// 	position: absolute;
+			// }
+		}
 	}
 </style>
