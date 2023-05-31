d5d1793ac61ea00a0c220b72255e39a668e5fcfb
Lee Moody <lee.moody@ft.com>
2023-05-31 12:36:26 +0100

    backstage: add o-top-story tsx and storybook demos

--- components/o-top-banner/src/tsx/top-banner.tsx
+++ components/o-top-banner/src/tsx/top-banner.tsx
@@ -1,3 +1,36 @@
-export function TopBanner() {
-	return (<div className="o-top-banner"></div>)
+export interface TopBannerLink {
+	copy: string;
+	url: string;
+}
+
+export interface TopBannerProps {
+	heading?: string;
+	content: string;
+	primaryAction: TopBannerLink;
+	theme: 'new-world' | 'professional-inverse';
+}
+
+export function TopBanner({
+	heading = '',
+	content = 'Hello!',
+	primaryAction = {
+		copy: 'OK',
+		url: '#',
+	},
+	theme = 'new-world',
+}: TopBannerProps) {
+	return (
+		<div className={`o-top-banner o-top-banner--${theme}`}>
+			<div className="o-top-banner__container">
+				<div className="o-top-banner__content">
+					<h2 className="o-top-banner__heading">{heading}</h2>
+					<h2 className="o-top-banner__copy">{content}</h2>
+				</div>
+
+				<div className="o-top-banner__actions">
+					<a href={primaryAction.url} className="o-top-banner__button">{primaryAction.copy}</a>
+				</div>
+			</div>
+		</div>
+	)
 }
--- components/o-top-banner/stories/top-banner.stories.tsx
+++ components/o-top-banner/stories/top-banner.stories.tsx
@@ -13,6 +13,25 @@ export default {
 } as ComponentMeta<typeof TopBanner>;
 
 const TopBannerStory = args => <TopBanner {...args} />;
-export const DefaultTopBanner: ComponentStory<typeof TopBanner> = TopBannerStory.bind(
-	{}
-);
+
+export const NewWorldTopBanner: ComponentStory<typeof TopBanner> = TopBannerStory.bind({});
+NewWorldTopBanner.args = {
+	heading: 'Make sense of it all.',
+	content: 'Become an FT subscriber. Pay annually and save 20%',
+	primaryAction:  {
+		copy: 'Subscribe now',
+		url: '#',
+	},
+	theme:'new-world'
+};
+
+export const InverseProfessionalTopBanner: ComponentStory<typeof TopBanner> = TopBannerStory.bind({});
+InverseProfessionalTopBanner.args = {
+	heading: 'You can now access Workspace',
+	content: 'Only available with your FT Professional subscription',
+	primaryAction:  {
+		copy: 'Visit Workspace',
+		url: '#',
+	},
+	theme:'professional-inverse'
+};
