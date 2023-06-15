c8978cbcd4c2d9992109efafa5b974d7666d6157
Akaki Mikaia <akakimikaia@gmail.com>
2023-06-15 18:22:05 +0400

    feat: init astro project for rewriting origami page

---
+++ apps/astro-website/astro.config.mjs
@@ -0,0 +1,26 @@
+import { defineConfig } from 'astro/config';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// https://astro.build/config
+export default defineConfig({
+	vite: {
+    resolve: {
+      alias: {
+        '@': `${path.resolve(__dirname, 'src')}/`
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/styles/global.scss";`
+        }
+      }
+    }
+  }
+});
