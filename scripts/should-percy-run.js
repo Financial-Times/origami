bddb8aa6c0471d35bae1fc656224fb193b5ff2af
ben.freshwater <ben.freshwater@ft.com>
2023-07-18 12:55:33 +0100

    backstage: use async on shouldPercyRun

--- scripts/should-percy-run.js
+++ scripts/should-percy-run.js
@@ -3,11 +3,16 @@
 import {dirname} from "node:path";
 import {fileURLToPath} from "node:url";
 import {shouldPercyRun} from "./lib/should-percy-run.js";
+import core from "@actions/core";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const workspace = "./" + process.env.WORKSPACE
-
-const run = shouldPercyRun(__dirname, workspace);
-console.log(workspace, '\nResult:', run, '/n');
-run ? process.exit(): process.exit(8);
+try {
+	const run = await shouldPercyRun(__dirname, workspace);
+	console.log(workspace, '\nResult:', run, '/n');
+	run ? process.exit(): process.exit(8);
+} catch (error) {
+	console.log(error);
+	core.setFailed(error.message)
+}
 
