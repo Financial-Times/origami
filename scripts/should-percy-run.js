dfcb46982cb0d821c69b43cd1f5dd05e9710d39e
ben.freshwater <ben.freshwater@ft.com>
2023-07-18 12:09:46 +0100

    backstage: determing if percy should run based on bash input

--- scripts/should-percy-run.js
+++ scripts/should-percy-run.js
@@ -3,7 +3,9 @@
 import {dirname} from "node:path";
 import {fileURLToPath} from "node:url";
 import {shouldPercyRun} from "./lib/should-percy-run.js";
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
-shouldPercyRun(__dirname ,component) ? process.exit(): process.exit(8);
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const workspace = "./" + process.env.WORKSPACE
+
+shouldPercyRun(__dirname, workspace) ? process.exit(): process.exit(8);
 
