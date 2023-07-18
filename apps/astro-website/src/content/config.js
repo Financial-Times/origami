72097c23dc39b827955f2e8be3e8c4b6c58aee35
Akaki Mikaia <akakimikaia@gmail.com>
2023-07-18 14:57:19 +0100

    backstage: move posts to content instead of pages

---
+++ apps/astro-website/src/content/config.js
@@ -0,0 +1,16 @@
+import {z, defineCollection} from "astro:content"
+
+const postsCollection = defineCollection({
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		publishDate: z.date(),
+		author: z.string(),
+		tags: z.array(z.string()).optional(),
+	}),
+})
+
+export const collections = {
+	posts: postsCollection,
+}
--- apps/astro-website/src/helpers/utils.js
+++ apps/astro-website/src/helpers/utils.js
@@ -3,3 +3,26 @@ export function capitaliseFirstLetter(string) {
 	const remainingLetters = string.substring(1)
 	return firstLetter + remainingLetters
 }
+
+
+export function formatDateString(date) {
+	const months = [
+		"January",
+		"February",
+		"march",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December",
+	]
+	const newDate = new Date(date)
+	const year = newDate.getFullYear()
+	const month = months[newDate.getMonth()]
+	const day = newDate.getDate()
+	return `${day} ${month} ${year}`
+}
