f33a79d232e008449dac4586e9c709ed03cb0a95
Akaki Mikaia <akakimikaia@gmail.com>
2023-08-23 14:34:54 +0100

    chore: generalise the config builder funciton

--- apps/dictionary/scripts/build-config/build.js
+++ apps/dictionary/scripts/build-config/build.js
@@ -3,6 +3,7 @@ const {registerTransforms} = require("@tokens-studio/sd-transforms")
 const {brandClasses} = require("../formatters/css/brand-classes")
 const {transformSVG} = require("../transforms/transformSVG")
 const themes = require("../../tokens/$themes.json")
+const {ConfigBuilder} = require("./configBuilder")
 
 StyleDictionaryPackage.registerFormat({
 	name: "css/brand/classes",
@@ -23,37 +24,86 @@ StyleDictionaryPackage.registerFilter({
 		token.original.value !== "{DO-NOT-USE}" && token.path[0] !== "DO-NOT-USE",
 })
 
-const getStyleDictionaryBrandConfig = brand => ({
-	source: brand.sources,
-	platforms: {
-		css: {
-			transformGroup: "css",
-			transforms: [
-				"ts/descriptionToComment",
-				"ts/typography/css/shorthand",
-				"ts/border/css/shorthand",
-				"ts/shadow/css/shorthand",
-				"ts/color/css/hexrgba",
-				"ts/color/modifiers",
-				"name/cti/kebab",
-			],
-			buildPath: `build/css/usecase/${brand.name}/`,
-			files: [
-				{
-					filter: "filter/doNotUse",
-					destination: "_variables.css",
-					format: "css/brand/classes",
-					options: {
-						outputReferences: true,
-						classNames: [`o-brand-${brand.name}`],
-					},
+class ConfigBuilder {
+	constructor() {
+		this.config = {
+			platforms: {
+				css: {
+					transformGroup: "css",
 				},
-			],
-		},
-	},
-})
-const getBrands = () => {
-	return themes.map(theme => {
+			},
+		}
+	}
+
+	/**
+	 * set source files
+	 * @param {string[]} sources - array of source files
+	 *  */
+	setSources(sources) {
+		this.config.source = sources
+		return this
+	}
+
+	/**
+	 * set transforms
+	 * @param {string[]} transforms - array of token transformers
+	 * */
+	setTransforms(transforms) {
+		this.config.platforms.css.transforms = transforms
+		return this
+	}
+
+	/**
+	 * set build path
+	 * @param {string} buildPath - path to build folder for tokens
+	 * */
+
+	setBuildPath(buildPath) {
+		this.config.platforms.css.buildPath = buildPath
+		return this
+	}
+
+	/**
+	 * set files config
+	 * @param {object[]} config - array of file configs
+	 * @param {string} [config[].filter] - filter name for tokens
+	 * @param {string} config[].destination - destination file name
+	 * @param {string} config[].format - format for token file
+	 * @param {object} config[].options - options for token file
+	 * @param {boolean} config[].options.outputReferences - output references for token file
+	 * @param {string[]} [config[].options.classNames] - class names for token file
+	 * */
+	setFilesConfig(config) {
+		this.config.platforms.css.files = config
+		return this
+	}
+
+	// check if config has source, transforms, buildPath and files set in config object, otherwise throw error with missing properties
+	validateConfig() {
+		if (!this.config.source) {
+			throw new Error("Source not set in config")
+		}
+		if (!this.config.platforms.css.transforms) {
+			throw new Error("Transforms not set in config")
+		}
+		if (!this.config.platforms.css.buildPath) {
+			throw new Error("Build path not set in config")
+		}
+		if (!this.config.platforms.css.files) {
+			throw new Error("Files not set in config")
+		}
+	}
+
+	// build dictionary
+	buildDictionary() {
+		this.validateConfig()
+		const StyleDictionary = StyleDictionaryPackage.extend(this.config)
+		StyleDictionary.buildAllPlatforms(this.config)
+	}
+}
+
+function buildBrandTokens() {
+	const brands = themes.map(theme => {
 		const brandName =
 			theme.group != theme.name ? `${theme.group}/${theme.name}` : theme.group
 		return {
@@ -63,41 +113,60 @@ const getBrands = () => {
 			),
 		}
 	})
-}
 
-;(async () => {
-	const brands = getBrands()
-	registerTransforms(StyleDictionaryPackage)
+	const brandTransforms = [
+		"ts/descriptionToComment",
+		"ts/typography/css/shorthand",
+		"ts/border/css/shorthand",
+		"ts/shadow/css/shorthand",
+		"ts/color/css/hexrgba",
+		"ts/color/modifiers",
+		"name/cti/kebab",
+	]
 	brands.forEach(brand => {
-		const StyleDictionary = StyleDictionaryPackage.extend(
-			getStyleDictionaryBrandConfig(brand)
-		)
-		StyleDictionary.buildPlatform("css")
+		const filesConfig = [
+			{
+				filter: "filter/doNotUse",
+				destination: "_variables.css",
+				format: "css/brand/classes",
+				options: {
+					outputReferences: true,
+					classNames: [`o-brand-${brand.name}`],
+				},
+			},
+		]
+		new ConfigBuilder()
+			.setSources(brand.sources)
+			.setTransforms(brandTransforms)
+			.setBuildPath(`build/css/usecase/${brand.name}/`)
+			.setFilesConfig(filesConfig)
+			.buildDictionary()
 	})
-
-	const StyleDictionaryIcons = StyleDictionaryPackage.extend({
-		source: ["tokens/icons/icons.json"],
-		platforms: {
-			css: {
-				transformGroup: "css",
-				transforms: [
-					"ts/descriptionToComment",
-					"name/cti/kebab",
-					"value/transformSVG",
-				],
-				buildPath: `build/css/icons/`,
-				files: [
-					{
-						destination: "_variables.css",
-						format: "css/variables",
-						options: {
-							outputReferences: true,
-						},
-					},
-				],
+}
+function buildIconTokens() {
+	const iconTransformers = [
+		"ts/descriptionToComment",
+		"name/cti/kebab",
+		"value/transformSVG",
+	]
+	const iconsFileConfig = [
+		{
+			destination: "_variables.css",
+			format: "css/variables",
+			options: {
+				outputReferences: true,
 			},
 		},
-	})
+	]
 
-	StyleDictionaryIcons.buildPlatform("css")
-})()
+	new ConfigBuilder()
+		.setSources(["tokens/icons/icons.json"])
+		.setTransforms(iconTransformers)
+		.setBuildPath("build/css/icons/")
+		.setFilesConfig(iconsFileConfig)
+		.buildDictionary()
+}
+
+registerTransforms(StyleDictionaryPackage)
+buildBrandTokens()
+buildIconTokens()
