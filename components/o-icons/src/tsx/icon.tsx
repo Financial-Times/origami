009831177543a69bd01866807fc4d5b41377f63c
ben.freshwater <ben.freshwater@ft.com>
2023-08-31 11:26:36 +0100

    chore: see output from component

--- components/o-icons/src/tsx/icon.tsx
+++ components/o-icons/src/tsx/icon.tsx
@@ -3,7 +3,7 @@ import {IconName} from './data';
 export interface IconProps {
 	icon: IconName;
 }
-
+// change
 export function Icon({icon}: IconProps): JSX.Element {
 	return <span className={`o-icons-icon o-icons-icon--${icon}`}></span>;
 }
