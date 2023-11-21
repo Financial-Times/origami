0b8e5d62ae0cc134708718fdfe39c4b4fc1bbd50
Lee Moody <lee.moody@ft.com>
2023-11-21 13:04:07 +0000

    backstage: Origami For Everyone website proof of concept.
    A very messy proof of concept, which we're going to use as a base for a new beta site origami-for-everyone.ft.com.
    
    - Abuses the mono-repo to grab raw token information directly, as pushed from Token Studio. We should instead build a json output to a package with selected token meta data retained for tools to use.
    - Branch switcher is a copy-pasta mess, but mostly works.
    - Uses the internationalisation feature to translate brands instead of languages. The search feature relies on the lang attribute to correctly search only content for the selected language brand so we set lang="en-GB-x-[abbreviated-brand]". I believe we can get away with this because [-x- is reserved for private use](https://datatracker.ietf.org/doc/html/rfc4646#section-2.2.1) :sweat_smile_with_cowboy_hat:

---
+++ apps/for-everyone-website/astro.config.mjs
@@ -0,0 +1,54 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import react from '@astrojs/react';
+
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		react(),
+		starlight({
+			title: 'Origami For Everyone',
+			locales: {
+				root: {
+				  label: 'core',
+				  lang: 'en-GB-x-core', // lang is required for root locales
+				},
+				'professional': {
+				  label: 'professional',
+				  lang: 'en-GB-x-prof',
+				},
+			},
+			components: {
+				Hero: './src/components/Hero.astro',
+				ThemeProvider: './src/components/ThemeProvider.astro',
+				LanguageSelect: './src/components/LanguageSelect.astro',
+				ThemeSelect: './src/components/ThemeSelect.astro',
+			},
+			customCss: [
+				'./src/styles/custom.css',
+			],
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: 'About',
+					autogenerate: { directory: 'about' },
+				},
+				{
+					label: 'Foundations',
+					autogenerate: { directory: 'guides' },
+				},
+				{
+					label: 'Components',
+					autogenerate: { directory: 'components' },
+				},
+				{
+					label: 'Patterns',
+					autogenerate: { directory: 'patterns' },
+				},
+			],
+		}),
+	],
+});
