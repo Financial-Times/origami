140747588f13a40813cb60090071f8aa64168bc2
Lee Moody <lee.moody@ft.com>
2024-01-04 15:06:56 +0000

    chore: o3-tooling-token, export SV token data
    we should map this for all brands, as with o3-web-token

---
+++ libraries/o3-tooling-token/build/sustainable-views/_variables.js
@@ -0,0 +1,657 @@
+export default {
+	"o3-spacing-5xs": {
+		"shortName": "5xs",
+		"value": "0.25rem",
+		"type": "spacing",
+		"path": [
+				"o3",
+				"spacing",
+				"5xs"
+		],
+		"css": "--o3-spacing-5xs",
+		"figma": "o3/spacing/5xs"
+},
+	"o3-spacing-4xs": {
+		"shortName": "4xs",
+		"value": "0.5rem",
+		"type": "spacing",
+		"path": [
+				"o3",
+				"spacing",
+				"4xs"
+		],
+		"css": "--o3-spacing-4xs",
+		"figma": "o3/spacing/4xs"
+},
+	"o3-spacing-3xs": {
+		"shortName": "3xs",
+		"value": "0.75rem",
+		"type": "spacing",
+		"path": [
+				"o3",
+				"spacing",
+				"3xs"
+		],
+		"css": "--o3-spacing-3xs",
+		"figma": "o3/spacing/3xs"
+},
+	"o3-spacing-2xs": {
+		"shortName": "2xs",
+		"value": "1rem",
+		"type": "spacing",
+		"path": [
+				"o3",
+				"spacing",
+				"2xs"
+		],
+		"css": "--o3-spacing-2xs",
+		"figma": "o3/spacing/2xs"
+},
+	"o3-spacing-xs": {
+		"shortName": "xs",
+		"value": "1.25rem",
+		"type": "spacing",
+		"path": [
+				"o3",
+				"spacing",
+				"xs"
+		],
+		"css": "--o3-spacing-xs",
+		"figma": "o3/spacing/xs"
+},
+	"o3-spacing-s": {
+		"shortName": "s",
+		"value": "1.5rem",
+		"type": "spacing",
+		"path": [
+				"o3",
+				"spacing",
+				"s"
+		],
+		"css": "--o3-spacing-s",
+		"figma": "o3/spacing/s"
+},
+	"o3-spacing-m": {
+		"shortName": "m",
+		"value": "2rem",
+		"type": "spacing",
+		"path": [
+				"o3",
+				"spacing",
+				"m"
+		],
+		"css": "--o3-spacing-m",
+		"figma": "o3/spacing/m"
+},
+	"o3-spacing-l": {
+		"shortName": "l",
+		"value": "2.5rem",
+		"type": "spacing",
+		"path": [
+				"o3",
+				"spacing",
+				"l"
+		],
+		"css": "--o3-spacing-l",
+		"figma": "o3/spacing/l"
+},
+	"o3-spacing-xl": {
+		"shortName": "xl",
+		"value": "3rem",
+		"type": "spacing",
+		"path": [
+				"o3",
+				"spacing",
+				"xl"
+		],
+		"css": "--o3-spacing-xl",
+		"figma": "o3/spacing/xl"
+},
+	"o3-spacing-2xl": {
+		"shortName": "2xl",
+		"value": "4rem",
+		"type": "spacing",
+		"path": [
+				"o3",
+				"spacing",
+				"2xl"
+		],
+		"css": "--o3-spacing-2xl",
+		"figma": "o3/spacing/2xl"
+},
+	"o3-spacing-3xl": {
+		"shortName": "3xl",
+		"value": "5rem",
+		"type": "spacing",
+		"path": [
+				"o3",
+				"spacing",
+				"3xl"
+		],
+		"css": "--o3-spacing-3xl",
+		"figma": "o3/spacing/3xl"
+},
+	"o3-spacing-4xl": {
+		"shortName": "4xl",
+		"value": "6rem",
+		"type": "spacing",
+		"path": [
+				"o3",
+				"spacing",
+				"4xl"
+		],
+		"css": "--o3-spacing-4xl",
+		"figma": "o3/spacing/4xl"
+},
+	"o3-font-family-metric": {
+		"shortName": "metric",
+		"value": "1",
+		"type": "fontFamilies",
+		"path": [
+				"o3",
+				"font",
+				"family",
+				"metric"
+		],
+		"css": "--o3-font-family-metric",
+		"figma": "o3/font/family/metric"
+},
+	"o3-font-family-financier-display": {
+		"shortName": "financier-display",
+		"value": "2",
+		"type": "fontFamilies",
+		"path": [
+				"o3",
+				"font",
+				"family",
+				"financier-display"
+		],
+		"css": "--o3-font-family-financier-display",
+		"figma": "o3/font/family/financier-display"
+},
+	"o3-font-weight-light": {
+		"shortName": "light",
+		"value": "Light",
+		"type": "fontWeights",
+		"path": [
+				"o3",
+				"font",
+				"weight",
+				"light"
+		],
+		"css": "--o3-font-weight-light",
+		"figma": "o3/font/weight/light"
+},
+	"o3-font-weight-regular": {
+		"shortName": "regular",
+		"value": "Regular",
+		"type": "fontWeights",
+		"path": [
+				"o3",
+				"font",
+				"weight",
+				"regular"
+		],
+		"css": "--o3-font-weight-regular",
+		"figma": "o3/font/weight/regular"
+},
+	"o3-font-weight-medium": {
+		"shortName": "medium",
+		"value": "Medium",
+		"type": "fontWeights",
+		"path": [
+				"o3",
+				"font",
+				"weight",
+				"medium"
+		],
+		"css": "--o3-font-weight-medium",
+		"figma": "o3/font/weight/medium"
+},
+	"o3-font-weight-semibold": {
+		"shortName": "semibold",
+		"value": "Semibold",
+		"type": "fontWeights",
+		"path": [
+				"o3",
+				"font",
+				"weight",
+				"semibold"
+		],
+		"css": "--o3-font-weight-semibold",
+		"figma": "o3/font/weight/semibold"
+},
+	"o3-font-weight-bold": {
+		"shortName": "bold",
+		"value": "Bold",
+		"type": "fontWeights",
+		"path": [
+				"o3",
+				"font",
+				"weight",
+				"bold"
+		],
+		"css": "--o3-font-weight-bold",
+		"figma": "o3/font/weight/bold"
+},
+	"o3-font-lineheight-0": {
+		"shortName": "0",
+		"value": "1.25rem",
+		"type": "lineHeights",
+		"path": [
+				"o3",
+				"font",
+				"lineheight",
+				"0"
+		],
+		"css": "--o3-font-lineheight-0",
+		"figma": "o3/font/lineheight/0"
+},
+	"o3-font-lineheight-1": {
+		"shortName": "1",
+		"value": "1.25rem",
+		"type": "lineHeights",
+		"path": [
+				"o3",
+				"font",
+				"lineheight",
+				"1"
+		],
+		"css": "--o3-font-lineheight-1",
+		"figma": "o3/font/lineheight/1"
+},
+	"o3-font-lineheight-2": {
+		"shortName": "2",
+		"value": "1.5rem",
+		"type": "lineHeights",
+		"path": [
+				"o3",
+				"font",
+				"lineheight",
+				"2"
+		],
+		"css": "--o3-font-lineheight-2",
+		"figma": "o3/font/lineheight/2"
+},
+	"o3-font-lineheight-3": {
+		"shortName": "3",
+		"value": "1.75rem",
+		"type": "lineHeights",
+		"path": [
+				"o3",
+				"font",
+				"lineheight",
+				"3"
+		],
+		"css": "--o3-font-lineheight-3",
+		"figma": "o3/font/lineheight/3"
+},
+	"o3-font-lineheight-4": {
+		"shortName": "4",
+		"value": "2rem",
+		"type": "lineHeights",
+		"path": [
+				"o3",
+				"font",
+				"lineheight",
+				"4"
+		],
+		"css": "--o3-font-lineheight-4",
+		"figma": "o3/font/lineheight/4"
+},
+	"o3-font-lineheight-5": {
+		"shortName": "5",
+		"value": "2rem",
+		"type": "lineHeights",
+		"path": [
+				"o3",
+				"font",
+				"lineheight",
+				"5"
+		],
+		"css": "--o3-font-lineheight-5",
+		"figma": "o3/font/lineheight/5"
+},
+	"o3-font-lineheight-6": {
+		"shortName": "6",
+		"value": "2.5rem",
+		"type": "lineHeights",
+		"path": [
+				"o3",
+				"font",
+				"lineheight",
+				"6"
+		],
+		"css": "--o3-font-lineheight-6",
+		"figma": "o3/font/lineheight/6"
+},
+	"o3-font-lineheight-7": {
+		"shortName": "7",
+		"value": "3rem",
+		"type": "lineHeights",
+		"path": [
+				"o3",
+				"font",
+				"lineheight",
+				"7"
+		],
+		"css": "--o3-font-lineheight-7",
+		"figma": "o3/font/lineheight/7"
+},
+	"o3-font-lineheight-8": {
+		"shortName": "8",
+		"value": "3.5rem",
+		"type": "lineHeights",
+		"path": [
+				"o3",
+				"font",
+				"lineheight",
+				"8"
+		],
+		"css": "--o3-font-lineheight-8",
+		"figma": "o3/font/lineheight/8"
+},
+	"o3-font-lineheight-9": {
+		"shortName": "9",
+		"value": "4.5rem",
+		"type": "lineHeights",
+		"path": [
+				"o3",
+				"font",
+				"lineheight",
+				"9"
+		],
+		"css": "--o3-font-lineheight-9",
+		"figma": "o3/font/lineheight/9"
+},
+	"o3-font-lineheight-10": {
+		"shortName": "10",
+		"value": "5.25rem",
+		"type": "lineHeights",
+		"path": [
+				"o3",
+				"font",
+				"lineheight",
+				"10"
+		],
+		"css": "--o3-font-lineheight-10",
+		"figma": "o3/font/lineheight/10"
+},
+	"o3-font-lineheight-negative-2": {
+		"shortName": "negative-2",
+		"value": "1rem",
+		"type": "lineHeights",
+		"path": [
+				"o3",
+				"font",
+				"lineheight",
+				"negative-2"
+		],
+		"css": "--o3-font-lineheight-negative-2",
+		"figma": "o3/font/lineheight/negative-2"
+},
+	"o3-font-lineheight-negative-1": {
+		"shortName": "negative-1",
+		"value": "1rem",
+		"type": "lineHeights",
+		"path": [
+				"o3",
+				"font",
+				"lineheight",
+				"negative-1"
+		],
+		"css": "--o3-font-lineheight-negative-1",
+		"figma": "o3/font/lineheight/negative-1"
+},
+	"o3-font-size-0": {
+		"shortName": "0",
+		"value": "1rem",
+		"type": "fontSizes",
+		"path": [
+				"o3",
+				"font",
+				"size",
+				"0"
+		],
+		"css": "--o3-font-size-0",
+		"figma": "o3/font/size/0"
+},
+	"o3-font-size-1": {
+		"shortName": "1",
+		"value": "1.125rem",
+		"type": "fontSizes",
+		"path": [
+				"o3",
+				"font",
+				"size",
+				"1"
+		],
+		"css": "--o3-font-size-1",
+		"figma": "o3/font/size/1"
+},
+	"o3-font-size-2": {
+		"shortName": "2",
+		"value": "1.25rem",
+		"type": "fontSizes",
+		"path": [
+				"o3",
+				"font",
+				"size",
+				"2"
+		],
+		"css": "--o3-font-size-2",
+		"figma": "o3/font/size/2"
+},
+	"o3-font-size-3": {
+		"shortName": "3",
+		"value": "1.5rem",
+		"type": "fontSizes",
+		"path": [
+				"o3",
+				"font",
+				"size",
+				"3"
+		],
+		"css": "--o3-font-size-3",
+		"figma": "o3/font/size/3"
+},
+	"o3-font-size-4": {
+		"shortName": "4",
+		"value": "1.75rem",
+		"type": "fontSizes",
+		"path": [
+				"o3",
+				"font",
+				"size",
+				"4"
+		],
+		"css": "--o3-font-size-4",
+		"figma": "o3/font/size/4"
+},
+	"o3-font-size-5": {
+		"shortName": "5",
+		"value": "2rem",
+		"type": "fontSizes",
+		"path": [
+				"o3",
+				"font",
+				"size",
+				"5"
+		],
+		"css": "--o3-font-size-5",
+		"figma": "o3/font/size/5"
+},
+	"o3-font-size-6": {
+		"shortName": "6",
+		"value": "2.5rem",
+		"type": "fontSizes",
+		"path": [
+				"o3",
+				"font",
+				"size",
+				"6"
+		],
+		"css": "--o3-font-size-6",
+		"figma": "o3/font/size/6"
+},
+	"o3-font-size-7": {
+		"shortName": "7",
+		"value": "3rem",
+		"type": "fontSizes",
+		"path": [
+				"o3",
+				"font",
+				"size",
+				"7"
+		],
+		"css": "--o3-font-size-7",
+		"figma": "o3/font/size/7"
+},
+	"o3-font-size-8": {
+		"shortName": "8",
+		"value": "3.5rem",
+		"type": "fontSizes",
+		"path": [
+				"o3",
+				"font",
+				"size",
+				"8"
+		],
+		"css": "--o3-font-size-8",
+		"figma": "o3/font/size/8"
+},
+	"o3-font-size-9": {
+		"shortName": "9",
+		"value": "4.5rem",
+		"type": "fontSizes",
+		"path": [
+				"o3",
+				"font",
+				"size",
+				"9"
+		],
+		"css": "--o3-font-size-9",
+		"figma": "o3/font/size/9"
+},
+	"o3-font-size-10": {
+		"shortName": "10",
+		"value": "5.25rem",
+		"type": "fontSizes",
+		"path": [
+				"o3",
+				"font",
+				"size",
+				"10"
+		],
+		"css": "--o3-font-size-10",
+		"figma": "o3/font/size/10"
+},
+	"o3-font-size-negative-2": {
+		"shortName": "negative-2",
+		"value": "0.75rem",
+		"type": "fontSizes",
+		"path": [
+				"o3",
+				"font",
+				"size",
+				"negative-2"
+		],
+		"css": "--o3-font-size-negative-2",
+		"figma": "o3/font/size/negative-2"
+},
+	"o3-font-size-negative-1": {
+		"shortName": "negative-1",
+		"value": "0.875rem",
+		"type": "fontSizes",
+		"path": [
+				"o3",
+				"font",
+				"size",
+				"negative-1"
+		],
+		"css": "--o3-font-size-negative-1",
+		"figma": "o3/font/size/negative-1"
+},
+	"o3-color-palette-teal": {
+		"shortName": "teal",
+		"value": "#0d7680ff",
+		"type": "color",
+		"description": "",
+		"path": [
+				"o3",
+				"color",
+				"palette",
+				"teal"
+		],
+		"css": "--o3-color-palette-teal",
+		"figma": "o3/color/palette/teal"
+},
+	"o3-color-palette-oceanwave": {
+		"shortName": "oceanwave",
+		"value": "#C9F5EA",
+		"type": "color",
+		"path": [
+				"o3",
+				"color",
+				"palette",
+				"oceanwave"
+		],
+		"css": "--o3-color-palette-oceanwave",
+		"figma": "o3/color/palette/oceanwave"
+},
+	"o3-color-palette-slate": {
+		"shortName": "slate",
+		"value": "#262A33",
+		"type": "color",
+		"path": [
+				"o3",
+				"color",
+				"palette",
+				"slate"
+		],
+		"css": "--o3-color-palette-slate",
+		"figma": "o3/color/palette/slate"
+},
+	"o3-color-palette-white": {
+		"shortName": "white",
+		"value": "#ffffffff",
+		"type": "color",
+		"description": "",
+		"path": [
+				"o3",
+				"color",
+				"palette",
+				"white"
+		],
+		"css": "--o3-color-palette-white",
+		"figma": "o3/color/palette/white"
+},
+	"o3-color-palette-black": {
+		"shortName": "black",
+		"value": "#000000ff",
+		"type": "color",
+		"description": "",
+		"path": [
+				"o3",
+				"color",
+				"palette",
+				"black"
+		],
+		"css": "--o3-color-palette-black",
+		"figma": "o3/color/palette/black"
+},
+	"o3-color-palette-claret": {
+		"shortName": "claret",
+		"value": "#990f3dff",
+		"type": "color",
+		"description": "",
+		"path": [
+				"o3",
+				"color",
+				"palette",
+				"claret"
+		],
+		"css": "--o3-color-palette-claret",
+		"figma": "o3/color/palette/claret"
+}
+};
