007fa5ba4f6bd1f1774c39374f591547986e1d46
Rhys Evans <rhys.evans@ft.com>
2024-01-10 17:07:18 +0000

    chore: fix o4e site analytics

--- apps/for-everyone-website/astro.config.mjs
+++ apps/for-everyone-website/astro.config.mjs
@@ -41,51 +41,20 @@ export default defineConfig({
 				github: 'https://github.com/financial-times/origami/',
 			},
 			head: [
-		    // Example: add Fathom analytics script tag.
 		    {
 		      tag: 'script',
 		      attrs: {
 		        src: 'https://www.ft.com/__origami/service/build/v3/bundles/js?components=o-tracking@^4.5.0&system_code=origami-website',
 		        defer: true,
-		        onload: "initTracking()"
 		      },
 		    },
 		    {
 		      tag: 'script',
-		      content: `
-const oTracking = window.Origami['o-tracking'];
-
-function getCookie(name) {
-  const value = \`; \${document.cookie}\`;
-  const parts = value.split(\`; \${name}=\`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
-}
-const oktaId = getCookie('oktaId');
-if ((window.location.href.startsWith('https://origami.ft.com') || window.location.href.startsWith('https://origami-for-everyone.ft.com')) && oktaId) {
-	oTracking.init({
-		context: {
-			product: 'engineering-enablement',
-			app: 'origami-website',
-		},
-		user: {
-			okta_id: oktaId,
-			is_staff: true,
-		},
-	});
-	// Send a page view tracking event.
-	oTracking.page();
-	// Track click events on the page.
-	oTracking.click.init();
-	// Track when elements with the attribute \`data-o-tracking-view\` are visible to the user.
-	oTracking.view.init();
-	// Tell o-tracking to listen for custom events named \`oTracking.event\`.
-	oTracking.event.init();
-} else {
-	console.log('Skipping analytics - not a production site');
-}
-
-		      `
-		    },
+		      attrs: {
+		        src: '/js/tracking.js',
+		        defer: true,
+		      }
+		    }
 		  ],
 			sidebar: [
 				{
