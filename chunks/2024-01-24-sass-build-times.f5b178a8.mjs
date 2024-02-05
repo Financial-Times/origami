5463ee1d0c16bbbc0d10d3e4b6e5af3c96200fc8
notlee <notlee@users.noreply.github.com>
2024-02-05 09:57:13 +0000

    Deploying to origami.ft.com from @ Financial-Times/origami@49888173d01c12e96a29a03b8488858854e27491 🚀

--- chunks/2024-01-24-sass-build-times.f5b178a8.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./2024-01-24-sass-build-times.18f2aa3f.mjs');
-					}
-					const collectedLinks = "@@ASTRO-LINKS@@";
-					const collectedStyles = "@@ASTRO-STYLES@@";
-					const collectedScripts = "@@ASTRO-SCRIPTS@@";
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
