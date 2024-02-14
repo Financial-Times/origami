7091b0bd34dd4719f37a776a6065c52a122fa678
Akaki Mikaia <akakimikaia@gmail.com>
2024-02-14 15:26:02 +0000

    backstage: setup basic build for o3-tooltip

--- components/o3-tooltip/main.js
+++ components/o3-tooltip/main.js
@@ -1,12 +1,1 @@
-import Tooltip from './src/js/tooltip.js';
-
-const constructAll = function() {
-	Tooltip.init();
-	document.removeEventListener('o.DOMContentLoaded', constructAll);
-};
-
-if (typeof document !== 'undefined') {
-	document.addEventListener('o.DOMContentLoaded', constructAll);
-}
-
-export default Tooltip;
+import './src/ts/index.js'
