66d28837a1f61552826e351ef6abd35f1dbdbf03
ben.freshwater <ben.freshwater@ft.com>
2024-02-16 10:53:04 +0000

    backstage: skip g-audio release
    The commit contained changes for a lot of projects and tried to release a lot of unrelated stuff. Skipping.

--- scripts/regenerate-ci-config-files.js
+++ scripts/regenerate-ci-config-files.js
@@ -1,36 +1,36 @@
 #!/usr/bin/env node
-import * as workspaces from "./lib/workspaces.js"
-import {readPackage} from "read-pkg"
-import {readFile, writeFile} from "fs/promises"
-import Mustache from "mustache"
-import {basename} from "path"
-import del from "del"
+import * as workspaces from './lib/workspaces.js';
+import {readPackage} from 'read-pkg';
+import {readFile, writeFile} from 'fs/promises';
+import Mustache from 'mustache';
+import {basename} from 'path';
+import del from 'del';
 
-let testTemplate = await readFile("templates/test-workflow.yml", "utf-8")
-let percyTemplate = await readFile("templates/percy-workflow.yml", "utf-8")
-let labelerTemplate = await readFile("templates/labeler.yml", "utf-8")
+let testTemplate = await readFile('templates/test-workflow.yml', 'utf-8');
+let percyTemplate = await readFile('templates/percy-workflow.yml', 'utf-8');
+let labelerTemplate = await readFile('templates/labeler.yml', 'utf-8');
 
-let workspacePaths = await workspaces.paths()
+let workspacePaths = await workspaces.paths();
 
 let labelerFile = Mustache.render(labelerTemplate, {
 	workspaces: workspacePaths.map(path => {
 		return {
 			name: basename(path),
 			path,
-		}
+		};
 	}),
-})
+});
 
-await writeFile(`.github/labeler.yml`, labelerFile)
+await writeFile(`.github/labeler.yml`, labelerFile);
 
-await del([".github/workflows/percy-*.yml", ".github/workflows/test-*.yml"])
+await del(['.github/workflows/percy-*.yml', '.github/workflows/test-*.yml']);
 
-let dotReleasePleaseManifest = {}
+let dotReleasePleaseManifest = {};
 let releasePleaseConfig = {
-	"bootstrap-sha": "4d52a27b6c08755d6c527723c2595ade3e024898",
-	"last-release-sha": "22303811874f601504a56ea79520c06cf1212336",
+	'bootstrap-sha': '4d52a27b6c08755d6c527723c2595ade3e024898',
+	'last-release-sha': '1b53b3b1e49955108622b839cfad0fa7d24c2a7b',
 	packages: {},
-}
+};
 
 const percyProjects = [];
 const allProjects = [];
@@ -38,42 +38,39 @@ const allProjects = [];
 for (let workspace of workspacePaths) {
 	const projectConfig = {
 		workspace: workspace,
-		percyTokenName: workspace.replace(/[/-]/g, "_").toUpperCase(),
-		workspaceFilename: workspace.replaceAll("/", "-"),
+		percyTokenName: workspace.replace(/[/-]/g, '_').toUpperCase(),
+		workspaceFilename: workspace.replaceAll('/', '-'),
 	};
 
 	allProjects.push(projectConfig);
 
-	let pkg = await readPackage({cwd: workspace})
+	let pkg = await readPackage({cwd: workspace});
 	if (pkg.percy === true) {
 		percyProjects.push(projectConfig);
 	}
 
 	if (pkg.private !== true) {
-		dotReleasePleaseManifest[workspace] = pkg.version
-		releasePleaseConfig.packages[workspace] = {}
+		dotReleasePleaseManifest[workspace] = pkg.version;
+		releasePleaseConfig.packages[workspace] = {};
 	}
 }
 
 let testFile = Mustache.render(testTemplate, {
-	projects: allProjects
-})
+	projects: allProjects,
+});
 
-await writeFile(`.github/workflows/test.yml`, testFile)
+await writeFile(`.github/workflows/test.yml`, testFile);
 
-let percyFile = Mustache.render(percyTemplate,  {
-	projects: percyProjects
-})
-await writeFile(
-	`.github/workflows/percy.yml`,
-	percyFile
-)
+let percyFile = Mustache.render(percyTemplate, {
+	projects: percyProjects,
+});
+await writeFile(`.github/workflows/percy.yml`, percyFile);
 
 await writeFile(
-	".release-please-manifest.json",
+	'.release-please-manifest.json',
 	JSON.stringify(dotReleasePleaseManifest)
-)
+);
 await writeFile(
-	"release-please-config.json",
-	JSON.stringify(releasePleaseConfig, null, "\t") + "\n"
-)
+	'release-please-config.json',
+	JSON.stringify(releasePleaseConfig, null, '\t') + '\n'
+);
