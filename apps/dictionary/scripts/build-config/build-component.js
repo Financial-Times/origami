cab9dd228c8d7c9d172ed67ae7782ca3facf65d8
ben.freshwater <ben.freshwater@ft.com>
2024-03-11 12:32:25 +0000

    backstage: separate brand/sub brand tokens

--- apps/dictionary/scripts/build-config/build-component.js
+++ apps/dictionary/scripts/build-config/build-component.js
@@ -2,23 +2,23 @@ import {
 	buildCSS,
 	getBrandNames,
 	getBrandSources,
-	getComponentTokens,
+	getBrandIncludes,
 } from './utils.js';
 
 function buildComponentTokens(componentName) {
 	const brands = getBrandNames();
 
 	brands.forEach(brand => {
-		const sources = getBrandSources(brand);
+		const brandSources = getBrandSources(brand);
 		const destination = `../../components/${componentName}/src/css/tokens/${brand}/${componentName}/_variables.css`;
-		const componentTokens = getComponentTokens(brand);
+		const brandIncludes = getBrandIncludes(brand);
 		const brandSelector = `[data-o3-brand="${brand.split('/').slice(-1)}"]`;
 		const componentSelector = `.${componentName}`;
 		const parentSelector = `${brandSelector} ${componentSelector}`;
 
 		buildCSS({
-			includes: componentTokens,
-			sources: sources,
+			includes: brandIncludes,
+			sources: brandSources,
 			destination,
 			tokenFilter: token => {
 				return token.name.match(`^_?(${componentName})`);
