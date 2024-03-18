6b520b28ef7abad90b0ded321dc45b40fc6ad1b7
Akaki Mikaia <akakimikaia@gmail.com>
2024-03-18 09:54:06 +0000

    chore: update type imports

--- components/o3-button/src/tsx/group.tsx
+++ components/o3-button/src/tsx/group.tsx
@@ -1,4 +1,4 @@
-import type {ButtonGroupProps} from '../types';
+import type {ButtonGroupProps} from '../types/index';
 export function ButtonGroup({children = []}: ButtonGroupProps) {
 	return <div className="o3-button-group">{children}</div>;
 }
