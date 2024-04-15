0008d41ba60742cf632618fb64a8fd48c748437d
ben.freshwater <ben.freshwater@ft.com>
2024-04-15 10:34:18 +0100

    chore: correct demos to use data-o3-brand

--- components/o3-typography/stories/internal/heading.stories.tsx
+++ components/o3-typography/stories/internal/heading.stories.tsx
@@ -1,7 +1,6 @@
 import type {Meta} from '@storybook/react';
 import {Heading as HeadingTsx} from '../../src/tsx';
 import * as TypographyStories from '../story-templates';
-import '../../main.css';
 
 import '../../src/css/brands/internal.css';
 
@@ -10,7 +9,7 @@ export default {
 	component: HeadingTsx,
 	decorators: [
 		Story => (
-			<div className="o3-brand-internal">
+			<div data-o3-brand="internal">
 				<Story />
 			</div>
 		),
