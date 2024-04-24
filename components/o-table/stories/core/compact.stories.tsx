4344cf7b6082b21c0b806190dc15f189e85f4af8
ben.freshwater <ben.freshwater@ft.com>
2024-04-24 10:25:14 +0100

    backstage: display iframed o-table demos instead of TSX stories

---
+++ components/o-table/stories/core/compact.stories.tsx
@@ -0,0 +1,26 @@
+
+export default {
+	title: 'Components/o-table/Compact',
+	args: {},
+	parameters: {
+		html: {},
+	},
+};
+
+export const Compact = args => {
+
+	return (<>
+		<iframe
+			loading="lazy"
+			scrolling="yes"
+			allowTransparency="true"
+			src="https://www.ft.com/__origami/service/build/v3/demo?component=o-table%409.1.0&demo=compact&system_code=origami&brand=core"
+			title="Inline Live Demo"
+			style={{
+				height: 'calc(100vh - 3rem)',
+				'box-sizing': 'border-box',
+				border: '0',
+				width: '100%',
+			}}></iframe>
+	</>);
+}
