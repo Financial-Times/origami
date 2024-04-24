081a4a10c684f36ad7d0d28b34b34c9bbaff834a
Ben Freshwater <ben.freshwater@ft.com>
2024-04-24 14:54:24 +0100

    backstage: display iframed o-table demos instead of TSX stories (#1593)

---
+++ components/o-table/stories/internal/multiple-columns.stories.tsx
@@ -0,0 +1,26 @@
+
+export default {
+	title: 'Components/o-table/Multiple Columns',
+	args: {},
+	parameters: {
+		html: {},
+	},
+};
+
+export const MultipleColumns = args => {
+
+	return (<>
+		<iframe
+			loading="lazy"
+			scrolling="yes"
+			allowTransparency="true"
+			src="https://www.ft.com/__origami/service/build/v3/demo?component=o-table%409.1.0&demo=multiple-columns-responsive-overflow&system_code=origami&brand=internal"
+			title="Inline Live Demo"
+			style={{
+				height: 'calc(100vh - 3rem)',
+				'box-sizing': 'border-box',
+				border: '0',
+				width: '100%',
+			}}></iframe>
+	</>);
+}
