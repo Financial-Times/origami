38d8de7a40f90e785ad24ad38f39cf7fe90fd936
ben.freshwater <ben.freshwater@ft.com>
2024-05-02 10:29:58 +0100

    feat!: remove verbose classes from o3-typography Caption

--- components/o3-typography/src/tsx/caption.tsx
+++ components/o3-typography/src/tsx/caption.tsx
@@ -1,9 +1,5 @@
 import {StyleArguments} from './getStyleAttributes';
 
 export const Caption: React.FC<{children: React.ReactNode}> = ({children}) => {
-	return (
-		<figcaption className="o3-typography o3-typography-caption">
-			{children}
-		</figcaption>
-	);
+	return <figcaption className="o3-typography-caption">{children}</figcaption>;
 };
