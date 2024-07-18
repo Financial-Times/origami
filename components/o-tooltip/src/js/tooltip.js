b3f683029f536eea93e346ba5e9939a51ed58dab
ben.freshwater <ben.freshwater@ft.com>
2024-07-18 15:25:45 +0100

    chore: change to trial install

--- components/o-tooltip/src/js/tooltip.js
+++ components/o-tooltip/src/js/tooltip.js
@@ -266,7 +266,7 @@ class Tooltip {
 		}
 		if (this.tooltipEl && this.tooltipEl.parentElement && this.opts && this.opts.content) {
 			this.tooltipEl.parentElement.removeChild(this.tooltipEl);
-		}
+		} //test change
 
 		Tooltip._tooltips.delete(this.tooltipEl);
 		if (Tooltip._tooltips.size === 0) {
