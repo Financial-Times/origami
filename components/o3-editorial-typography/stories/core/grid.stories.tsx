aa86ebb54b30a92367530b1bf8673fc50de06000
ben.freshwater <ben.freshwater@ft.com>
2024-07-30 15:42:17 +0100

    docs: <dropable> implement as two grids

---
+++ components/o3-editorial-typography/stories/core/grid.stories.tsx
@@ -0,0 +1,40 @@
+import type {Meta} from '@storybook/react';
+import '../../src/css/brands/core.css';
+import './grid.css';
+
+export default {
+	title: 'Core/o3-grid-playground',
+	decorators: [
+		Story => (
+			<div data-o3-brand="core">
+				<Story />
+			</div>
+		),
+	],
+	parameters: {
+		backgrounds: {default: 'paper'},
+		controls: {exclude: ['children']},
+	},
+} as Meta;
+
+
+export const GridStories = {
+	render: (args) => {
+		return <div className="grid-overflow-wrapper">
+			<div className="grid-wrapper">
+				<div>Item 1</div>
+				<div>Item 2</div>
+				<div>Item 3</div>
+				<div>Item 4</div>
+				<div>Item 4</div>
+				<div>Item 4</div>
+				<div>Item 4</div>
+				<div>Item 4</div>
+				<div>Item 4</div>
+				<div>Item 4</div>
+				<div>Item 4</div>
+				<div>Item 4</div>
+			</div>
+		</div>;
+	},
+};
