3008353ba06fd1fc32dafe04b6bb48c590c51ce1
Akaki Mikaia <akakimikaia@gmail.com>
2024-08-07 15:15:57 +0100

    chore: initialise o3 form with basic setup for whitelabel

---
+++ components/o3-form/src/tsx/checkBox.tsx
@@ -0,0 +1,8 @@
+export const CheckBox = () => {
+	return (
+		<>
+			<label htmlFor="checkbox">Input</label>
+			<input type="checkbox" id="checkbox" name="checkbox" value="checkbox" />
+		</>
+	);
+};
---
+++ components/o3-form/stories/whitelabel/forms.stories.tsx
@@ -0,0 +1,21 @@
+import type {Meta} from '@storybook/react';
+import {CheckBox as CheckBoxTsx} from '../../src/tsx/checkBox';
+import '../../src/css/brands/whitelabel.css';
+import links from '@financial-times/o3-figma-sb-links';
+
+export default {
+	title: 'Whitelabel/o3-form',
+	component: CheckBoxTsx,
+	decorators: [
+		Story => (
+			<div data-o3-brand="whitelabel">
+				<Story />
+			</div>
+		),
+	],
+	parameters: {
+		backgrounds: {disable: true},
+	},
+} as Meta;
+
+export const CheckBox = {};
