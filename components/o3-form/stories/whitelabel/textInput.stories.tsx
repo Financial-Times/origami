79e236e424af35bcd50d66eb872b86dd809be498
Ben Freshwater <ben.freshwater@ft.com>
2024-08-27 16:10:34 +0100

    fix: align text input max width to 4 columns (#1802)

--- components/o3-form/stories/whitelabel/textInput.stories.tsx
+++ components/o3-form/stories/whitelabel/textInput.stories.tsx
@@ -9,8 +9,10 @@ const meta: Meta<typeof TextInputTsx> = {
 	component: TextInputTsx,
 	decorators: [
 		Story => (
-			<div data-o3-brand="whitelabel">
-				<Story />
+			<div data-o3-brand="whitelabel" className="o3-grid">
+				<div style={{gridColumn: `content-start / content-end`}}>
+					<Story />
+				</div>
 			</div>
 		),
 	],
@@ -54,33 +56,33 @@ export const TextInputErrorState = {
 		return (
 			<TextInputTsx label={args.label} disabled={args.disabled} description={args.description} length={args.length}
 										feedback={args.feedback}
-										 />);
+			/>);
 	},
 };
 
 export const ShortTextInput = (args) => {
-		return (
-			<>
-				<TextInputTsx label="Day" description="Two digit day of the month" feedback={args.feedback} length={2}
-				/>
-				<TextInputTsx label="Security code"
-											description="3 digit security code as printed on the back of your credit card."
-											feedback={args.feedback} length={3} />
-				<TextInputTsx label="Date of Bith" description="The year you were born" feedback={args.feedback} length={4}
-				/>
-				<TextInputTsx label="Passcode" description="A 5-digin code to authenticate you on login"
-											feedback={args.feedback} length={5} />
-			</>
-		);
+	return (
+		<>
+			<TextInputTsx label="Day" description="Two digit day of the month" feedback={args.feedback} length={2}
+			/>
+			<TextInputTsx label="Security code"
+										description="3 digit security code as printed on the back of your credit card."
+										feedback={args.feedback} length={3} />
+			<TextInputTsx label="Date of Bith" description="The year you were born" feedback={args.feedback} length={4}
+			/>
+			<TextInputTsx label="Passcode" description="A 5-digin code to authenticate you on login"
+										feedback={args.feedback} length={5} />
+		</>
+	);
 };
 ShortTextInput.parameters = {
 	controls: {
-		disable: true
+		disable: true,
 	},
 	design: {
 		type: 'figma',
 		url: links['whitelabel-o3-form--text-input'].figma,
 	},
-}
+};
 
 export default meta;
