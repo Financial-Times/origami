214851183a9f6371c5725af4134d9ea7e61d3b4a
ben.freshwater <ben.freshwater@ft.com>
2024-09-26 13:44:40 +0100

    chore: space icons evenly

--- components/o3-social-sign-in/src/tsx/socialSignIn.tsx
+++ components/o3-social-sign-in/src/tsx/socialSignIn.tsx
@@ -1,19 +1,38 @@
+import {AppleIcon, GoogleIcon} from './icons';
+
 type Provider = 'apple' | 'google';
 
 const providerToTitle: Record<Provider, string> = {
 	apple: 'Apple',
-	google: 'Google'
-}
+	google: 'Google',
+};
 
 type Flow = 'signin' | 'signup';
+
 export interface SocialSignInProps {
 	provider: Provider,
 	flow: Flow
 }
 
+function isProviderApple(provider: Provider): boolean {
+	return provider === 'apple';
+}
+
 export const SocialSignIn = (props: SocialSignInProps) => {
-	const providerId = props.provider === 'apple' ? 'appleid-signin' : 'gSignInWrapper';
+	const providerId = isProviderApple(props.provider) ? 'appleid-signin' : 'gSignInWrapper';
 	const flowCopy = props.flow === 'signin' ? 'in' : 'up';
 
-	return (<button id={providerId} className={`o3-social-sign-in-button o3-social-sign-in-button--${props.provider}`}>Sign {flowCopy} with {providerToTitle[props.provider]}</button>)
-}
+	return (
+		<button id={providerId} className={`o3-social-sign-in-button o3-social-sign-in-button--${props.provider}`}>
+			{/*TODO: Check if this gets read, may need to be aria hidden*/}
+			<span
+				className="o3-social-sign-in-button__icon">{isProviderApple(props.provider) ?
+				<AppleIcon /> :
+				<GoogleIcon />
+			}</span>
+			<span
+				className="o3-social-sign-in-button__copy">
+				Sign {flowCopy} with {providerToTitle[props.provider]}
+			</span>
+		</button>);
+};
