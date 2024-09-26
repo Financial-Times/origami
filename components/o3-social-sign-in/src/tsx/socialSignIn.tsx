bb15620988cc11f7f1f0d97de2a33329ef119c28
ben.freshwater <ben.freshwater@ft.com>
2024-09-26 15:39:29 +0100

    chore: display icons as background image

--- components/o3-social-sign-in/src/tsx/socialSignIn.tsx
+++ components/o3-social-sign-in/src/tsx/socialSignIn.tsx
@@ -14,22 +14,15 @@ export interface SocialSignInProps {
 	flow: Flow
 }
 
-function isProviderApple(provider: Provider): boolean {
-	return provider === 'apple';
-}
-
 export const SocialSignIn = (props: SocialSignInProps) => {
-	const providerId = isProviderApple(props.provider) ? 'appleid-signin' : 'gSignInWrapper';
+	const providerId = props.provider === 'apple' ? 'appleid-signin' : 'gSignInWrapper';
 	const flowCopy = props.flow === 'signin' ? 'in' : 'up';
 
 	return (
 		<button id={providerId} className={`o3-social-sign-in-button o3-social-sign-in-button--${props.provider}`}>
 			<span
 				className="o3-social-sign-in-button__icon"
-				aria-hidden={true}>{isProviderApple(props.provider) ?
-				<AppleIcon /> :
-				<GoogleIcon />
-			}</span>
+				aria-hidden={true}></span>
 			<span
 				className="o3-social-sign-in-button__copy">
 				Sign {flowCopy} with {providerToTitle[props.provider]}
