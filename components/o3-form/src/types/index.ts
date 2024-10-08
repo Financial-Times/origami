f92ac53a96e1496e671b5fe5f0d4a9437ce680cd
Akaki Mikaia <akakimikaia@gmail.com>
2024-10-08 09:52:50 +0100

    fix: udpate token values and implement radio button icon in css

--- components/o3-form/src/types/index.ts
+++ components/o3-form/src/types/index.ts
@@ -23,7 +23,7 @@ export interface CheckBoxProps extends BaseInputProps {
 
 export interface RadioButtonProps extends BaseInputProps {
 	inputId: string;
-	radioButtonLabel: string; // Label specifically for the checkbox
+	radioButtonLabel: string; // Label specifically for the radio button
 }
 export interface FormFieldsetProps {
 	label: string;
