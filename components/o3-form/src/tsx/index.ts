c8947dd912254d097856f36f1b8ea67fcb3f7189
Akaki Mikaia <akakimikaia@gmail.com>
2024-10-11 16:03:08 +0100

    chore: initialise guidlines for form elements craete tip component

--- components/o3-form/src/tsx/index.ts
+++ components/o3-form/src/tsx/index.ts
@@ -3,3 +3,5 @@ export * from './fieldComponents/FormField';
 export * from './fieldComponents/Form';
 export * from './CheckBox';
 export * from './Error-summary';
+export * from './PasswordInput';
+export * from './TextInput';
