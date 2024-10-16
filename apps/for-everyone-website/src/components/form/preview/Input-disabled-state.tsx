b7743556fbbca9335a9ff3eecaf2853c3abc7f23
Akaki Mikaia <akakimikaia@gmail.com>
2024-10-16 12:02:46 +0100

    docs: - add examples and guidlines for form intro page - organise folders to make it clear where do preveiws, guidlines and exmaples go

---
+++ apps/for-everyone-website/src/components/form/preview/Input-disabled-state.tsx
@@ -0,0 +1,21 @@
+import {Form, TextInput} from '@financial-times/o3-form/esm';
+
+function ButtonPreview() {
+	return (
+		<>
+			<meta itemProp="@preview" />
+			<Form>
+				<TextInput
+					label="Email"
+					description='You need to confirm your email before you can change it'
+					attributes={{defaultValue: 'john@gmail.com'}}
+					disabled={true}
+				/>
+			</Form>
+			<meta itemProp="@preview" />
+		</>
+	);
+}
+export const filePath = 'src/components/form/preview/Input-error.tsx';
+
+export {ButtonPreview as preview};
