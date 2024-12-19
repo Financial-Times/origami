4022d6049ff2ccdd05b8516bb9c372a04774d565
Lee Moody <notlee@users.noreply.github.com>
2024-12-19 10:33:23 +0000

    chore: 2nd attempt to ignore bad release commits (#1908)

--- scripts/regenerate-ci-config-files.js
+++ scripts/regenerate-ci-config-files.js
@@ -28,7 +28,7 @@ await del(['.github/workflows/percy-*.yml', '.github/workflows/test-*.yml']);
 let dotReleasePleaseManifest = {};
 let releasePleaseConfig = {
 	'bootstrap-sha': '4d52a27b6c08755d6c527723c2595ade3e024898',
-	'last-release-sha': '4381d13715297e1c04de729261b2b53ac3396621',
+	'last-release-sha': 'c5034a17aaeb69df1201d542bd4e5accdc50931f',
 	packages: {},
 };
 
