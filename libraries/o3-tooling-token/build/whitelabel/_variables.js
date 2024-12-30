cc5e133fec1823726e5f1cdb63c6069f5f26ad7f
ben.freshwater <ben.freshwater@ft.com>
2024-12-30 13:15:04 +0000

    feat: introduce rounded borders to buttons

--- libraries/o3-tooling-token/build/whitelabel/_variables.js
+++ libraries/o3-tooling-token/build/whitelabel/_variables.js
@@ -1,5 +1,5 @@
 export default {
-	"_o3-border-radius-1": {
+	"o3-border-radius-1": {
 		"shortName": "1",
 		"value": "4px",
 		"originalValue": "4",
@@ -9,10 +9,10 @@ export default {
 				"border-radius",
 				"1"
 		],
-		"css": "--_o3-border-radius-1",
+		"css": "--o3-border-radius-1",
 		"figma": "o3/border-radius/1"
 },
-	"_o3-border-radius-2": {
+	"o3-border-radius-2": {
 		"shortName": "2",
 		"value": "6px",
 		"originalValue": "6",
@@ -22,7 +22,7 @@ export default {
 				"border-radius",
 				"2"
 		],
-		"css": "--_o3-border-radius-2",
+		"css": "--o3-border-radius-2",
 		"figma": "o3/border-radius/2"
 },
 	"_o3-font-family-metric": {
