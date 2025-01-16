ac4c628200692a874086e2e7c3af15c2d7fb0d99
Lee Moody <notlee@users.noreply.github.com>
2025-01-16 17:37:42 +0000

    chore: Remove ui caption component
    Instead use type usecases

--- components/o3-foundation/src/tsx/index.ts
+++ components/o3-foundation/src/tsx/index.ts
@@ -1,5 +1,4 @@
 import {UnorderedList, OrderedList} from './list';
-import {Caption} from './caption';
 import {Wrapper} from './wrapper';
 
-export {UnorderedList, OrderedList, Caption, Wrapper};
+export {UnorderedList, OrderedList, Wrapper};
