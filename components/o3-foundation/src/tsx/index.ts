45f3fb788929195bc1816d26bcdddc910ec824ef
Lee Moody <notlee@users.noreply.github.com>
2025-01-16 16:27:30 +0000

    chore: Delete Heading (UI) component
    This has been replaced by the title usecases.

--- components/o3-foundation/src/tsx/index.ts
+++ components/o3-foundation/src/tsx/index.ts
@@ -1,4 +1,3 @@
-import {Heading} from './heading';
 import {Link} from './link';
 import {Body} from './body';
 import {UnorderedList, OrderedList} from './list';
@@ -6,13 +5,4 @@ import {Footer} from './footer';
 import {Caption} from './caption';
 import {Wrapper} from './wrapper';
 
-export {
-	Heading,
-	Link,
-	Body,
-	UnorderedList,
-	OrderedList,
-	Footer,
-	Caption,
-	Wrapper,
-};
+export {Link, Body, UnorderedList, OrderedList, Footer, Caption, Wrapper};
