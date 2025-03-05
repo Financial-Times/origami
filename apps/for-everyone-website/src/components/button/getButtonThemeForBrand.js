42d89ef25e36e1987f652d8483530e1c7bc97886
ben.freshwater <ben.freshwater@ft.com>
2025-03-05 10:34:10 +0000

    backstage: define themes for each brand

---
+++ apps/for-everyone-website/src/components/button/getButtonThemeForBrand.js
@@ -0,0 +1,7 @@
+export const getButtonThemeForBrand = {
+	core: ['standard', 'inverse', 'mono'],
+	professional: ['standard', 'inverse'],
+	'sustainable-views': ['standard', 'inverse', 'mono'],
+	internal: ['standard', 'inverse', 'mono'],
+	whitelabel: ['standard', 'inverse', 'mono'],
+};
