0c9db5a89448fc3eb9502711232adad84425035b
James Loveridge <james.loveridge@ft.com>
2025-03-21 15:48:04 +0000

    feat: add neutral to the accepted list of themes for buttons props

--- components/o3-button/src/types/index.ts
+++ components/o3-button/src/types/index.ts
@@ -4,7 +4,7 @@ export interface ButtonProps {
 	type: 'primary' | 'secondary' | 'ghost';
 	size?: 'small' | '';
 	fluid?: boolean;
-	theme?: 'inverse' | 'mono';
+	theme?: 'inverse' | 'mono' | 'neutral';
 	icon?:
 		| 'chevron-left'
 		| 'chevron-right'
