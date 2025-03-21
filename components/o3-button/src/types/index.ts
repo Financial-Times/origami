bed643655aa38dce01cb0d797a726751a5a0301c
leanne cornish <leanne.cornish@ft.com>
2025-03-21 14:53:24 +0000

    feat: calendar as default icon

--- components/o3-button/src/types/index.ts
+++ components/o3-button/src/types/index.ts
@@ -17,7 +17,8 @@ export interface ButtonProps {
 		| 'download'
 		| 'search'
 		| 'refresh'
-		| 'cross';
+		| 'cross'
+		| 'calendar';
 	iconOnly?: boolean;
 	visuallyHideDisabled?: boolean;
 	attributes?: {
