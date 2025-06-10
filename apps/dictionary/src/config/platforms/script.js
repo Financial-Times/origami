047e5c1d44ca914ab7099fb341d855ccda3d8b6d
Lee Moody <notlee@users.noreply.github.com>
2025-06-10 09:51:23 +0100

    feat: Enable named exports for esm tokens.
    - Uses snake case: kebab case isn't support as a variable name in JS. Camel case makes for some awkward token names e.g. `o3_font_size_metric2_0` vs. `o3FontLineheightMetric20`
    - Non-breaking by adding an extra export under `[brand]-named.js` as in named export.
    - These a tree-shakeable for reduced bundle sizes.

--- apps/dictionary/src/config/platforms/script.js
+++ apps/dictionary/src/config/platforms/script.js
@@ -1,7 +1,7 @@
 import path from 'node:path';
 import {webTransformers} from '../transformers.js';
 
-function jsConfigForBrand(brand) {
+function jsObjectConfigForBrand(brand) {
 	const subBrand = brand.split('/').pop();
 	return {
 		transformGroup: 'js',
@@ -30,4 +30,30 @@ function jsConfigForBrand(brand) {
 	};
 }
 
-export {jsConfigForBrand};
+function jsNamedExportConfigForBrand(brand) {
+	const subBrand = brand.split('/').pop();
+	return {
+		transformGroup: 'js',
+		transforms: [...webTransformers, 'name/snake'],
+		buildPath: path.join(process.cwd(), '../../components/'),
+		files: [
+			{
+				filter: token => {
+					if (
+						token.original.value === '{DO-NOT-USE}' ||
+						token.path[0] === 'DO-NOT-USE'
+					) {
+						return false;
+					}
+					return (
+						!token.filePath.includes(`/components/`)
+					);
+				},
+				destination: `o3-foundation/src/js/tokens/${subBrand}-named.js`,
+				format: 'javascript/es6'
+			}
+		],
+	};
+}
+
+export {jsObjectConfigForBrand, jsNamedExportConfigForBrand};
