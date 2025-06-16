2372c9d9c7afb573bcf4e3d39b54cbdf08f92f2a
Lee Moody <notlee@users.noreply.github.com>
2025-06-16 15:08:44 +0100

    feat: first / end button icon helper

--- components/o-buttons/src/tsx/button.tsx
+++ components/o-buttons/src/tsx/button.tsx
@@ -19,6 +19,8 @@ export interface ButtonProps {
 		| "search"
 		| "refresh"
 		| "cross"
+		| "start"
+		| "end"
 		| ""
 		| (string & Record<never, never>); // Support IDE autocomplete whilst allowing any string https://github.com/microsoft/TypeScript/issues/29729#issuecomment-1331857805
 	iconOnly?: boolean;
