d88fcdb7e8bcb0f00bf36f09ef18de8e97ead912
Aylin Usendzhikova <aylin.u@mail.bg>
2025-07-02 13:28:17 +0300

    Revert "feat: update ask-ft-button"
    This reverts commit a85935b7dc473210460a329ed3f5c9fd3b1fb8ac.

--- components/o-header/src/tsx/components/ask-ft-button.tsx
+++ components/o-header/src/tsx/components/ask-ft-button.tsx
@@ -1,23 +1,17 @@
-import {LinkButton} from '@financial-times/o3-button';
-
 export interface AskFtButtonProps {
-	id: string;
-	variant: 'top' | 'drawer';
-	dataTrackable: string;
+	id: string
+	variant: 'top' | 'drawer'
+	dataTrackable: string
 }
 
-export const AskFtButton = ({id, variant, dataTrackable}: AskFtButtonProps) => (
-	<LinkButton
+export const AskFtButton = ({ id, variant, dataTrackable }: AskFtButtonProps) => (
+	<a
+		id={id}
+		className={`o-header__ask-ft-button o-header__ask-ft-button--${variant}`}
+		data-trackable={dataTrackable}
 		href="https://ask.ft.com"
-		icon="sparkles"
-		label="Ask FT"
-		size="small"
-		theme="neutral"
-		type="primary"
-		attributes={{
-			id: id,
-			'data-trackable': dataTrackable,
-			className: `o-header__ask-ft-button o-header__ask-ft-button--${variant}`,
-		}}
-	/>
-);
+		title="ASK FT"
+	>
+		Ask FT
+	</a>
+)
