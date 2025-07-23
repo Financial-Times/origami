e74888caf034cb2d48cbf17ad2984fc8b3cd8a9e
Sqyphen <Tobias.fieldhouse@gmail.com>
2025-07-23 11:47:19 +0100

    backstage: introduce interaction tests to storybook (#2182)
    * backstage: adding initial o-tootip interaction tests
    
    * backstage: updating core o-tooltip story changes
    
    * backstage: update test implementation and add test-runner
    
    * backstage: removed return test
    
    * docs: add whitelabel/core examples for o3-button
    
    * feat: introduce checkbox descriptions (#2176)
    
    * feat: introduce checkbox descriptions
    
    * feat: use span on phrasing content
    
    * chore: release main (#2183)
    
    * chore: release main
    
    * chore: update package-lock.json
    
    ---------
    
    Co-authored-by: ben.freshwater <ben.freshwater@ft.com>
    
    ---------
    
    Co-authored-by: James Loveridge <james.loveridge@ft.com>
    Co-authored-by: Ben Freshwater <ben.freshwater@ft.com>
    Co-authored-by: Fox <origamiserviceuser@users.noreply.github.com>

---
+++ components/o3-tooltip/stories/core/tooltip-tests.stories.tsx
@@ -0,0 +1,72 @@
+import type { Meta } from '@storybook/react';
+import { TooltipOnboarding as OnboardingTooltipTsx } from '../../src/tsx/onboardingTooltip';
+import {
+	OnboardingTooltipTest as OnboardingTooltipInteractionStory,
+	ToggleToolTipTest as ToggleToolTipInteractionStory,
+} from '../story-template';
+
+import { userEvent, within, expect } from '@storybook/test';
+
+import '../../src/css/brands/core.css';
+import '../../../o3-button/src/css/brands/core.css';
+
+export default {
+	title: 'Tests/Core/o3-tooltip',
+	tags: ['!autodocs'],
+	component: OnboardingTooltipTsx,
+	decorators: [
+		Story => (
+			<div data-o3-brand="core">
+				<Story />
+			</div>
+		),
+	],
+	parameters: {
+		backgrounds: {default: 'paper'},
+		chromatic: { disableSnapshot: true },
+	},
+} as Meta;
+
+export const OnboardingTooltipTest = OnboardingTooltipInteractionStory;
+OnboardingTooltipTest.play = async ({ canvasElement }) => {
+	const canvas = within(canvasElement);
+
+	const tooltipText = 'Tool tip content that is quite long, Tool tip content that is quite long, Tool tip content that is quite long';
+
+	await expect(
+		await canvas.findByText(tooltipText)
+	).not.toBeNull();
+
+	await userEvent.click(
+		canvas.getByRole('button', { name: 'Close tooltip' }));
+
+	await expect(
+		canvas.queryByText(tooltipText)
+	).toBeNull();
+}
+
+
+export const ToggleTooltipTest = ToggleToolTipInteractionStory;
+ToggleTooltipTest.play = async ({ canvasElement }) => {
+	const canvas = within(canvasElement);
+
+	const tooltipText = 'tooltip content that is quite long and has some information about the target';
+
+	await expect(
+		await canvas.queryByText(tooltipText)
+	).toBeNull();
+
+	await userEvent.click(
+		await canvas.getByRole('button', { name: 'more information' }));
+
+	await expect(
+		await canvas.findByText(tooltipText)
+	).not.toBeNull();
+
+	await userEvent.click(
+		await canvas.getByRole('button', { name: 'more information' }));
+
+	await expect(
+		await canvas.queryByText(tooltipText)
+	).toBeNull();
+}
