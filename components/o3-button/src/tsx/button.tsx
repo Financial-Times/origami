fe3b15ac92549a8a18a95084a90a8c3ae025f91d
ben.freshwater <ben.freshwater@ft.com>
2025-08-05 13:55:15 +0100

    feat: allow refs on o3-button

--- components/o3-button/src/tsx/button.tsx
+++ components/o3-button/src/tsx/button.tsx
@@ -58,6 +58,7 @@ export function Button({
 	visuallyHideDisabled = false,
 	attributes = {},
 	onClick,
+	ref,
 }: ButtonProps) {
 	// Combine custom classes with first party o3-button classes,
 	// rather than override them.
@@ -71,6 +72,7 @@ export function Button({
 	}
 	return (
 		<button
+			ref={ref}
 			onClick={onClick ? event => onClick(event) : undefined}
 			className={makeClassNames({
 				customClasses,
