5babf653fdbf4e2a61bfce81d067e2cfe7605023
Kara Brightwell <kara@153.io>
2025-09-16 15:26:50 +0100

    fix: remove tabindex on footer headings when transitioning to large breakpoint (#2221)
    * fix: remove tabindex on footer headings when transitioning to large breakpoint
    
    since the heading is no longer interactive, it shouldn't have a `tabindex`. this was reported by DAC in [CPP-2630](https://financialtimes.atlassian.net/browse/CPP-2630).
    
    * backstage: pass toggle and target elements into o-footer stub toggle
    
    * backstage: add tests for o-footer tabindex behaviour

--- components/o-footer/test/oFooter.test.js
+++ components/o-footer/test/oFooter.test.js
@@ -83,11 +83,24 @@ describe("oFooter", () => {
 				proclaim.equal(typeof footer._toggles, 'object');
 				proclaim.equal(footer._toggles.length, 5);
 			});
+
+			it("sets tabindex=0 for every toggle target on the page", () => {
+				const footer = oFooter.init();
+
+				footer.setup();
+
+				const toggleEls = footer.footerEl.querySelectorAll('[aria-controls]');
+
+				proclaim.deepEqual(
+					Array.from(toggleEls, el => el.getAttribute('tabindex')),
+					['0', '0', '0', '0', '0']
+				)
+			})
 		});
 		describe("destroy()", () => {
 			it("calls destroy on things found in _toggles and then sets _toggles to null", () => {
 				const footer = oFooter.init();
-				const toggle = new Toggle();
+				const toggle = new Toggle(document.createElement('h3'), { target: document.createElement('ul') });
 				const toggleSpy = sinon.stub(toggle, "destroy").returns(true);
 
 				footer._toggles = [toggle];
@@ -97,6 +110,20 @@ describe("oFooter", () => {
 				proclaim.equal(toggleSpy.called, true);
 				proclaim.equal(footer._toggles, null);
 
+			});
+
+			it('removes tabindex on every toggle target on the page', () => {
+				const footer = oFooter.init();
+
+				footer.setup();
+				footer.destroy()
+
+				const toggleEls = footer.footerEl.querySelectorAll('[aria-controls]');
+
+				proclaim.deepEqual(
+					Array.from(toggleEls, el => el.getAttribute('tabindex')),
+					[null, null, null, null, null]
+				);
 			});
 		});
 
