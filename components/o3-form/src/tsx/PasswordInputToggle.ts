e5e2b881d4b8b8fe4876974eb2fc80a2d4fc97c8
ben.freshwater <ben.freshwater@ft.com>
2025-11-05 16:11:21 +0000

    fix: fix password js bundling

--- components/o3-form/src/tsx/PasswordInputToggle.ts
+++ components/o3-form/src/tsx/PasswordInputToggle.ts
@@ -10,6 +10,10 @@
  * @export
  */
 export class PasswordInputToggle {
+	_toggleButtonId: string;
+	_input: HTMLInputElement;
+	_toggleState: boolean;
+	_toggleButton: HTMLElement | null;
 	/**
 	 * Class constructor.
 	 *
@@ -17,13 +21,14 @@ export class PasswordInputToggle {
 	 * @param {PasswordInputToggleOptions} [options] - An options object for configuring the password input
 	 */
 	constructor(
-		element,
+		element: HTMLInputElement,
 		options = {toggleButtonId: 'o3-form-password-input-toggle'}
 	) {
 		this._toggleButtonId = options.toggleButtonId;
 		this._input = element;
 		this._toggleState = false;
 		this._toggleButton = document.getElementById(options.toggleButtonId);
+
 		this._setAttributes();
 	}
 
@@ -32,23 +37,25 @@ export class PasswordInputToggle {
 
 		this._input.type = this._toggleState ? 'text' : 'password';
 
-		this._toggleButton.setAttribute(
-			'aria-pressed',
-			this._toggleState.toString()
-		);
-		this._toggleButton.setAttribute('aria-label', buttonLabel);
-		this._toggleButton.setAttribute('title', buttonLabel);
+		if (this._toggleButton) {
+			this._toggleButton.setAttribute(
+				'aria-pressed',
+				this._toggleState.toString()
+			);
+			this._toggleButton.setAttribute('aria-label', buttonLabel);
+			this._toggleButton.setAttribute('title', buttonLabel);
 
-		this._toggleButton.classList.remove(
-			this._toggleState
-				? 'o3-password-input__show-password-toggle--show'
-				: 'o3-password-input__show-password-toggle--hide'
-		);
-		this._toggleButton.classList.add(
-			this._toggleState
-				? 'o3-password-input__show-password-toggle--hide'
-				: 'o3-password-input__show-password-toggle--show'
-		);
+			this._toggleButton.classList.remove(
+				this._toggleState
+					? 'o3-password-input__show-password-toggle--show'
+					: 'o3-password-input__show-password-toggle--hide'
+			);
+			this._toggleButton.classList.add(
+				this._toggleState
+					? 'o3-password-input__show-password-toggle--hide'
+					: 'o3-password-input__show-password-toggle--show'
+			);
+		}
 	}
 
 	/** @returns {void} */
