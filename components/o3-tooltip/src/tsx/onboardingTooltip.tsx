7890c1931d14ff97df42e35af7695bf854eb0b82
ben.freshwater <ben.freshwater@ft.com>
2026-01-09 11:16:30 +0000

    feat: introduce onClose callback to o3-tooltip Web Component

--- components/o3-tooltip/src/tsx/onboardingTooltip.tsx
+++ components/o3-tooltip/src/tsx/onboardingTooltip.tsx
@@ -1,14 +1,27 @@
 import type {OnboardingToolTipProps} from '../types/index';
+import {MutableRefObject, useEffect, useRef} from "react";
 
 export function TooltipOnboarding({
+	id,
 	targetId,
 	placement = 'top',
 	content,
 	title,
 	contentId,
+	onClose
 }: OnboardingToolTipProps) {
+	const ref: MutableRefObject<{ onClose?: () => void } | null> = useRef(null);
+
+	useEffect(() => {
+		if (!ref.current) return;
+
+		ref.current.onClose = onClose;
+	}, [onClose]);
+
 	return (
 		<o3-tooltip-onboarding
+			id={id}
+			ref={ref}
 			role="tooltip"
 			placement={placement}
 			target-id={targetId}
