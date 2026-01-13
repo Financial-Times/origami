b5731083a9f904e9be936942f829db0e3789f7ef
ben.freshwater <ben.freshwater@ft.com>
2026-01-13 10:56:50 +0000

    chore: improve styling of validation form

--- components/o3-form/stories/FormValidationHtml.tsx
+++ components/o3-form/stories/FormValidationHtml.tsx
@@ -64,7 +64,7 @@ export const FormValidationHtml = () => {
 					type="text"
 				/>
 			</div>
-			<input data-testid="submit-button" className="o3-button o3-button--primary" type="submit" value="Submit"/>
+			<input data-testid="submit-button" className="o3-button o3-button--primary" type="submit" value="Submit" style={{'margin-top': '12px'}}/>
 		</form>
 	)
 }
