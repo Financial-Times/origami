33db417527d7a1058e6e4a35300927907d86a671
James Loveridge <james.loveridge@ft.com>
2026-01-19 14:41:38 +0000

    backstage: use types in o3-foundation

---
+++ components/o3-foundation/types/index.ts
@@ -0,0 +1,86 @@
+// Base Input Props
+export type BaseInputProps = {
+	inputId?: string;
+	label?: string;
+	description?: string;
+	optional?: boolean;
+	error?: boolean;
+	attributes?: JSX.IntrinsicElements['input'];
+};
+
+export interface TextInputProps extends BaseInputProps {
+	disabled?: boolean;
+	length?: 2 | 3 | 4 | 5;
+	feedback?: FeedbackProps;
+}
+
+export interface FileInputProps extends BaseInputProps {
+	isUploading?: boolean;
+	disabled?: boolean;
+	feedback?: FeedbackProps;
+}
+
+export interface DateInputProps extends BaseInputProps {
+	disabled?: boolean;
+	feedback?: FeedbackProps;
+	isDatePicker?: boolean;
+}
+
+export interface SelectInputProps {
+	disabled?: boolean;
+	feedback?: FeedbackProps;
+	children?: React.JSX.Element[];
+	inputId?: string;
+	label?: string;
+	length?: 1 | 3;
+	description?: string;
+	optional?: boolean;
+	error?: boolean;
+	attributes?: JSX.IntrinsicElements['select'];
+}
+
+export interface PasswordInputProps extends BaseInputProps {
+	disabled?: boolean;
+	feedback?: FeedbackProps;
+	forgotPasswordLink?: string;
+
+}
+
+export interface CheckBoxProps extends BaseInputProps {
+	inputId: string;
+	checkboxLabel: string; // Label specifically for the checkbox
+	feedback?: FeedbackProps;
+	checkboxDescription?: string;
+}
+
+export interface RadioButtonProps extends BaseInputProps {
+	inputId: string;
+	radioButtonLabel: string; // Label specifically for the radio button.
+}
+export interface FormFieldsetProps {
+	label: string;
+	description?: string;
+	children: React.JSX.Element | React.JSX.Element[];
+	feedback?: FeedbackProps;
+	optional?: boolean;
+}
+
+export interface FormFieldProps extends BaseInputProps {
+	feedback?: FeedbackProps;
+	children: React.JSX.Element & JSX.IntrinsicElements['input'];
+}
+
+export type FeedbackProps = {
+	errorElementIds?: string[];
+	message: string;
+	type: 'error';
+};
+
+export type ErrorSummaryProps = {
+	errorMessage?: string;
+	errors: {
+		id: string;
+		fieldName: string;
+		message: string;
+	}[];
+};
