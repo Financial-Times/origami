18e60ae039be2d792aa07b432aad2aeeb9325b6f
stegosauruss <russell.wind@ft.com>
2026-02-19 07:43:44 +0000

    BOLT-211 Add event management for subnav

--- components/o-header/src/js/subnav.js
+++ components/o-header/src/js/subnav.js
@@ -1,6 +1,101 @@
 import viewport from '@financial-times/o-viewport';
 import * as oUtils from '@financial-times/o-utils';
 
+const INTENT_ENTER = 300;
+const INTENT_LEAVE = 400;
+
+const expandedDropdowns = [];
+
+function addDropdownEvents(parent, dropdown) {
+	let timeout;
+
+	parent.addEventListener('mouseenter', () => {
+		clearTimeout(timeout);
+
+		if (isDropdownOpen(dropdown)) {
+			return;
+		}
+
+		timeout = setTimeout(() => {
+			if (expandedDropdowns.length) {
+				hideDropdown(expandedDropdowns[0]);
+				showDropdown(dropdown, false);
+			} else {
+				showDropdown(dropdown, true);
+			}
+		}, INTENT_ENTER);
+	});
+
+	const handleKeydown = (event) => {
+		const key = event.key || event.keyCode;
+
+		// Internet Explorer 11 incorrectly maps the escape key to `Esc` instead of `Escape`
+		if (key === 'Escape' || key === 'Esc' || key === 27) {
+			if (isDropdownOpen(dropdown)) {
+				hideDropdown(dropdown);
+			}
+		}
+	};
+
+	document.addEventListener('keydown', handleKeydown);
+
+	parent.addEventListener('mouseleave', () => {
+		clearTimeout(timeout);
+		timeout = setTimeout(() => isDropdownOpen(dropdown) && hideDropdown(dropdown), INTENT_LEAVE);
+	});
+}
+
+function isDropdownOpen(dropdown) {
+	return expandedDropdowns.indexOf(dropdown) !== -1;
+}
+
+function showDropdown(dropdown, animate) {
+	if (animate) {
+		dropdown.classList.add('o-header__subnav-dropdown--animation');
+	}
+
+	dropdown.setAttribute('aria-hidden', 'false');
+	dropdown.setAttribute('aria-expanded', 'true');
+
+	dropdown.dispatchEvent(new CustomEvent('oHeader.SubnavDropdownShow', { bubbles: true }));
+
+	expandedDropdowns.push(dropdown);
+}
+
+function hideDropdown(dropdown) {
+	dropdown.classList.remove('o-header__subnav-dropdown--animation');
+	dropdown.setAttribute('aria-hidden', 'true');
+	dropdown.setAttribute('aria-expanded', 'false');
+
+	dropdown.dispatchEvent(new CustomEvent('oHeader.SubnavDropdownHide', { bubbles: true }));
+
+	expandedDropdowns.splice(expandedDropdowns.indexOf(dropdown), 1);
+}
+
+function initSubnavDropdowns(headerEl) {
+	const dropdowns = Array.from(headerEl.querySelectorAll('[data-o-header-subnav-dropdown]'));
+	const parents = dropdowns.map(dropdown => dropdown.parentNode);
+
+	dropdowns.forEach(dropdown => {
+		dropdown.setAttribute('aria-hidden', 'true');
+		dropdown.setAttribute('aria-expanded', 'false');
+	});
+
+	parents.forEach((parent, i) => addDropdownEvents(parent, dropdowns[i]));
+}
+
+function addHandlersForSubnavItems() {
+	const subnavItems = Array.from(subnav.getElementsByClassName('o-header__subnav-item'));
+    subnavItems.forEach(item => {
+        item.addEventListener('mouseenter', (e) => {
+            console.log('mouseenter', e);
+        });
+		item.addEventListener('mouseleave', (e) => {
+			console.log('mouseleave', e);
+		});
+    });
+}
+
 function init(headerEl) {
 	const subnav = headerEl.querySelector('[data-o-header-subnav]');
 
@@ -88,8 +183,13 @@ function init(headerEl) {
 		button.onclick = scroll;
 	});
 
+	addHandlersForSubnavItems();
+
+	// Initialize subnav dropdown functionality
+	initSubnavDropdowns(headerEl);
+
 	checkCurrentPosition();
 }
 
-export { init };
-export default { init };
+export { init, showDropdown, hideDropdown };
+export default { init, showDropdown, hideDropdown };
